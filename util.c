3542220a002a1cc16a4b11c60c94b5140df081ea
Cyrill Gorcunov <gorcunov@gmail.com>
2011-11-15 13:32:09 +0400

    util: Split long line
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- util.c
+++ util.c
@@ -175,7 +175,8 @@ void printk_vma(struct vma_area *vma_area)
 	if (!vma_area)
 		return;
 
-	printk("s: %16lx e: %16lx l: %4liK p: %4x f: %8x pg: %8lx fd: %4d pid: %4d dev:%02x:%02x:%08lx vf: %s st: %s spc: %s\n",
+	printk("s: %16lx e: %16lx l: %4liK p: %4x f: %8x pg: %8lx "
+	       "fd: %4d pid: %4d dev:%02x:%02x:%08lx vf: %s st: %s spc: %s\n",
 	       vma_area->vma.start, vma_area->vma.end,
 	       (vma_area->vma.end - vma_area->vma.start) >> 10,
 	       vma_area->vma.prot,
