85506595e9b039677041441d484c1096c5e2b4c4
Cyrill Gorcunov <gorcunov@gmail.com>
2011-11-15 23:05:36 +0400

    restorer: Drop futex constants
    We don't use them yet, no need to carry.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- include/restorer.h
+++ include/restorer.h
@@ -221,20 +221,6 @@ static void always_inline write_hex_n(unsigned long num)
 	sys_write(1, &c, 1);
 }
 
-#define FUTEX_WAIT		0
-#define FUTEX_WAKE		1
-#define FUTEX_FD		2
-#define FUTEX_REQUEUE		3
-#define FUTEX_CMP_REQUEUE	4
-#define FUTEX_WAKE_OP		5
-#define FUTEX_LOCK_PI		6
-#define FUTEX_UNLOCK_PI		7
-#define FUTEX_TRYLOCK_PI	8
-#define FUTEX_WAIT_BITSET	9
-#define FUTEX_WAKE_BITSET	10
-#define FUTEX_WAIT_REQUEUE_PI	11
-#define FUTEX_CMP_REQUEUE_PI	12
-
 static always_inline void r_lock(rlock_t *v)
 {
 	while (*v) {
