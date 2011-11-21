37e73d6a1db1128d3ff9da99cdc1d3ab168719e6
Cyrill Gorcunov <gorcunov@gmail.com>
2011-11-21 21:25:43 +0400

    test: Add test-counter test
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

---
+++ test/test-counter.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+
+#include <sys/types.h>
+
+int main(int argc, char *argv[])
+{
+	int counter = 0;
+
+	while (1) {
+		printf("Pid: %10d Counter: %10d\n",
+		       getpid(), counter++);
+		sleep(3);
+	}
+
+	return 0;
+}
