350225a2ae0de4d1b80770772e86880f8e14c4ab
Cyrill Gorcunov <gorcunov@openvz.org>
2011-12-19 23:22:31 +0400

    log: Drop unneeded #includes
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>

--- log.c
+++ log.c
@@ -6,11 +6,9 @@
 #include <stdbool.h>
 #include <limits.h>
 
-#include <sys/param.h>
 #include <sys/types.h>
 #include <sys/time.h>
 #include <sys/resource.h>
-#include <errno.h>
 
 #include <fcntl.h>
 
