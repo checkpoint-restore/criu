269fb0b7774a02982d5b729742e9f75b9ed81162
Cyrill Gorcunov <gorcunov@openvz.org>
2011-12-27 23:38:41 +0400

    net: libnetlink -- Drop trailing whitespaces
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>

--- libnetlink.c
+++ libnetlink.c
@@ -10,13 +10,13 @@ int parse_rtattr(struct rtattr *tb[], int max, struct rtattr *rta, int len)
 	memset(tb, 0, sizeof(struct rtattr *) * (max + 1));
 	while (RTA_OK(rta, len)) {
 		if ((rta->rta_type <= max) && (!tb[rta->rta_type]))
-			tb[rta->rta_type] = rta; 
+			tb[rta->rta_type] = rta;
 		rta = RTA_NEXT(rta,len);
-	}       
+	}
 	if (len)
 		pr_warning("Trimmed RTA: len %d, rta_len %d\n", len, rta->rta_len);
 	return 0;
-}       
+}
 
 int nlmsg_receive(char *buf, int len, int (*cb)(struct nlmsghdr *))
 {
