44a05e70ec7310f90d053818a4f650b82e1512ee
Cyrill Gorcunov <gorcunov@openvz.org>
2012-02-01 14:38:22 +0400

    ips_ns.h: Add CR_ prefix to header defines
    Reported-by: Stanislav Kinsbursky <skinsbursky@parallels.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>

--- include/ipc_ns.h
+++ include/ipc_ns.h
@@ -1,5 +1,5 @@
-#ifndef IPC_NS_H_
-#define IPC_NS_H_
+#ifndef CR_IPC_NS_H_
+#define CR_IPC_NS_H_
 
 #include "crtools.h"
 
@@ -7,4 +7,4 @@ extern void show_ipc_ns(int fd);
 extern int dump_ipc_ns(int ns_pid, struct cr_fdset *fdset);
 extern int prepare_ipc_ns(int pid);
 
-#endif /* IPC_NS_H_ */
+#endif /* CR_IPC_NS_H_ */
