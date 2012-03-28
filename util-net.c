5813cd0b55f3f3a658e497384dcc0f186975ea6d
Cyrill Gorcunov <gorcunov@openvz.org>
2012-03-28 17:36:00 +0400

    util-net: Switch send_fd/recv_fd to use scm_fdset structure
    To be able to use scm_fdset structure the two helpers
    added scm_fdset_init_chunk and scm_fdset_init.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- util-net.c
+++ util-net.c
@@ -1,66 +1,82 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 
+#include <errno.h>
+
+#include "compiler.h"
+#include "types.h"
 #include "syscall.h"
+
+#include "util-net.h"
+
+static void scm_fdset_init_chunk(struct scm_fdset *fdset, int nr_fds)
+{
+	struct cmsghdr *cmsg;
+
+	fdset->hdr.msg_controllen = CMSG_LEN(sizeof(int) * nr_fds);
+
+	cmsg		= CMSG_FIRSTHDR(&fdset->hdr);
+	cmsg->cmsg_len	= fdset->hdr.msg_controllen;
+}
+
+static int *scm_fdset_init(struct scm_fdset *fdset, struct sockaddr_un *saddr, int saddr_len)
+{
+	struct cmsghdr *cmsg;
+
+	BUILD_BUG_ON(CR_SCM_MAX_FD > SCM_MAX_FD);
+	BUILD_BUG_ON(sizeof(fdset->msg_buf) < (CMSG_SPACE(sizeof(int) * CR_SCM_MAX_FD)));
+
+	fdset->msg			= '*';
+
+	fdset->iov.iov_base		= &fdset->msg;
+	fdset->iov.iov_len		= sizeof(fdset->msg);
+
+	fdset->hdr.msg_iov		= &fdset->iov;
+	fdset->hdr.msg_iovlen		= 1;
+	fdset->hdr.msg_name		= (struct sockaddr *)saddr;
+	fdset->hdr.msg_namelen		= saddr_len;
+
+	fdset->hdr.msg_control		= &fdset->msg_buf;
+	fdset->hdr.msg_controllen	= CMSG_LEN(sizeof(int) * CR_SCM_MAX_FD);
+
+	cmsg				= CMSG_FIRSTHDR(&fdset->hdr);
+	cmsg->cmsg_len			= fdset->hdr.msg_controllen;
+	cmsg->cmsg_level		= SOL_SOCKET;
+	cmsg->cmsg_type			= SCM_RIGHTS;
+
+	return (int *)CMSG_DATA(cmsg);
+}
 
 int send_fd(int sock, struct sockaddr_un *saddr, int len, int fd)
 {
-	char cmsgbuf[CMSG_SPACE(sizeof(int))];
-	struct msghdr hdr = { };
-	struct iovec data = { };
-	struct cmsghdr* cmsg;
+	struct scm_fdset fdset;
 	int *cmsg_data;
-	char dummy = '*';
 
-	data.iov_base	= &dummy;
-	data.iov_len	= sizeof(dummy);
-
-	hdr.msg_name	= (struct sockaddr *)saddr;
-	hdr.msg_namelen	= len;
-	hdr.msg_iov	= &data;
-	hdr.msg_iovlen	= 1;
-
-	hdr.msg_control = &cmsgbuf;
-	hdr.msg_controllen = CMSG_LEN(sizeof(int));
-
-	cmsg = CMSG_FIRSTHDR(&hdr);
-	cmsg->cmsg_len   = hdr.msg_controllen;
-	cmsg->cmsg_level = SOL_SOCKET;
-	cmsg->cmsg_type  = SCM_RIGHTS;
-
-	cmsg_data = (int *)CMSG_DATA(cmsg);
+	cmsg_data = scm_fdset_init(&fdset, saddr, len);
+	scm_fdset_init_chunk(&fdset, 1);
 	*cmsg_data = fd;
 
-	return sys_sendmsg(sock, &hdr, 0);
+	return sys_sendmsg(sock, &fdset.hdr, 0);
 }
 
 int recv_fd(int sock)
 {
-	char ccmsg[CMSG_SPACE(sizeof(int))];
-	struct msghdr msg = { };
-	struct iovec iov = { };
+	struct scm_fdset fdset;
 	struct cmsghdr *cmsg;
 	int *cmsg_data;
-	char buf[1];
 	int ret;
 
-	iov.iov_base	= buf;
-	iov.iov_len	= 1;
+	cmsg_data = scm_fdset_init(&fdset, NULL, 0);
+	scm_fdset_init_chunk(&fdset, 1);
 
-	msg.msg_iov	= &iov;
-	msg.msg_iovlen	= 1;
-	msg.msg_control	= ccmsg;
-	msg.msg_controllen = sizeof(ccmsg);
-
-	ret = sys_recvmsg(sock, &msg, 0);
+	ret = sys_recvmsg(sock, &fdset.hdr, 0);
 	if (ret < 0)
 		return ret;
 
-	cmsg = CMSG_FIRSTHDR(&msg);
+	cmsg = CMSG_FIRSTHDR(&fdset.hdr);
 	if (!cmsg || (cmsg->cmsg_type != SCM_RIGHTS))
 		return -2;
 
-	cmsg_data = (int *)CMSG_DATA(cmsg);
 	return *cmsg_data;
 }
 
