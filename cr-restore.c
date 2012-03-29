f964951a147618aae72ea966e3ec4c249deb1765
Cyrill Gorcunov <gorcunov@openvz.org>
2012-03-29 14:49:00 +0400

    restorer: Fix bug in address computation of thread args area
    It's wrong to treat restore_thread_exec_start as arguments
    area (I managed to overlook this problem in commit
    014841825acb14a1d695569b9fe3575f5de6442b) it's rather
    a function start address.
    
    The thread arguments area allocated dynamically after the
    restorer blob itself.
    
    We didn't hit any problems earlier simply because there
    were a few bytes owerwritten in function prologue.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    CC: Andrey Vagin <avagin@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- cr-restore.c
+++ cr-restore.c
@@ -1601,10 +1601,10 @@ static int sigreturn_restore(pid_t pid, struct list_head *tgt_vmas, int nr_vmas)
 	restore_thread_exec_start	= restore_code_start + restorer_blob_offset__restore_thread;
 	restore_task_exec_start		= restore_code_start + restorer_blob_offset__restore_task;
 	task_args			= restore_code_start + restore_code_len;
-	thread_args			= restore_thread_exec_start;
+	thread_args			= (void *)((long)task_args + sizeof(*task_args));
 
 	memzero_p(task_args);
-	memzero_p(thread_args);
+	memzero(thread_args, sizeof(*thread_args) * me->nr_threads);
 
 	/*
 	 * Code at a new place.
