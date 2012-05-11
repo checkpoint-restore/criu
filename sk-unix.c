32e07745cc89d05a2ada33612f294d87d03028a1
Cyrill Gorcunov <gorcunov@openvz.org>
2012-05-11 14:24:00 +0400

    sockets: Print error message on unexpected socket state
    While been playing with sockets dumping I found a case where
    we exit without descriptive error message. Fix it.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- sk-unix.c
+++ sk-unix.c
@@ -90,8 +90,11 @@ static int can_dump_unix_sk(const struct unix_sk_desc *sk)
 	case TCP_ESTABLISHED:
 		break;
 	case TCP_CLOSE:
-		if (sk->type != SOCK_DGRAM)
+		if (sk->type != SOCK_DGRAM) {
+			pr_err("Unexpected state %d on type %d\n",
+				sk->state, sk->type);
 			return 0;
+		}
 		break;
 	default:
 		pr_err("Unknown state %d\n", sk->state);
