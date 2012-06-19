d25f5348ae421b1ce0d2f852b6e1da6315c048f3
Evgeny Antyshev <eantyshev@parallels.com>
2012-06-19 18:38:00 +0400

    zdtm: uptime_grow test
    new testcase important for rebootless update testing:
    we query system timer CLOCK_MONOTONIC continuously
    and fail in case it stepped backwards
    
    Signed-off-by: Evgeny Antyshev <eantyshev@parallels.com>
    Acked-by: Andrew Vagin <avagin@parallels.com>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

---
+++ test/zdtm/live/static/uptime_grow.c
@@ -0,0 +1,42 @@
+#include "zdtmtst.h"
+
+const char *test_doc    = "test to ensure that monotonic clock doesn't decrease";
+const char *test_author = "Evgeny Antysev <eantyshev@parallels.com>";
+
+#include <time.h>
+#include <stdlib.h>
+
+# define tv_gt(a, b) \
+  (((a)->tv_sec == (b)->tv_sec) ? \
+   ((a)->tv_nsec > (b)->tv_nsec) : \
+   ((a)->tv_sec > (b)->tv_sec))
+ 
+int main(int argc, char **argv)
+{
+	struct timespec tm_old, tm;
+	double diff_nsec;
+	test_init(argc, argv);
+
+	if (clock_gettime(CLOCK_MONOTONIC, &tm_old)) {
+		err("clock_gettime failed: %m\n");
+		exit(1);
+	}
+
+	test_daemon();
+
+	while (test_go()) {
+		if (clock_gettime(CLOCK_MONOTONIC, &tm)) {
+			err("clock_gettime failed: %m\n");
+			exit(1);
+		}
+		if (!tv_gt(&tm, &tm_old)) {
+			diff_nsec = (tm_old.tv_sec - tm.tv_sec) * 1.0E9 +\
+				(tm_old.tv_nsec - tm.tv_nsec);
+			fail("clock step backward for %e nsec\n", diff_nsec);
+			exit(1);
+		}
+		tm_old = tm;
+	}
+	pass();
+	return 0;
+}
