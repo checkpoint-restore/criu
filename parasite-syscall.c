97a08a801a521b8af27682b6284337f137a5bfa3
Andrey Vagin <avagin@openvz.org>
2012-06-26 20:00:00 +0400

    parasite: return exit code from parasite
    Signed-off-by: Andrey Vagin <avagin@openvz.org>
    Acked-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- parasite-syscall.c
+++ parasite-syscall.c
@@ -240,6 +240,8 @@ static int parasite_execute_by_pid(unsigned long cmd, struct parasite_ctl *ctl,
 	parasite_setup_regs(ctl->parasite_ip, &regs);
 
 	ret = __parasite_execute(ctl, pid, &regs);
+	if (ret == 0)
+		ret = (int)regs.ax;
 
 	if (args)
 		memcpy(args, ctl->addr_args, args_size);
