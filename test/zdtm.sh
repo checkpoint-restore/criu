18d8950355cc818858d575e8bb912c398461bfcb
Cyrill Gorcunov <gorcunov@openvz.org>
2012-07-01 21:23:00 +0400

    zdtm: Enable fifo and fifo_wronly tests
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- test/zdtm.sh
+++ test/zdtm.sh
@@ -45,6 +45,8 @@ static/inotify00
 static/unbound_sock
 static/fifo-rowo-pair
 static/fifo-ghost
+static/fifo
+static/fifo_wronly
 "
 # Duplicate list with pidns/ prefix
 TEST_LIST=$TEST_LIST$(echo $TEST_LIST | tr ' ' '\n' | sed 's#^#pidns/#')
