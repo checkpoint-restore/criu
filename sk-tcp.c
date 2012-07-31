4ee52f340392254de0e80d2b040927bd10fb18d4
Pavel Emelyanov <xemul@parallels.com>
2012-07-31 13:57:44 +0400

    tcp: Use sk ino number for opening tcp stream image on restore
    It was accidentally broken by 424a4adb6. It's better to use sk ino
    instead of sk id, since tcp connection may be unbound from any fds
    (not supported now) and thus there may be no ID for those.
    
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- sk-tcp.c
+++ sk-tcp.c
@@ -441,7 +441,7 @@ static int restore_tcp_conn_state(int sk, struct inet_sk_info *ii)
 
 	pr_info("Restoring TCP connection id %x ino %x\n", ii->ie->id, ii->ie->ino);
 
-	ifd = open_image_ro(CR_FD_TCP_STREAM, ii->ie->id);
+	ifd = open_image_ro(CR_FD_TCP_STREAM, ii->ie->ino);
 	if (ifd < 0)
 		goto err;
 
