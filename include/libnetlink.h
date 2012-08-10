52b2ca86c984e9846b5268c28167562fba353cbe
Pavel Emelyanov <xemul@parallels.com>
2012-08-10 17:21:34 +0400

    libnetlink: More helpers
    Add more helpers that manage nla blobs (taken from iproute2 tool). Need for veth support.
    
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- include/libnetlink.h
+++ include/libnetlink.h
@@ -4,7 +4,16 @@
 #define CR_NLMSG_SEQ		24680	/* arbitrary chosen */
 
 extern int parse_rtattr(struct rtattr *tb[], int max, struct rtattr *rta, int len);
+#define parse_rtattr_nested(tb, max, rta) \
+	(parse_rtattr((tb), (max), RTA_DATA(rta), RTA_PAYLOAD(rta)))
 extern int do_rtnl_req(int nl, void *req, int size,
 		int (*receive_callback)(struct nlmsghdr *h, void *), void *);
+
+extern int addattr_l(struct nlmsghdr *n, int maxlen, int type,
+		const void *data, int alen);
+
+#define NLMSG_TAIL(nmsg) \
+	((struct rtattr *) (((void *) (nmsg)) + NLMSG_ALIGN((nmsg)->nlmsg_len)))
+
 
 #endif /* LIBNETLINK_H__ */
--- libnetlink.c
+++ libnetlink.c
@@ -113,3 +113,21 @@ err:
 	return -1;
 }
 
+int addattr_l(struct nlmsghdr *n, int maxlen, int type, const void *data,
+		int alen)
+{
+	int len = RTA_LENGTH(alen);
+	struct rtattr *rta;
+
+	if (NLMSG_ALIGN(n->nlmsg_len) + RTA_ALIGN(len) > maxlen) {
+		pr_err("addattr_l ERROR: message exceeded bound of %d\n",maxlen);
+		return -1;
+	}
+
+	rta = NLMSG_TAIL(n);
+	rta->rta_type = type;
+	rta->rta_len = len;
+	memcpy(RTA_DATA(rta), data, alen);
+	n->nlmsg_len = NLMSG_ALIGN(n->nlmsg_len) + RTA_ALIGN(len);
+	return 0;
+}
