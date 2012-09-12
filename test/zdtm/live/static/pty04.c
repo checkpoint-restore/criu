889572990607d61b05f46dc8ffb0200ad8826ca5
Cyrill Gorcunov <gorcunov@openvz.org>
2012-09-12 20:02:53 +0400

    zdtm: Add pty04 test
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

---
+++ test/zdtm/live/static/pty04.c
@@ -0,0 +1,64 @@
+#define _XOPEN_SOURCE
+#include <stdlib.h>
+#include "zdtmtst.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <termios.h>
+#include <signal.h>
+#include <sys/ioctl.h>
+
+const char *test_doc	= "Check two pts with a fake ptmx";
+const char *test_author	= "Cyrill Gorcunov <gorcunov@openvz.org>";
+
+int main(int argc, char *argv[])
+{
+	int master, slave1, slave2;
+	char *slavename;
+
+	test_init(argc, argv);
+
+	master = open("/dev/ptmx", O_RDWR);
+	if (master == -1) {
+		err("open(%s) failed", "/dev/ptmx");
+		return 1;
+	}
+
+	grantpt(master);
+	unlockpt(master);
+
+	slavename = ptsname(master);
+
+	slave1 = open(slavename, O_RDWR);
+	if (slave1 == -1) {
+		err("open(%s) failed", slavename);
+		return 1;
+	}
+
+	slave2 = open(slavename, O_RDWR);
+	if (slave2 == -1) {
+		err("open(%s) failed", slavename);
+		return 1;
+	}
+
+	if (ioctl(slave1, TIOCSCTTY, 1)) {
+		err("Can't set a controll terminal");
+		return 1;
+	}
+
+	test_msg("Closing master\n");
+	signal(SIGHUP, SIG_IGN);
+	close(master);
+
+	test_daemon();
+	test_waitsig();
+
+	close(slave1);
+	close(slave2);
+
+	pass();
+
+	return 0;
+}
