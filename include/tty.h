1838bee1be4b864c0c0e307f74a3b611d14cc371
Pavel Emelyanov <xemul@parallels.com>
2012-09-12 20:07:34 +0400

    tty: Remove unused constants from header
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- include/tty.h
+++ include/tty.h
@@ -13,9 +13,6 @@
 #endif
 #define PTS_FMT		"/dev/pts/%d"
 
-#define TTY_LOCKED	(1 << 0)
-#define TTY_EXCLUSIVE	(1 << 1)
-
 extern int dump_tty(struct fd_parms *p, int lfd, const struct cr_fdset *set);
 extern int collect_tty(void);
 extern int tty_is_master(struct fdinfo_list_entry *le);
