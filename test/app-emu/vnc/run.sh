281f0c7d102a1aa8977beb5540f1f748f042ea4b
Andrey Vagin <avagin@openvz.org>
2012-11-22 18:56:48 +0400

    test/app-emu: dump/restore of VNC server
    * Execute VNC server and glxgears
    * Dump/restore
    * Check, that VNC server accepts connections
    
    Signed-off-by: Andrey Vagin <avagin@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

---
+++ test/app-emu/vnc/run.sh
@@ -0,0 +1,28 @@
+set -m
+
+crtools="../../../crtools"
+
+mkdir data
+
+./vnc-server.sh 25 &> data/vnc.log
+pid=`jobs -p %1`
+bg
+
+$crtools dump -j --tcp-established -D data/ -o dump.log -v 4 -t $pid || {
+	echo "Dump failed"
+	exit 1
+}
+
+$crtools restore -j --tcp-established -D data/ -d -o restore.log -v 4 -t $pid || {
+	echo "Restore failed"
+	exit 1
+}
+
+nc -w 1 localhost 5925 | grep -am 1 RFB
+ret=$?
+
+kill $pid
+
+[ "$ret" -eq 0 ] && echo PASS || echo FAIL;
+
+exit $ret
