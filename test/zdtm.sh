6089f8c5a50451c5d8596f5dd04741e143e5ed4e
Cyrill Gorcunov <gorcunov@openvz.org>
2012-12-21 17:35:50 +0400

    zdtm: Enable mmx00/sse00/sse20 tests
    Since we supprt FPU c/r now we can run
    these tests.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- test/zdtm.sh
+++ test/zdtm.sh
@@ -74,6 +74,9 @@ static/child_opened_proc
 static/cow01
 static/fpu00
 static/fpu01
+static/mmx00
+static/sse00
+static/sse20
 "
 # Duplicate list with ns/ prefix
 TEST_LIST=$TEST_LIST$(echo $TEST_LIST | tr ' ' '\n' | sed 's#^#ns/#')
