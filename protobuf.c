c9797cfa27deac64d053f9d1a436d4e763d3434a
Qiang Huang <h.huangqiang@huawei.com>
2013-01-17 16:09:28 +0800

    protobuf: add a new description for saving file-lock
    We need a new protobuf description for file-lock.
    
    Originally-signed-off-by: Zheng Gu <cengku.gu@huawei.com>
    Signed-off-by: Qiang Huang <h.huangqiang@huawei.com>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- protobuf.c
+++ protobuf.c
@@ -49,6 +49,7 @@
 #include "protobuf/netdev.pb-c.h"
 #include "protobuf/tcp-stream.pb-c.h"
 #include "protobuf/tty.pb-c.h"
+#include "protobuf/file-lock.pb-c.h"
 #include "protobuf/rlimit.pb-c.h"
 
 typedef size_t (*pb_getpksize_t)(void *obj);
@@ -129,6 +130,7 @@ void cr_pb_init(void)
 	CR_PB_DESC(PACKETSK,		PacketSock,	packet_sock);
 	CR_PB_DESC(TTY,			TtyFile,	tty_file);
 	CR_PB_DESC(TTY_INFO,		TtyInfo,	tty_info);
+	CR_PB_DESC(FILE_LOCK,		FileLock,	file_lock);
 	CR_PB_DESC(RLIMIT,		Rlimit,		rlimit);
 }
 
