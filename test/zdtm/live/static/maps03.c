7a74043c592827ea3fe4ad1530fb39d72f31eee1
Cyrill Gorcunov <gorcunov@openvz.org>
2013-02-06 21:20:23 +0400

    zdtm: Add mmap03 test case
    To be able to test it one need indeed enough
    physical memory on the node (at least 16G).
    Thus I don't include this test for regular
    passes.
    
    Inspired by commit 40dcaf885eaf3f2f51d08ecb6c18c3f948655c6e
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

---
+++ test/zdtm/live/static/maps03.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include "zdtmtst.h"
+
+const char *test_doc	= "Test for huge VMA area";
+const char *test_author	= "Cyrill Gorcunov <gorcunov@openvz.org>";
+
+int main(int argc, char **argv)
+{
+	test_init(argc, argv);
+	unsigned char *mem;
+
+	test_msg("Alloc huge VMA\n");
+	mem = (void *)mmap(NULL, (10 << 30), PROT_READ | PROT_WRITE,
+			   MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if ((void *)mem == MAP_FAILED) {
+		err("mmap failed: %m");
+		return -1;
+	}
+
+	mem[4 << 30] = 1;
+	mem[8 << 30] = 2;
+
+	test_daemon();
+	test_waitsig();
+
+	test_msg("Testing restored data\n");
+
+	if (mem[4 << 30] != 1 || mem[8 << 30] != 2) {
+		fail("Data corrupted!\n");
+		exit(1);
+	}
+
+	pass();
+
+	return 0;
+}
