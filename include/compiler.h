75e1fb5c3a35770741e825b6081d76ed2ed6daa9
Cyrill Gorcunov <gorcunov@openvz.org>
2013-02-15 17:34:38 +0400

    include/compiler.h: Cleanup
    - Drop parasite \Space in NORETURN definition
    - Align likely/unlikely
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- include/compiler.h
+++ include/compiler.h
@@ -13,7 +13,7 @@
 #define __stringify_1(x...)	#x
 #define __stringify(x...)	__stringify_1(x)
 
-#define NORETURN 		__attribute__((__noreturn__))
+#define NORETURN		__attribute__((__noreturn__))
 #define __packed		__attribute__((__packed__))
 #define __used			__attribute__((__used__))
 #define __maybe_unused		__attribute__((unused))
@@ -25,8 +25,8 @@
 # define __always_inline	inline __attribute__((always_inline))
 #endif
 
-#define likely(x)	__builtin_expect(!!(x), 1)
-#define unlikely(x)	__builtin_expect(!!(x), 0)
+#define likely(x)		__builtin_expect(!!(x), 1)
+#define unlikely(x)		__builtin_expect(!!(x), 0)
 
 #ifndef always_inline
 # define always_inline		__always_inline
