d274025e425e8e03064c0fa874860621e41642ab
Andrey Vagin <avagin@openvz.org>
2013-03-27 23:51:02 +0400

    util: encode and decode errno into pointer
    Signed-off-by: Andrey Vagin <avagin@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- include/util.h
+++ include/util.h
@@ -305,4 +305,29 @@ static inline bool dir_dots(struct dirent *de)
 
 extern int read_fd_link(int lfd, char *buf, size_t size);
 
+
+#define MAX_ERRNO	4095
+
+#define IS_ERR_VALUE(x) unlikely((x) >= (unsigned long)-MAX_ERRNO)
+
+static inline void * ERR_PTR(long error)
+{
+	return (void *) error;
+}
+
+static inline long PTR_ERR(const void *ptr)
+{
+	return (long) ptr;
+}
+
+static inline long IS_ERR(const void *ptr)
+{
+	return IS_ERR_VALUE((unsigned long)ptr);
+}
+
+static inline long IS_ERR_OR_NULL(const void *ptr)
+{
+	return !ptr || IS_ERR_VALUE((unsigned long)ptr);
+}
+
 #endif /* __CR_UTIL_H__ */
