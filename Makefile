checkpoint-restore/criu

commit a519e0c824a92fb494ec979b7f86ee94aed55004

make: insert Makefile.inc into Makefile
Makefile.inc is included only into Makefile now. So let's insert it's content
into Makefile itself.

Signed-off-by: Stanislav Kinsbursky <skinsbursky@openvz.org>
Looks-good-to: Cyrill Gorcunov <gorcunov@openvz.org>
Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

Makefile
@@ -1,4 +1,78 @@
-include Makefile.inc
+#
+# Common definitions
+#
+
+ifeq ($(strip $(V)),)
+	E = @echo
+	Q = @
+else
+	E = @\#
+	Q =
+endif
+
+FIND		:= find
+CSCOPE		:= cscope
+TAGS		:= ctags
+RM		:= rm
+LD		:= ld
+HEXDUMP		:= hexdump
+CC		:= gcc
+ECHO		:= echo
+NM		:= nm
+AWK		:= awk
+SH		:= sh
+MAKE		:= make
+
+# Additional ARCH settings for x86
+ARCH ?= $(shell echo $(uname_M) | sed -e s/i.86/i386/ -e s/sun4u/sparc64/ \
+                  -e s/arm.*/arm/ -e s/sa110/arm/ \
+                  -e s/s390x/s390/ -e s/parisc64/parisc/ \
+                  -e s/ppc.*/powerpc/ -e s/mips.*/mips/ \
+                  -e s/sh[234].*/sh/ )
+
+uname_M      := $(shell uname -m | sed -e s/i.86/i386/)
+ifeq ($(uname_M),i386)
+	ARCH         := x86
+	DEFINES      := -DCONFIG_X86_32
+endif
+ifeq ($(uname_M),x86_64)
+	ARCH         := x86
+	DEFINES      := -DCONFIG_X86_64
+endif
+
+SRC_DIR		?= $(shell pwd)
+
+CFLAGS		= -I$(SRC_DIR)/include -fno-strict-aliasing
+
+LIBS		:= -lrt -lpthread -lprotobuf-c
+
+DEFINES		+= -D_FILE_OFFSET_BITS=64
+DEFINES		+= -D_GNU_SOURCE
+
+WARNINGS	:= -Wall
+
+ifneq ($(WERROR),0)
+	WARNINGS += -Werror
+endif
+
+ifeq ($(DEBUG),1)
+	DEFINES += -DCR_DEBUG
+endif
+
+ifeq ($(DEBUG),1)
+	DEFINES += -DCR_DEBUG
+	CFLAGS	+= -O0 -ggdb3
+else
+	CFLAGS	+= -O2
+endif
+
+CFLAGS		+= $(WARNINGS) $(DEFINES)
+MAKEFLAGS	:= --no-print-directory
+SYSCALL-LIB	= $(SRC_DIR)/arch/$(ARCH)/syscalls.o
+PROTOBUF-LIB	= $(SRC_DIR)/protobuf/protobuf-lib.o
+
+export E Q CC ECHO MAKE CFLAGS LIBS ARCH DEFINES MAKEFLAGS SRC_DIR SYSCALL-LIB SH
+
 
 PROGRAM		:= crtools
 

Makefile.inc
@@ -1,75 +1,0 @@
-#
-# Common definitions
-#
-
-ifeq ($(strip $(V)),)
-	E = @echo
-	Q = @
-else
-	E = @\#
-	Q =
-endif
-
-FIND		:= find
-CSCOPE		:= cscope
-TAGS		:= ctags
-RM		:= rm
-LD		:= ld
-HEXDUMP		:= hexdump
-CC		:= gcc
-ECHO		:= echo
-NM		:= nm
-AWK		:= awk
-SH		:= sh
-MAKE		:= make
-
-# Additional ARCH settings for x86
-ARCH ?= $(shell echo $(uname_M) | sed -e s/i.86/i386/ -e s/sun4u/sparc64/ \
-                  -e s/arm.*/arm/ -e s/sa110/arm/ \
-                  -e s/s390x/s390/ -e s/parisc64/parisc/ \
-                  -e s/ppc.*/powerpc/ -e s/mips.*/mips/ \
-                  -e s/sh[234].*/sh/ )
-
-uname_M      := $(shell uname -m | sed -e s/i.86/i386/)
-ifeq ($(uname_M),i386)
-	ARCH         := x86
-	DEFINES      := -DCONFIG_X86_32
-endif
-ifeq ($(uname_M),x86_64)
-	ARCH         := x86
-	DEFINES      := -DCONFIG_X86_64
-endif
-
-SRC_DIR		?= $(shell pwd)
-
-CFLAGS		= -I$(SRC_DIR)/include -fno-strict-aliasing
-
-LIBS		:= -lrt -lpthread -lprotobuf-c
-
-DEFINES		+= -D_FILE_OFFSET_BITS=64
-DEFINES		+= -D_GNU_SOURCE
-
-WARNINGS	:= -Wall
-
-ifneq ($(WERROR),0)
-	WARNINGS += -Werror
-endif
-
-ifeq ($(DEBUG),1)
-	DEFINES += -DCR_DEBUG
-endif
-
-ifeq ($(DEBUG),1)
-	DEFINES += -DCR_DEBUG
-	CFLAGS	+= -O0 -ggdb3
-else
-	CFLAGS	+= -O2
-endif
-
-CFLAGS		+= $(WARNINGS) $(DEFINES)
-MAKEFLAGS	:= --no-print-directory
-SYSCALL-LIB	= $(SRC_DIR)/arch/$(ARCH)/syscalls.o
-PROTOBUF-LIB	= $(SRC_DIR)/protobuf/protobuf-lib.o
-
-export E Q CC ECHO MAKE CFLAGS LIBS ARCH DEFINES MAKEFLAGS SRC_DIR SYSCALL-LIB SH
-

commit 97802b0d5589d91526d2dd07166f0b02be7f9fbd

Makefile.inc: restore
This reverts part of commit a519e0c which broke the man page build:

	[kir@kirpad Documentation]$ make
	"  GEN     " crtools.1
	/bin/sh:   GEN     : command not found
	make: *** [crtools.1] Error 127

So let's reintroduce Q and E in a separate Makefile.inc as it was
before.

Cc: Stanislav Kinsbursky <skinsbursky@openvz.org>
Cc: Cyrill Gorcunov <gorcunov@openvz.org>
Cc: Pavel Emelyanov <xemul@parallels.com>
Signed-off-by: Kir Kolyshkin <kir@openvz.org>
Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

Makefile
@@ -16,16 +16,11 @@ export VERSION_MAJOR VERSION_MINOR VERSION_SUBLEVEL VERSION_EXTRA VERSION_NAME
 #
 #MAKEFLAGS := -r -R --no-print-directory
 
+include Makefile.inc
+
 #
 # Common definitions
 #
-ifeq ($(strip $(V)),)
-	E = @echo
-	Q = @
-else
-	E = @\#
-	Q =
-endif
 
 FIND		:= find
 CSCOPE		:= cscope
@@ -103,7 +98,7 @@ CFLAGS		+= $(WARNINGS) $(DEFINES)
 SYSCALL-LIB	:= arch/$(ARCH)/syscalls.built-in.o
 ARCH-LIB	:= arch/$(ARCH)/crtools.built-in.o
 
-export E Q CC ECHO MAKE CFLAGS LIBS ARCH DEFINES MAKEFLAGS
+export CC ECHO MAKE CFLAGS LIBS ARCH DEFINES MAKEFLAGS
 export SRC_DIR SYSCALL-LIB SH RM ARCH_DIR OBJCOPY LDARCH LD
 export cflags-y
 

Makefile.inc
@@ -1,0 +1,11 @@
+# Silent make rules
+
+ifeq ($(strip $(V)),)
+	E = @echo
+	Q = @
+else
+	E = @\#
+	Q =
+endif
+
+export E Q