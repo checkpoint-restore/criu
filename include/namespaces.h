c459e417f3f02e933d6b1073ac4611e036d1c07e
Cyrill Gorcunov <gorcunov@openvz.org>
2013-05-15 00:37:44 +0400

    ns: Beautify namespaces.h
    I'll need to modify this header so before anything
    else lets beautify it
    
     - drop struct pstree_item declaration it's already in pstree.h
     - move struct cr_options to top
     - align members of struct ns_desc
     - move externs to top
     - add argument name to try_show_namespaces
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- include/namespaces.h
+++ include/namespaces.h
@@ -4,23 +4,23 @@
 #include "crtools.h"
 #include "pstree.h"
 
-int dump_namespaces(struct pid *pid, unsigned int ns_flags);
-int prepare_namespace(int pid, unsigned long clone_flags);
 struct cr_options;
-int try_show_namespaces(int pid, struct cr_options *);
 
 struct ns_desc {
-	unsigned int cflag;
-	char *str;
+	unsigned int	cflag;
+	char		*str;
 };
+
+extern struct ns_desc pid_ns_desc;
+extern unsigned long current_ns_mask;
+
+int dump_namespaces(struct pid *pid, unsigned int ns_flags);
+int prepare_namespace(int pid, unsigned long clone_flags);
+int try_show_namespaces(int pid, struct cr_options *o);
 
 int switch_ns(int pid, struct ns_desc *nd, int *rst);
 int restore_ns(int rst, struct ns_desc *nd);
-extern struct ns_desc pid_ns_desc;
 
-struct pstree_item;
 int dump_task_ns_ids(struct pstree_item *);
-
-extern unsigned long current_ns_mask;
 
 #endif /* __CR_NS_H__ */
