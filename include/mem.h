52509a20c7aa70366994c9607b36866bc619b059
Cyrill Gorcunov <gorcunov@openvz.org>
2013-05-22 23:09:39 +0400

    mem: Beautify mem.h
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- include/mem.h
+++ include/mem.h
@@ -1,10 +1,14 @@
 #ifndef __CR_MEM_H__
 #define __CR_MEM_H__
-struct vm_area_list;
-int do_task_reset_dirty_track(int pid);
-unsigned int dump_pages_args_size(struct vm_area_list *vmas);
+
 struct parasite_ctl;
+struct vm_area_list;
 struct page_pipe;
-int parasite_dump_pages_seized(struct parasite_ctl *ctl,
-		struct vm_area_list *vma_area_list, struct page_pipe **pp);
-#endif
+
+extern int do_task_reset_dirty_track(int pid);
+extern unsigned int dump_pages_args_size(struct vm_area_list *vmas);
+extern int parasite_dump_pages_seized(struct parasite_ctl *ctl,
+				      struct vm_area_list *vma_area_list,
+				      struct page_pipe **pp);
+
+#endif /* __CR_MEM_H__ */
