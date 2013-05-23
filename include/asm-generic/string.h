a21cdb843767cc21a1b371105883c735784c8793
Cyrill Gorcunov <gorcunov@openvz.org>
2013-05-23 16:41:42 +0400

    asm: Add builtin_memcpy, builtin_memcmp generic helpers
    Will need them in pie code soon.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- include/asm-generic/string.h
+++ include/asm-generic/string.h
@@ -18,4 +18,34 @@ static always_inline void *builtin_memcpy(void *to, const void *from, unsigned i
 }
 #endif
 
+#ifndef HAS_BUILTIN_MEMCMP
+static always_inline int builtin_memcmp(const void *cs, const void *ct, size_t count)
+{
+	const unsigned char *su1, *su2;
+	int res = 0;
+
+	for (su1 = cs, su2 = ct; 0 < count; ++su1, ++su2, count--)
+		if ((res = *su1 - *su2) != 0)
+			break;
+	return res;
+}
+#endif
+
+#ifndef HAS_BUILTIN_STRCMP
+static always_inline int builtin_strcmp(const char *cs, const char *ct)
+{
+	unsigned char c1, c2;
+
+	while (1) {
+		c1 = *cs++;
+		c2 = *ct++;
+		if (c1 != c2)
+			return c1 < c2 ? -1 : 1;
+		if (!c1)
+			break;
+	}
+	return 0;
+}
+#endif
+
 #endif /* __CR_ASM_GENERIC_STRING_H__ */
