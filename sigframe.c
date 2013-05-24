45233b5ec97e6cf85b3a701186973db8d36570ae
Andrey Vagin <avagin@openvz.org>
2013-05-24 16:20:17 +0400

    restore: add a function for constructing sigframe
    Signed-off-by: Andrey Vagin <avagin@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

---
+++ sigframe.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+
+#include "restorer.h"
+#include "asm/restore.h"
+#include "asm/restorer.h"
+
+#include "protobuf/core.pb-c.h"
+
+static inline int construct_sigframe(struct rt_sigframe *sigframe,
+				     struct rt_sigframe *rsigframe,
+				     CoreEntry *core)
+{
+	k_rtsigset_t *blk_sigset = &RT_SIGFRAME_UC(sigframe).uc_sigmask;
+
+	if (core->tc)
+		memcpy(blk_sigset, &core->tc->blk_sigset, sizeof(k_rtsigset_t));
+	else if (core->thread_core->has_blk_sigset) {
+		memcpy(blk_sigset,
+			&core->thread_core->blk_sigset, sizeof(k_rtsigset_t));
+	} else
+		memset(blk_sigset, 0, sizeof(k_rtsigset_t));
+
+	if (sigreturn_prep_fpu_frame(&sigframe->fpu_state, core))
+		return -1;
+
+	if (restore_fpu(sigframe, &rsigframe->fpu_state))
+		return -1;
+
+	if (restore_gpregs(sigframe, CORE_THREAD_ARCH_INFO(core)->gpregs))
+		return -1;
+
+	return 0;
+}
