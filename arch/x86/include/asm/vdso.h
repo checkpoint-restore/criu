5e5b82154d0fccaebc50ffcfaa7baecb452bc687
Cyrill Gorcunov <gorcunov@openvz.org>
2013-05-31 10:17:51 +0400

    vdso: Make parasite_fixup_vdso being arch implemented
    parasite_fixup_vdso is not worth calling on arm at all,
    moreover it might try to read pfn for addresses greater
    than TASK_SIZE, leading to dump interruption. Lets make
    it arch dependent.
    
    [alekskartashov@: Fix compiling on arm]
    
    Reported-by: Alexander Kartashov <alekskartashov@parallels.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Tested-by: Alexander Kartashov <alekskartashov@parallels.com>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- arch/x86/include/asm/vdso.h
+++ arch/x86/include/asm/vdso.h
@@ -6,10 +6,14 @@
 #include "protobuf/vma.pb-c.h"
 
 struct vdso_symtable;
+struct parasite_ctl;
+struct vm_area_list;
 
 extern int vdso_redirect_calls(void *base_to, void *base_from, struct vdso_symtable *to, struct vdso_symtable *from);
 extern int vdso_fill_symtable(char *mem, size_t size,struct vdso_symtable *t);
 extern int vdso_remap(char *who, unsigned long from, unsigned long to, size_t size);
 extern int vdso_proxify(char *who, struct vdso_symtable *sym_rt, VmaEntry *vma_entry, unsigned long vdso_rt_parked_at);
+extern int parasite_fixup_vdso(struct parasite_ctl *ctl, pid_t pid,
+			       struct vm_area_list *vma_area_list);
 
 #endif /* __CR_ASM_VDSO_H__ */
