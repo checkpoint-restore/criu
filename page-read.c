c97911a5f208231e6ca63bab7aa3207623722546
Andrey Vagin <avagin@openvz.org>
2013-06-22 13:15:27 +0400

    mm: initialize PageEntry pe to NULL in PagemapEntry
    Otherwise it contains trash and BUG at page-read.c:98 may be triggered.
    
    https://bugzilla.openvz.org/show_bug.cgi?id=2633
    
    Signed-off-by: Andrey Vagin <avagin@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- page-read.c
+++ page-read.c
@@ -179,6 +179,8 @@ err_cl:
 
 static int open_page_read_at(int dfd, int pid, struct page_read *pr)
 {
+	pr->pe = NULL;
+
 	pr->fd = open_image_at(dfd, CR_FD_PAGEMAP, O_RSTR, (long)pid);
 	if (pr->fd < 0) {
 		pr->fd_pg = open_image_at(dfd, CR_FD_PAGES_OLD, O_RSTR, pid);
