2508d051ac2d0a82106166a6038d1a0831dd46ba
Andrey Vagin <avagin@openvz.org>
2013-07-24 19:10:49 +0400

    zdtm: the post-dump scripts returns zero, if action is not post-dump
    All scripts are executed for all actions, so if one of them failed for
    any actions, crtools returns with non-zero code.
    E.g: ./zdtm.sh -d ns/static/env00
    
    Signed-off-by: Andrey Vagin <avagin@openvz.org>
    Acked-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- test/post-dump.sh
+++ test/post-dump.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-[ $CRTOOLS_SCRIPT_ACTION == post-dump ] && {
-	#
-	# Special code to inform zdtm that we're
-	# done and should proceed testing treating
-	# non-zero return as known case.
-        exit 32
-}
+[ "$CRTOOLS_SCRIPT_ACTION" == post-dump ] || exit 0
+
+#
+# Special code to inform zdtm that we're
+# done and should proceed testing treating
+# non-zero return as known case.
+exit 32
