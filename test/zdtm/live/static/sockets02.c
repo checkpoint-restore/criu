f6654dc41196afc218aebc7d8b1392035ab585f1
Ruslan Kuprieiev <kupruser@gmail.com>
2013-08-28 16:52:45 +0400

    v4 zdtm: test semi-closed unix stream connection for data loss
    Signed-off-by: Ruslan Kuprieiev <kupruser@gmail.com>
    Acked-by: Andrew Vagin <avagin@parallels.com>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- test/zdtm/live/static/sockets02.c
+++ test/zdtm/live/static/sockets02.c
@@ -21,12 +21,19 @@ const char *test_author	= "Pavel Emelyanov <xemul@parallels.com>\n";
 int main(int argc, char *argv[])
 {
 	int ssk_pair[2], ret;
-	char aux;
+	char aux, data;
 
 	test_init(argc, argv);
 
+	data = (char)lrand48();
+
 	if (socketpair(AF_UNIX, SOCK_STREAM, 0, ssk_pair) == -1) {
 		fail("socketpair\n");
+		exit(1);
+	}
+
+	if (write(ssk_pair[1], &data, sizeof(data)) != sizeof(data)) {
+		fail("write\n");
 		exit(1);
 	}
 
@@ -34,6 +41,12 @@ int main(int argc, char *argv[])
 
 	test_daemon();
 	test_waitsig();
+
+	ret = read(ssk_pair[0], &aux, sizeof(aux));
+	if (ret != sizeof(data) && aux != data) {
+		fail("Data loss (write %d, read %d)", data, aux);
+		return 1;
+	}
 
 	errno = 0;
 	ret = read(ssk_pair[0], &aux, sizeof(aux));
