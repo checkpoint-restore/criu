70bb57e20a081d280d9acc989f783c8b0532b159
Pavel Emelyanov <xemul@parallels.com>
2013-12-26 17:00:38 +0400

    restore: Run setup-ns scripts before restoring them
    We should call external scripts when namespaces are created,
    but before we try to fill them with data from images.
    
    This is done so e.g. to make it possible to push external net
    links to netns.
    
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- cr-restore.c
+++ cr-restore.c
@@ -1263,6 +1263,9 @@ static int restore_task_with_children(void *_arg)
 
 	/* Restore root task */
 	if (current->parent == NULL) {
+		if (restore_finish_stage(CR_STATE_RESTORE_NS) < 0)
+			exit(1);
+
 		if (collect_mount_info(getpid()))
 			exit(1);
 
@@ -1275,9 +1278,6 @@ static int restore_task_with_children(void *_arg)
 		 * Thus -- mount proc at custom location for any new namespace
 		 */
 		if (mount_proc())
-			exit(1);
-
-		if (restore_finish_stage(CR_STATE_RESTORE_NS) < 0)
 			exit(1);
 
 		if (root_prepare_shared())
