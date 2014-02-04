checkpoint-restore/criu

commit 3929c853dd894fde9de3e01aeedf2a77a742499b

page-xfer: print error message if splice failed
Signed-off-by: Andrey Vagin <avagin@openvz.org>
Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

page-xfer.c
@@ -473,8 +473,16 @@ static int write_pagemap_loc(struct page_xfer *xfer,
 static int write_pages_loc(struct page_xfer *xfer,
 		int p, unsigned long len)
 {
-	if (splice(p, NULL, xfer->fd_pg, NULL, len, SPLICE_F_MOVE) != len)
+	ssize_t ret;
+	ret = splice(p, NULL, xfer->fd_pg, NULL, len, SPLICE_F_MOVE);
+	if (ret == -1) {
+		pr_perror("Unable to spice data");
 		return -1;
+	}
+	if (ret != len) {
+		pr_err("Only %lu of %lu bytes have been spliced\n", ret, len);
+		return -1;
+	}
 
 	return 0;
 }

commit 9ff3f30b75d1b661194f6ef6c763c27d0299740b

page-xfer: Fix 32-bit compilation
Broken by 3929c853

Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

page-xfer.c
@@ -480,7 +480,7 @@ static int write_pages_loc(struct page_xfer *xfer,
 		return -1;
 	}
 	if (ret != len) {
-		pr_err("Only %lu of %lu bytes have been spliced\n", ret, len);
+		pr_err("Only %zu of %lu bytes have been spliced\n", ret, len);
 		return -1;
 	}
 