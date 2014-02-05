1e837af193e832d702e6bf66084baac2a483d93d
Pavel Emelyanov <xemul@parallels.com>
2014-02-05 19:40:47 +0400

    scripts: Add script to abort restore at the end
    Usefult to test restore time, for example.
    
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

---
+++ scripts/fake-restore.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# A stupid script to abort restore at the very end. Useful to test
+# restore w/o letting the restored processes continue running. E.g.
+# can be used to measure the restore time.
+#
+# Usage:
+# criu restore <options> --action-script $(pwd)/scripts/fake-restore.sh
+#
+if [ "$CRTOOLS_SCRIPT_ACTION" == "post-restore" ]; then
+	exit 1
+else
+	exit 0
+fi
