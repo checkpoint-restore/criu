6e95f544de56d1361e26fece406cd9827db29d87
Cyrill Gorcunov <gorcunov@openvz.org>
2014-02-26 13:44:13 +0400

    headers: Add fs-magic.h
    Not all distros provide magic numbers we might need
    during build procedure, thus provide own definitions
    in one known place.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

---
+++ include/fs-magic.h
@@ -0,0 +1,32 @@
+#ifndef __CR_FS_MAGIC_H__
+#define __CR_FS_MAGIC_H__
+
+#include <sys/vfs.h>
+
+/*
+ * Gather magic numbers in case if distros
+ * do not provide appropriate entry in
+ * linux/magic.h.
+ */
+
+#ifndef NFS_SUPER_MAGIC
+# define NFS_SUPER_MAGIC	0x6969
+#endif
+
+#ifndef PIPEFS_MAGIC
+# define PIPEFS_MAGIC		0x50495045
+#endif
+
+#ifndef ANON_INODE_FS_MAGIC
+# define ANON_INODE_FS_MAGIC	0x09041934
+#endif
+
+#ifndef TMPFS_MAGIC
+# define TMPFS_MAGIC		0x01021994
+#endif
+
+#ifndef SOCKFS_MAGIC
+# define SOCKFS_MAGIC		0x534f434b
+#endif
+
+#endif /* __CR_FS_MAGIC_H__ */
