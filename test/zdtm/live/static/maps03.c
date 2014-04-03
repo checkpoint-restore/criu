4848ef660f39c3f5ce463f730222e330427d32e8
Jamie Liu <jamieliu@google.com>
2014-04-03 04:53:00 +0400

    zdtm: fix integer literal type bug in maps03
    maps03 should have caught the bug fixed by 288cf5174165 "restore: mutate
    tgt_addr in map_private_vma", but didn't because integer literals
    (defaulting to 32-bit ints) were shifted out of range.
    
    Signed-off-by: Jamie Liu <jamieliu@google.com>
    Acked-by: Andrew Vagin <avagin@parallels.com>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- test/zdtm/live/static/maps03.c
+++ test/zdtm/live/static/maps03.c
@@ -12,22 +12,22 @@ int main(int argc, char **argv)
 	unsigned char *mem;
 
 	test_msg("Alloc huge VMA\n");
-	mem = (void *)mmap(NULL, (10 << 30), PROT_READ | PROT_WRITE,
+	mem = (void *)mmap(NULL, (10L << 30), PROT_READ | PROT_WRITE,
 			   MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 	if ((void *)mem == MAP_FAILED) {
 		err("mmap failed: %m");
 		return -1;
 	}
 
-	mem[4 << 30] = 1;
-	mem[8 << 30] = 2;
+	mem[4L << 30] = 1;
+	mem[8L << 30] = 2;
 
 	test_daemon();
 	test_waitsig();
 
 	test_msg("Testing restored data\n");
 
-	if (mem[4 << 30] != 1 || mem[8 << 30] != 2) {
+	if (mem[4L << 30] != 1 || mem[8L << 30] != 2) {
 		fail("Data corrupted!\n");
 		exit(1);
 	}
