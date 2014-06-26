checkpoint-restore/criu

commit c473461d24fdfcd25542b427829a37fd2f0facb5

vdso: Make it arch specific
Currently we build vDSO handling code for all archs provided
in the source code having some "common" parts inside pie/vdso.c,
pie/vdso-stub.c, vdso-stub.c and vdso.c. This were more or
less well but in new linux kernels (starting from 3.16 presumably)
the vDSO has been significantly reworked so every architecture
must have own vDSO handling engine (just like the kernel does).

So in this patch we move vDSO code to arch specific and because
aarch64 actually doesn't implement proxification yet due to
kernel restrictions -- we drops it out. When there will be
kernel support we bring it back in proper arch/aarch64
implementation.

Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
Acked-by: Alexander Kartashov <alekskartashov@parallels.com>
Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

Makefile
@@ -35,8 +35,6 @@ OBJCOPY		:= $(CROSS_COMPILE)objcopy
 
 CFLAGS		+= $(USERCFLAGS)
 
-VDSO_O		:= vdso.o
-
 #
 # Fetch ARCH from the uname if not yet set
 #
@@ -73,8 +71,6 @@ ifeq ($(shell echo $(ARCH) | sed -e 's/arm.*/arm/'),arm)
 	ifeq ($(ARMV),7)
 		USERCFLAGS += -march=armv7-a
 	endif
-
-	VDSO_O       := vdso-stub.o
 endif
 
 SRCARCH		?= $(ARCH)
@@ -124,7 +120,6 @@ export CC MAKE CFLAGS LIBS SRCARCH DEFINES MAKEFLAGS CRIU-SO
 export SRC_DIR SYSCALL-LIB SH RM ARCH_DIR OBJCOPY LDARCH LD
 export USERCFLAGS
 export cflags-y
-export VDSO_O
 export VDSO
 
 include Makefile.inc
@@ -176,17 +171,13 @@ lib/%:: $(VERSION_HEADER) config built-in.o
 lib: $(VERSION_HEADER) config built-in.o
 	$(Q) $(MAKE) $(build)=lib all
 
+ifeq ($(VDSO),y)
+PROGRAM-BUILTINS	+= $(ARCH_DIR)/vdso-pie.o
+endif
 PROGRAM-BUILTINS	+= pie/util-fd.o
 PROGRAM-BUILTINS	+= pie/util.o
 PROGRAM-BUILTINS	+= protobuf/built-in.o
 PROGRAM-BUILTINS	+= built-in.o
-
-$(ARCH_DIR)/vdso-pie.o: pie
-	$(Q) $(MAKE) $(build)=pie $(ARCH_DIR)/vdso-pie.o
-PROGRAM-BUILTINS	+= $(ARCH_DIR)/vdso-pie.o
-pie/$(VDSO_O): pie
-	$(Q) $(MAKE) $(build)=pie pie/$(VDSO_O)
-PROGRAM-BUILTINS	+= pie/$(VDSO_O)
 
 $(SYSCALL-LIB) $(ARCH-LIB) $(PROGRAM-BUILTINS): config
 

pie/Makefile
@@ -4,9 +4,10 @@ targets			+= restorer
 obj-y			+= log-simple.o
 obj-y			+= util.o
 obj-y			+= util-fd.o
-obj-y			+= $(VDSO_O)
 
+ifeq ($(VDSO),y)
 obj-e			+= $(ARCH_DIR)/vdso-pie.o
+endif
 
 parasite-obj-y		+= parasite.o
 parasite-asm-e		+= $(ARCH_DIR)/parasite-head.o

Makefile.crtools
@@ -58,11 +58,12 @@ obj-y	+= kerndat.o
 obj-y	+= stats.o
 obj-y	+= string.o
 obj-y	+= sigframe.o
+ifeq ($(VDSO),y)
 obj-y	+= $(ARCH_DIR)/vdso.o
+endif
 obj-y	+= cr-service.o
 obj-y	+= sd-daemon.o
 obj-y	+= plugin.o
-obj-y	+= $(VDSO_O)
 
 ifneq ($(MAKECMDGOALS),clean)
 incdeps := y

arch/aarch64/include/asm/vdso.h
@@ -1,47 +1,0 @@
-#ifndef __CR_ASM_VDSO_H__
-#define __CR_ASM_VDSO_H__
-
-#include <sys/types.h>
-
-#include "protobuf/vma.pb-c.h"
-
-struct vdso_symtable;
-struct parasite_ctl;
-struct vm_area_list;
-
-
-enum {
-	VDSO_SYMBOL_CLOCK_GETRES,
-	VDSO_SYMBOL_CLOCK_GETTIME,
-	VDSO_SYMBOL_GETTIMEOFDAY,
-	VDSO_SYMBOL_RT_SIGRETURN,
-
-	VDSO_SYMBOL_MAX
-};
-
-#define VDSO_SYMBOL_CLOCK_GETRES_NAME	"__kernel_clock_getres"
-#define VDSO_SYMBOL_CLOCK_GETTIME_NAME	"__kernel_clock_gettime"
-#define VDSO_SYMBOL_GETTIMEOFDAY_NAME	"__kernel_gettimeofday"
-#define VDSO_SYMBOL_RT_SIGRETURN_NAME	"__kernel_rt_sigreturn"
-
-
-#define DECLARE_VDSO(ident_name, symtab_name)				\
-									\
-char ident_name[] = {							\
-	0x7f, 0x45, 0x4c, 0x46, 0x02, 0x01, 0x01, 0x00,			\
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,			\
-};									\
-									\
-char *symtab_name[VDSO_SYMBOL_MAX] = {					\
-	[VDSO_SYMBOL_CLOCK_GETRES] = VDSO_SYMBOL_CLOCK_GETRES_NAME,	\
-	[VDSO_SYMBOL_RT_SIGRETURN] = VDSO_SYMBOL_RT_SIGRETURN_NAME,	\
-	[VDSO_SYMBOL_GETTIMEOFDAY] = VDSO_SYMBOL_GETTIMEOFDAY_NAME,	\
-	[VDSO_SYMBOL_CLOCK_GETTIME] = VDSO_SYMBOL_CLOCK_GETTIME_NAME	\
-};
-
-
-extern int vdso_redirect_calls(void *base_to, void *base_from, struct vdso_symtable *to, struct vdso_symtable *from);
-extern int parasite_fixup_vdso(struct parasite_ctl *ctl, pid_t pid,
-			       struct vm_area_list *vma_area_list);
-
-#endif /* __CR_ASM_VDSO_H__ */

arch/aarch64/vdso-pie.c
@@ -1,34 +1,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <elf.h>
-#include <fcntl.h>
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/mman.h>
-
-#include "asm/string.h"
-#include "asm/types.h"
-
-#include "compiler.h"
-#include "syscall.h"
-#include "crtools.h"
-#include "vdso.h"
-#include "vma.h"
-#include "log.h"
-
-#ifdef LOG_PREFIX
-# undef LOG_PREFIX
-#endif
-#define LOG_PREFIX "vdso: "
-
-
-int vdso_redirect_calls(void *base_to, void *base_from,
-			struct vdso_symtable *to,
-			struct vdso_symtable *from)
-{
-	pr_err("vDSO proxy isn't implemented yet");
-	return -1;
-}

arch/aarch64/vdso.c
@@ -1,36 +1,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <elf.h>
-#include <fcntl.h>
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/mman.h>
-
-#include "parasite-syscall.h"
-#include "parasite.h"
-#include "compiler.h"
-#include "kerndat.h"
-#include "vdso.h"
-#include "util.h"
-#include "log.h"
-#include "mem.h"
-#include "vma.h"
-
-#include "asm/types.h"
-#include "asm/parasite-syscall.h"
-
-
-#ifdef LOG_PREFIX
-# undef LOG_PREFIX
-#endif
-#define LOG_PREFIX "vdso: "
-
-
-int parasite_fixup_vdso(struct parasite_ctl *ctl, pid_t pid,
-			struct vm_area_list *vma_area_list)
-{
-	return 0;
-}

arch/arm/include/asm/vdso.h
@@ -1,18 +1,0 @@
-#ifndef __CR_ASM_VDSO_H__
-#define __CR_ASM_VDSO_H__
-
-#include <sys/types.h>
-
-#include "protobuf/vma.pb-c.h"
-
-struct vdso_symtable;
-struct parasite_ctl;
-struct vm_area_list;
-
-#define VDSO_SYMBOL_MAX 1
-
-extern int vdso_redirect_calls(void *base_to, void *base_from, struct vdso_symtable *to, struct vdso_symtable *from);
-extern int parasite_fixup_vdso(struct parasite_ctl *ctl, pid_t pid,
-			       struct vm_area_list *vma_area_list);
-
-#endif /* __CR_ASM_VDSO_H__ */

arch/arm/vdso-pie.c
@@ -1,17 +1,0 @@
-#include <sys/types.h>
-
-#include "vdso.h"
-#include "vma.h"
-#include "log.h"
-
-#ifdef LOG_PREFIX
-# undef LOG_PREFIX
-#endif
-#define LOG_PREFIX "vdso: "
-
-int vdso_redirect_calls(void *base_to, void *base_from,
-			struct vdso_symtable *to,
-			struct vdso_symtable *from)
-{
-	return 0;
-}

commit 2812d04d29d7eaef741eb6c5b5fca3b4de95d107

make: fix "make criu" after arch-specific vdso broke it
Building criu with "make criu" on a clean tree was not working, failing on:

  make[1]: *** No rule to make target `arch/x86/vdso-pie.o'.  Stop.
  make: *** [arch/x86/vdso-pie.o] Error 2

git bisect traced the regression to commit c473461d24fd (vdso: Make it arch
specific) which apparently dropped the rule to build $(ARCH_DIR)/vdso-pie.o
using the pie rule.  Restore the dependency for "make criu" to work again from
a clean tree.

Tested:
$ git clean -fdx
$ make criu

Fixes: c473461d24fdfcd25542b427829a37fd2f0facb5

Signed-off-by: Filipe Brandenburger <filbranden@google.com>
Acked-by: Cyrill Gorcunov <gorcunov@openvz.org>
Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

Makefile
@@ -172,8 +172,11 @@ lib: $(VERSION_HEADER) config built-in.o
 	$(Q) $(MAKE) $(build)=lib all
 
 ifeq ($(VDSO),y)
+$(ARCH_DIR)/vdso-pie.o: pie
+	$(Q) $(MAKE) $(build)=pie $(ARCH_DIR)/vdso-pie.o
 PROGRAM-BUILTINS	+= $(ARCH_DIR)/vdso-pie.o
 endif
+
 PROGRAM-BUILTINS	+= pie/util-fd.o
 PROGRAM-BUILTINS	+= pie/util.o
 PROGRAM-BUILTINS	+= protobuf/built-in.o