828715f9f9c6f811c458186fffb680dbc3ea2972
Andrey Vagin <avagin@openvz.org>
2014-07-07 15:48:00 +0400

    jenkins/btrfs: exclude inotify_irmap
    Cyrill:  we exclude these tests because of braindamaged scheme
             btrfs uses for subvolume device numbering which we don't
    	 yet support in inrmaps.
    
    Test: zdtm/live/static/inotify_irmap, Result: FAIL
    ==================================== ERROR ====================================
    Test: zdtm/live/static/inotify_irmap, Namespace: 1
    Dump log   : /mnt/btrfs/workspace/test/dump/inotify_irmap/16077/1/dump.log
    --------------------------------- grep Error ---------------------------------
    (00.024342) Error (irmap.c:80): irmap: Can't stat /var/spool: No such file or directory
    (00.024365) Error (irmap.c:80): irmap: Can't stat /lib/udev: No such file or directory
    (00.024378) Error (irmap.c:80): irmap: Can't stat /no-such-path: No such file or directory
    (00.024383) Error (fsnotify.c:184): fsnotify: Can't dump that handle
    (00.024397) Error (cr-dump.c:1563): Dump files (pid: 16090) failed with -1
    (00.024779) Error (cr-dump.c:1911): Dumping FAILED.
    ------------------------------------- END -------------------------------------
    ================================= ERROR OVER =================================
    
    Signed-off-by: Andrey Vagin <avagin@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- test/jenkins/criu-btrfs.sh
+++ test/jenkins/criu-btrfs.sh
@@ -2,5 +2,5 @@
 
 source `dirname $0`/criu-lib.sh &&
 prep &&
-make -C test -j 4 ZDTM_ARGS="-C -x '\(maps04\|mountpoints\)'" &&
+make -C test -j 4 ZDTM_ARGS="-C -x '\(maps04\|mountpoints\|inotify_irmap\)'" &&
 true || fail
