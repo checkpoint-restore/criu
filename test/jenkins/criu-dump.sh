02ecc822873a797e324cd170f94d343294fe804b
Andrey Vagin <avagin@openvz.org>
2014-08-26 14:53:00 +0400

    jenkins: exclude static/cgroups02 from the criu-dump job (v2)
    In this job tests are dumped and resumed. The cgroup02 test checks,
    that it is moved in another set of cgroups, but this is done on restore.
    
    Output file: test/zdtm/live/static/cgroup02.out>
    ------------------------------------------------------------------------------
    14:35:55.127:    85: found cgroup at cgroup02.test/zdtmtst>
    14:35:55.127:    85: found cgroup at cgroup02.test/defaultroot>
    14:35:55.127:    85: FAIL: cgroup02.c:132: oldroot not rewritten to zdtmtstroot!
    
    v2: typo fix
    Signed-off-by: Andrey Vagin <avagin@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- test/jenkins/criu-dump.sh
+++ test/jenkins/criu-dump.sh
@@ -4,5 +4,5 @@ source `dirname $0`/criu-lib.sh &&
 prep &&
 mkdir -p test/dump &&
 mount -t tmpfs dump test/dump &&
-make -C test -j 4 ZDTM_ARGS="-d -C" &&
+make -C test -j 4 ZDTM_ARGS="-d -C -x static/cgroup02" &&
 true || fail
