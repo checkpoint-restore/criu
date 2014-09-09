f88d72d0bd9ea56363423664d10a1c042f61ae15
Andrey Vagin <avagin@openvz.org>
2014-09-10 00:17:46 +0400

    mount: strip options for all mounts
    Currently we stript options only one of brothers, but
    mount_equal() thinks that two brothers should have the same options.
    
    Execute zdtm/live/static/mountpoints
    ./mountpoints --pidfile=mountpoints.pid --outfile=mountpoints.out
    Dump 2737
    WARNING: mountpoints returned 1 and left running for debug needs
    Test: zdtm/live/static/mountpoints, Result: FAIL
    ==================================== ERROR ====================================
    Test: zdtm/live/static/mountpoints, Namespace:
    Dump log   : /root/git/criu/test/dump/static/mountpoints/2737/1/dump.log
    --------------------------------- grep Error ---------------------------------
    (00.146444) Error (mount.c:399): Two shared mounts 50, 67 have different sets of children
    (00.146460) Error (mount.c:402): 67:./zdtm_mpts/dev/share-1 doesn't have a proper point for 54:./zdtm_mpts/dev/share-3/test.mnt.share
    (00.146820) Error (cr-dump.c:1921): Dumping FAILED.
    ------------------------------------- END -------------------------------------
    ================================= ERROR OVER =================================
    
    Reported-by: Ruslan Kuprieiev <kupruser@gmail.com>
    Signed-off-by: Andrey Vagin <avagin@openvz.org>
    Tested-by: Ruslan Kuprieiev <kupruser@gmail.com>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- mount.c
+++ mount.c
@@ -901,19 +901,6 @@ uns:
 	return &fstypes[0];
 }
 
-static char *strip(char *opt)
-{
-	int len;
-
-	len = strlen(opt);
-	if (len > 1 && opt[len - 1] == ',')
-		opt[len - 1] = '\0';
-	if (opt[0] == ',')
-		opt++;
-
-	return opt;
-}
-
 static int dump_one_mountpoint(struct mount_info *pm, int fd)
 {
 	MntEntry me = MNT_ENTRY__INIT;
@@ -940,7 +927,7 @@ static int dump_one_mountpoint(struct mount_info *pm, int fd)
 	me.flags		= pm->flags;
 	me.mountpoint		= pm->mountpoint + 1;
 	me.source		= pm->source;
-	me.options		= strip(pm->options);
+	me.options		= pm->options;
 	me.shared_id		= pm->shared_id;
 	me.has_shared_id	= true;
 	me.master_id		= pm->master_id;
