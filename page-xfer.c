checkpoint-restore/criu

commit e47eccb1889f3bc8bd6c0a80b111b1dfcb4752dd

page-server: don't forget to close a sever socket
CID 73348 (#1 of 1): Resource leak (RESOURCE_LEAK)
19. leaked_handle: Handle variable sk going out of scope leaks the handle.

Signed-off-by: Andrey Vagin <avagin@openvz.org>
Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

page-xfer.c
@@ -307,8 +307,9 @@ int cr_page_server(bool daemon_mode, int cfd)
 no_server:
 	if (daemon_mode) {
 		ret = cr_daemon(1, 0, &ask, cfd);
+		close_safe(&sk);
 		if (ret == -1) {
-			pr_perror("Can't run in the background");
+			pr_err("Can't run in the background\n");
 			goto out;
 		}
 		if (ret > 0) { /* parent task, daemon started */

commit 391269b58f8092f44be97c58b9aa71d8cb362b93

page-server: close server socket only in the parent task
Fixes: e47eccb1889f ("page-server: don't forget to close a sever socket")
Signed-off-by: Andrey Vagin <avagin@openvz.org>
Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

page-xfer.c
@@ -307,12 +307,12 @@ int cr_page_server(bool daemon_mode, int cfd)
 no_server:
 	if (daemon_mode) {
 		ret = cr_daemon(1, 0, &ask, cfd);
-		close_safe(&sk);
 		if (ret == -1) {
 			pr_err("Can't run in the background\n");
 			goto out;
 		}
 		if (ret > 0) { /* parent task, daemon started */
+			close_safe(&sk);
 			if (opts.pidfile) {
 				if (write_pidfile(ret) == -1) {
 					pr_perror("Can't write pidfile");