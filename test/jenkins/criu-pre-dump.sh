41b972ccbc63f2f4f5de6ccb34c8211492c0258a
Andrey Vagin <avagin@openvz.org>
2014-11-05 23:23:00 +0400

    jenkins: check page-server (v2)
    page-server are not tested now. I suggest to add it in the pre-dump job.
    
    v2: execute all tests with and without page server
    Signed-off-by: Andrey Vagin <avagin@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- test/jenkins/criu-pre-dump.sh
+++ test/jenkins/criu-pre-dump.sh
@@ -5,4 +5,5 @@ prep &&
 mkdir -p test/dump &&
 mount -t tmpfs dump test/dump &&
 make -C test -j 4 'ZDTM_ARGS=-P -i 3 -C' zdtm &&
+make -C test -j 4 'ZDTM_ARGS=-p -P -i 3 -C' zdtm && # + page server
 true || fail
