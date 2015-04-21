02dff55113a4cc3f4f5479a2e12ef606d62850d6
Andrey Vagin <avagin@openvz.org>
2015-04-21 16:09:09 +0300

    service: allocate buffers for messages dinamically (v2)
    Currently we use a static buffer, but it is too small.
    
    Error (cr-service.c:58): Failed unpacking request: Success
    Error (cr-service.c:694): Can't recv request: Success
    data too short after length-prefix of 1217
    
    v2: use recv instead on recvmsg
    
    Reported-by: Ross Boucher <rboucher@gmail.com>
    Cc: Ross Boucher <rboucher@gmail.com>
    Signed-off-by: Andrey Vagin <avagin@openvz.org>
    Acked-by: Ruslan Kuprieiev <rkuprieiev@cloudlinux.com>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- include/cr-service-const.h
+++ include/cr-service-const.h
@@ -1,7 +1,6 @@
 #ifndef __CR_SERVICE_CONST_H__
 #define __CR_SERVICE_CONST_H__
 
-#define CR_MAX_MSG_SIZE 1024
 #define CR_DEFAULT_SERVICE_ADDRESS "/var/run/criu_service.socket"
 
 #endif /* __CR_SERVICE_CONST_H__ */
