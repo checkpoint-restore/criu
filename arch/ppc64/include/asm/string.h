d28984e80ec61d3529f8cbe0338431491ae38720
Laurent Dufour <ldufour@linux.vnet.ibm.com>
2015-05-13 18:45:10 +0200

    ppc64: Use optimized memcmp
    Instead of belonging to the common C memcmp() function, belong on the
    optimized one stolen from the kernel.
    
    Signed-off-by: Laurent Dufour <ldufour@linux.vnet.ibm.com>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- arch/ppc64/include/asm/string.h
+++ arch/ppc64/include/asm/string.h
@@ -4,6 +4,7 @@
 #include "compiler.h"
 
 #define HAS_BUILTIN_MEMCPY
+#define HAS_BUILTIN_MEMCMP
 
 #include "asm-generic/string.h"
 
@@ -14,5 +15,7 @@ static inline void *builtin_memcpy(void *to, const void *from, unsigned long n)
 		memcpy_power7(to, from, n);
 	return to;
 }
+
+extern int builtin_memcmp(const void *cs, const void *ct, size_t count);
 
 #endif /* __CR_ASM_STRING_H__ */
