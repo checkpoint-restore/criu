2f16ec33a1acba56758de284d58d046693535ec8
Cyrill Gorcunov <gorcunov@openvz.org>
2015-06-05 00:04:16 +0300

    pie: piegen -- Add PIE_SIZE helper
    To hide @nr_gotpcrel
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- pie/pie-relocs.h
+++ pie/pie-relocs.h
@@ -7,8 +7,10 @@
 #include "config.h"
 
 #if defined(CONFIG_X86_64) || defined(CONFIG_X86_32) || defined(CONFIG_PPC64)
+#define PIE_SIZE(__blob_name)	(round_up(sizeof(__blob_name) + nr_gotpcrel * sizeof(long), PAGE_SIZE))
 extern __maybe_unused void elf_relocs_apply(void *mem, void *vbase, size_t size, elf_reloc_t *elf_relocs, size_t nr_relocs);
 #else
+#define PIE_SIZE(__blob_name)	(round_up(sizeof(__blob_name), PAGE_SIZE))
 static always_inline void elf_relocs_apply(void *mem, void *vbase, size_t size, elf_reloc_t *elf_relocs, size_t nr_relocs) { }
 #endif
 
