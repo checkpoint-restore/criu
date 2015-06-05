25d074ad91ca6e45970c991b522ec17692e02dde
Pavel Emelyanov <xemul@parallels.com>
2015-06-05 15:53:00 +0300

    unix: Don't dump external peer w/o name
    On restore we will use the peer's name to connect() the
    socket back, so if there's no name dump should be aborted.
    
    This situation happens when we create a socketpair(), fork
    and dump only one task with one pair end.
    
    Reported-by: Artem Kuzmitskiy <artem.kuzmitskiy@gmail.com>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- sk-unix.c
+++ sk-unix.c
@@ -564,6 +564,12 @@ static int dump_external_sockets(struct unix_sk_desc *peer)
 				pr_err("Can't dump half of stream unix connection.\n");
 				return -1;
 			}
+
+			if (!peer->name) {
+				show_one_unix("Ext dgram w/o name", peer);
+				pr_err("Can't dump name-less external socket.\n");
+				return -1;
+			}
 		} else if (ret < 0)
 			return -1;
 		else
