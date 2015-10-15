33ebd47c980b10fff8c732aafb542198b80b3334
Tycho Andersen <tycho.andersen@canonical.com>
2015-10-15 16:28:08 -0600

    check: mnt_id is now in mainline
    As of
    https://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/?id=49d063cb3
    the mnt_id field is in mainline, so let's check for it in --ms.
    
    Signed-off-by: Tycho Andersen <tycho.andersen@canonical.com>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- cr-check.c
+++ cr-check.c
@@ -293,11 +293,6 @@ int check_mnt_id(void)
 	struct fdinfo_common fdinfo = { .mnt_id = -1 };
 	int ret;
 
-	if (opts.check_ms_kernel) {
-		pr_warn("Skipping mnt_id support check\n");
-		return 0;
-	}
-
 	ret = parse_fdinfo(get_service_fd(LOG_FD_OFF), FD_TYPES__UND, NULL, &fdinfo);
 	if (ret < 0)
 		return -1;
