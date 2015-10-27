c51cdedcf98e1872b74b60018e3c748f0bfeaa7a
Andrew Vagin <avagin@openvz.org>
2015-10-27 18:02:00 +0300

    jenkins: use zdtm.py for criu-iter
    Signed-off-by: Andrew Vagin <avagin@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- test/jenkins/criu-iter.sh
+++ test/jenkins/criu-iter.sh
@@ -1,8 +1,8 @@
 # Make 3 iteration of dump/restore for each test
-
-source `dirname $0`/criu-lib.sh &&
-prep &&
-mkdir -p test/dump &&
-mount -t tmpfs dump test/dump &&
-make -C test ZDTM_ARGS="-C -i 3 -x maps04" zdtm &&
-true || fail
+set -e
+source `dirname $0`/criu-lib.sh
+prep
+mkdir -p test/dump
+mount -t tmpfs dump test/dump
+cd test
+./zdtm.py run --all --parallel 4 --iter 3 -x 'maps04'
