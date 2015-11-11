20088c4fccdd455d1c89ecffbe51bf585fe4905b
Pavel Emelyanov <xemul@parallels.com>
2015-11-11 11:36:18 +0300

    jenkins: Move regular test on .py
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- test/jenkins/criu.sh
+++ test/jenkins/criu.sh
@@ -1,11 +1,5 @@
-source `dirname $0`/criu-lib.sh &&
-prep &&
-bash test/zdtm.sh -C &&
-true || fail
-
-# Execute tests for each new commit
-git rev-parse tested || ( git tag tested; exit )
-for i in `git rev-list --reverse tested..HEAD`; do
-    curl "http://localhost:8080/job/CRIU-by-id/buildWithParameters?token=d6edab71&TEST_COMMIT=$i" || exit 1
-done
-git tag -f tested
+# Make one regular C/R cycle
+set -e
+source `dirname $0`/criu-lib.sh
+prep
+./test/zdtm.py run --all --report report --parallel 4 || fail
