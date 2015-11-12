cc378b909a227f2a9d02cea9a01deae35bf8f500
Pavel Emelyanov <xemul@parallels.com>
2015-11-12 18:01:37 +0400

    jenkins: Make criu-dump test use zdtm.py
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- test/jenkins/criu-dump.sh
+++ test/jenkins/criu-dump.sh
@@ -1,8 +1,6 @@
 # Check that dump is not destructive
-
-source `dirname $0`/criu-lib.sh &&
-prep &&
-mkdir -p test/dump &&
-mount -t tmpfs dump test/dump &&
-make -C test -j 4 ZDTM_ARGS="-d -C -x static/cgroup02" zdtm &&
-true || fail
+set -e
+source `dirname $0`/criu-lib.sh
+prep
+mount_tmpfs_to_dump
+./test/zdtm.py run --all --report report --parallel 4 --norst -x 'maps04' || fail
