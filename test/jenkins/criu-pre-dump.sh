37ec967643254a3b930bf6186bb27b1d34aa2077
Pavel Emelyanov <xemul@parallels.com>
2015-11-12 16:58:04 +0400

    jenkins: Move pre-dump test on .py launcher
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- test/jenkins/criu-pre-dump.sh
+++ test/jenkins/criu-pre-dump.sh
@@ -1,9 +1,7 @@
-# Check pre-dump
-
-source `dirname $0`/criu-lib.sh &&
-prep &&
-mkdir -p test/dump &&
-mount -t tmpfs dump test/dump &&
-make -C test -j 4 'ZDTM_ARGS=-P -i 3 -C' zdtm &&
-make -C test -j 4 'ZDTM_ARGS=-p -P -i 3 -C' zdtm && # + page server
-true || fail
+# Check 3 pre-dump-s before dump (with and w/o page server)
+set -e
+source `dirname $0`/criu-lib.sh
+prep
+mount_tmpfs_to_dump
+./test/zdtm.py run --all --report report --parallel 4 --pre 3 -x 'maps04' || fail
+./test/zdtm.py run --all --report report --parallel 4 --pre 3 --page-server -x 'maps04' || fail
