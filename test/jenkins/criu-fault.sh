8135e26ae34ccf496f167b4b321711463e3519a5
Pavel Emelyanov <xemul@parallels.com>
2015-11-23 13:22:08 +0300

    jenkins: Fix path to zdtm.py in criu-fault
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- test/jenkins/criu-fault.sh
+++ test/jenkins/criu-fault.sh
@@ -2,5 +2,5 @@
 set -e
 source `dirname $0`/criu-lib.sh
 prep
-./zdtm.py run -t zdtm/live/static/env00 --fault 1 --report report -f h || fail
-./zdtm.py run -t zdtm/live/static/unlink_fstat00 --fault 2 --report report -f h || fail
+./test/zdtm.py run -t zdtm/live/static/env00 --fault 1 --report report -f h || fail
+./test/zdtm.py run -t zdtm/live/static/unlink_fstat00 --fault 2 --report report -f h || fail
