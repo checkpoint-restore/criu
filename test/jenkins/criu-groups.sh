e71d50894d86aaf2cb03a1a66d603ac4dcae9c7d
Pavel Emelyanov <xemul@parallels.com>
2015-12-09 15:57:41 +0300

    jenkins: Add groups
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

---
+++ test/jenkins/criu-groups.sh
@@ -0,0 +1,7 @@
+# Make one regular C/R cycle over randomly-generated groups
+set -e
+source `dirname $0`/criu-lib.sh
+prep
+mount_tmpfs_to_dump
+./test/zdtm.py group --max 32 -x maps04 -x cgroup || fail
+./test/zdtm.py --set groups run --all --report report --parallel 2 -f best || fail
