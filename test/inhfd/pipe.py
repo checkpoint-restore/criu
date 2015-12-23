57d73475affa234351c3765a5d2eedcf6f279102
Andrew Vagin <avagin@virtuozzo.com>
2015-12-23 16:33:00 +0300

    test/inhfd: use os.fdopen instead of a home-made class
    Signed-off-by: Andrew Vagin <avagin@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- test/inhfd/pipe.py
+++ test/inhfd/pipe.py
@@ -1,28 +1,8 @@
 import os
 
-class pipef:
-	def __init__(self, fd):
-		self.__fd = fd
-
-	def read(self, blen):
-		return os.read(self.__fd, blen)
-
-	def write(self, msg):
-		return os.write(self.__fd, msg)
-
-	def flush(self):
-		pass
-
-	def close(self):
-		os.close(self.__fd)
-		self.__fd = -1
-
-	def fileno(self):
-		return self.__fd
-
 def create_fds():
 	(fd1, fd2) = os.pipe()
-	return (pipef(fd2), pipef(fd1))
+	return (os.fdopen(fd2, "w"), os.fdopen(fd1, "r"))
 
 def filename(pipef):
 	return 'pipe:[%d]' % os.fstat(pipef.fileno()).st_ino
