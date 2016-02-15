3a234722df3b4885e51e74e5687ee3f3aa1a8f2e
Cyrill Gorcunov <gorcunov@openvz.org>
2016-02-16 00:00:00 +0300

    test: jenkins -- Fix path for pycriu
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- test/jenkins/crit.sh
+++ test/jenkins/crit.sh
@@ -3,5 +3,5 @@ set -e
 source `dirname $0`/criu-lib.sh
 prep
 ./test/zdtm.py run --all -f best -x maps04 -x cgroup02 --norst --keep always || fail
-PYTHONPATH="$(pwd)" ./test/crit-recode.py || fail
+PYTHONPATH="$(pwd)/lib/" ./test/crit-recode.py || fail
 exit 0
