6b4fb4d4de92a4023051624ab2e1f25181437d72
Andrew Vagin <avagin@virtuozzo.com>
2016-02-27 02:06:58 +0300

    test: remove test/post-dump.sh
    Signed-off-by: Andrew Vagin <avagin@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- test/post-dump.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-[ "$CRTOOLS_SCRIPT_ACTION" = post-dump ] || exit 0
-
-#
-# Special code to inform zdtm that we're
-# done and should proceed testing treating
-# non-zero return as known case.
-exit 32
