3006ada21d2afa592ef4a26bc4d54a359d63daa8
Andrew Vagin <avagin@virtuozzo.com>
2016-03-12 03:53:00 +0300

    jenkins: add a script to execute tests on overlayfs
    Here are all tests which pass now
    
    And I created an issue to investigate fails of other tests:
    https://github.com/xemul/criu/issues/136
    
    Signed-off-by: Andrew Vagin <avagin@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

---
+++ test/jenkins/criu-overlay.sh
@@ -0,0 +1,7 @@
+# Make one regular C/R cycle
+set -e
+source `dirname $0`/criu-lib.sh
+prep
+mkdir -p test.up test.work
+mount -t overlay overlay -olowerdir=test,upperdir=test.up,workdir=test.work test
+./test/zdtm.py run --all --report report --parallel 4 -x inotify -x mntns_open -x socket -x sk-unix -x unlink -x fsnotify -x fanotify -x ghost || fail
