cd5f7df520a73214b813a6bb748b0e146d779181
Cyrill Gorcunov <gorcunov@openvz.org>
2016-03-24 21:25:35 +0300

    nmk: build.mk -- Squash gen-rule-x into gen-cc-rules
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- scripts/nmk/scripts/build.mk
+++ scripts/nmk/scripts/build.mk
@@ -33,51 +33,33 @@ endef
 
 #
 # General rules.
-define gen-rule-o-from-c-by-name
+define gen-cc-rules
 $(1).o: $(2).c $(3)
 	$$(call msg-cc, $$@)
 	$$(Q) $$(CC) -c $$(strip $$(nmk-ccflags)) $(4) $$< -o $$@
-endef
-define gen-rule-i-from-c-by-name
 $(1).i: $(2).c $(3)
 	$$(call msg-cc, $$@)
 	$$(Q) $$(CC) -E $$(strip $$(nmk-ccflags)) $(4) $$< -o $$@
-endef
-define gen-rule-s-from-c-by-name
 $(1).s: $(2).c $(3)
 	$$(call msg-cc, $$@)
 	$$(Q) $$(CC) -S -fverbose-asm $$(strip $$(nmk-ccflags)) $(4) $$< -o $$@
-endef
-define gen-rule-o-from-S-by-name
-$(1).o: $(2).S $(3)
-	$$(call msg-cc, $$@)
-	$$(Q) $$(CC) -c $$(strip $$(nmk-asflags)) $(4) $$< -o $$@
-endef
-define gen-rule-d-from-c-by-name
 $(1).d: $(2).c $(3)
 	$$(call msg-dep, $$@)
 	$$(Q) $$(CC) -M -MT $$@ -MT $$(patsubst %.d,%.o,$$@) $$(strip $$(nmk-ccflags)) $(4) $$< -o $$@
-endef
-define gen-rule-d-from-S-by-name
-$(1).d: $(2).S $(3)
-	$$(call msg-dep, $$@)
-	$$(Q) $$(CC) -M -MT $$@ -MT $$(patsubst %.d,%.o,$$@) $$(strip $$(nmk-asflags)) $(4) $$< -o $$@
-endef
-define gen-rule-i-from-S-by-name
+$(1).o: $(2).S $(3)
+	$$(call msg-cc, $$@)
+	$$(Q) $$(CC) -c $$(strip $$(nmk-asflags)) $(5) $$< -o $$@
 $(1).i: $(2).S $(3)
 	$$(call msg-cc, $$@)
-	$$(Q) $$(CC) -E $$(strip $$(nmk-asflags)) $(4) $$< -o $$@
+	$$(Q) $$(CC) -E $$(strip $$(nmk-asflags)) $(5) $$< -o $$@
+$(1).d: $(2).S $(3)
+	$$(call msg-dep, $$@)
+	$$(Q) $$(CC) -M -MT $$@ -MT $$(patsubst %.d,%.o,$$@) $$(strip $$(nmk-asflags)) $(5) $$< -o $$@
 endef
 
 #
 # Expand early which matched all implicits.
-$(eval $(call gen-rule-o-from-c-by-name,%,%))
-$(eval $(call gen-rule-i-from-c-by-name,%,%))
-$(eval $(call gen-rule-s-from-c-by-name,%,%))
-$(eval $(call gen-rule-o-from-S-by-name,%,%))
-$(eval $(call gen-rule-d-from-c-by-name,%,%))
-$(eval $(call gen-rule-d-from-S-by-name,%,%))
-$(eval $(call gen-rule-i-from-S-by-name,%,%))
+$(eval $(call gen-cc-rules,%,%))
 
 src-makefile	:= $(call objectify,$(makefile))
 include $(src-makefile)
@@ -107,13 +89,7 @@ ld_flags	:= $(strip $(LDFLAGS) $(ldflags-y))
 
 #
 # $(obj) related rules.
-$(eval $(call gen-rule-o-from-c-by-name,$(obj)/%,$(obj)/%,$(src-makefile)))
-$(eval $(call gen-rule-i-from-c-by-name,$(obj)/%,$(obj)/%,$(src-makefile)))
-$(eval $(call gen-rule-s-from-c-by-name,$(obj)/%,$(obj)/%,$(src-makefile)))
-$(eval $(call gen-rule-o-from-S-by-name,$(obj)/%,$(obj)/%,$(src-makefile)))
-$(eval $(call gen-rule-d-from-c-by-name,$(obj)/%,$(obj)/%,$(src-makefile)))
-$(eval $(call gen-rule-d-from-S-by-name,$(obj)/%,$(obj)/%,$(src-makefile)))
-$(eval $(call gen-rule-i-from-S-by-name,$(obj)/%,$(obj)/%,$(src-makefile)))
+$(eval $(call gen-cc-rules,$(obj)/%,$(obj)/%,$(src-makefile)))
 
 #
 # Prepare targets.
