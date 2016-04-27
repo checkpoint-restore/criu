f68e5a6b3dd669cafab3cd3e5c556363d6ac357b
Andrew Vagin <avagin@virtuozzo.com>
2016-04-27 22:45:00 +0300

    criu: always enable the userfaultfd support
    Add linux/userfaultfd.h to criu sources. This header is a part
    of the kernel API and I see nothing wrong to have in the repo.
    
    Why we want to do this:
    * to check that criu works correctly if a kernel doesn't
      support userfaultfd.
    * to check compilation of the userfaultfd part in travis-ci.
    
    v2: remove UFFD from FEATURES_LIST
    
    Acked-by: Mike Rapoport <rppt@linux.vnet.ibm.com>
    Acked-by: Adrian Reber <areber@redhat.com>
    Cc: Mike Rapoport <rppt@linux.vnet.ibm.com>
    Cc: Adrian Reber <areber@redhat.com>
    Signed-off-by: Andrew Vagin <avagin@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/include/uffd.h
+++ criu/include/uffd.h
@@ -1,22 +1,7 @@
 #ifndef __CR_UFFD_H_
 #define __CR_UFFD_H_
 
-#include "config.h"
-#include "restorer.h"
-
-#ifdef CONFIG_HAS_UFFD
-
-#include <syscall.h>
-#include <linux/userfaultfd.h>
-
-#ifndef __NR_userfaultfd
-#error "missing __NR_userfaultfd definition"
-#endif
-
+struct task_restore_args;
 extern int setup_uffd(struct task_restore_args *task_args, int pid);
-#else
-static inline int setup_uffd(struct task_restore_args *task_args, int pid) { return 0; }
-
-#endif /* CONFIG_HAS_UFFD */
 
 #endif /* __CR_UFFD_H_ */
