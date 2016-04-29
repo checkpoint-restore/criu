8fe2ce725adc342c6a6f5e09028f35cab98da9a2
Dmitry Safonov <dsafonov@virtuozzo.com>
2016-04-29 22:47:00 +0300

    compel: shuffle skeleton a bit
    I propose to change compel directory structure:
    - if we want support more arch's than x86/ppc66, it seems
      worth to add arch/ folder
    - move all sources from src/ folder up
    - to have headers and build additional object with CFLAGS for
      a symlink seems for me less hacky way than mess around
      with .c files cross-linking
    - I made handle-elf.h header for arch helpers code. I may named
      that just "elf.h", but that may confuse, as there are <elf.h>
      system header
    - I would like to drop those ELF_PPC64/ELF_X86_32/ELF_X86_64
      defines and use CONFIG_X86_64 and whatnot
    
    After this patch compel directory become:
    compel/
    ├── arch
    │   ├── ppc64
    │   │   └── include
    │   │       └── handle-elf.h
    │   └── x86
    │       └── include
    │           └── handle-elf.h
    ├── handle-elf-32.c -> handle-elf.c
    ├── handle-elf.c
    ├── include
    │   ├── piegen.h
    │   └── uapi
    │       ├── elf32-types.h
    │       ├── elf64-types.h
    │       └── types.h
    ├── main.c
    └── Makefile
    
    Note: temporary I make value32 and addend32 for compilation on arm/aarch64
    
    Cc: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>
    Acked-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

---
+++ compel/arch/ppc64/include/handle-elf.h
@@ -0,0 +1,9 @@
+#ifndef __COMPEL_HANDLE_ELF_H__
+#define __COMPEL_HANDLE_ELF_H__
+
+#include "uapi/elf32-types.h"
+
+#define ELF_PPC64
+#define handle_elf	handle_elf_ppc64
+
+#endif /* __COMPEL_HANDLE_ELF_H__ */
