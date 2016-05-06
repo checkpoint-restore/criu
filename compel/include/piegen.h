fc1c1e725f18d64e5e26bdf5eb269ec280ed784b
Dmitry Safonov <dsafonov@virtuozzo.com>
2016-05-06 19:14:57 +0300

    compel: make output streams options
    That will help to omit pr_* output on the screen on tests,
    where one can redefine `opts`.
    
    Cc: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>
    Reviewed-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- compel/include/piegen.h
+++ compel/include/piegen.h
@@ -15,17 +15,39 @@ typedef struct {
 	char		*prefix_name;
 	char		*var_name;
 	char		*nrgotpcrel_name;
+	FILE		*fout;
+	FILE		*ferr;
+	FILE		*fdebug;
 } piegen_opt_t;
 
 extern piegen_opt_t opts;
-extern FILE *fout;
 
-#define pr_out(fmt, ...)	fprintf(fout, fmt, ##__VA_ARGS__)
+#define pr_out(fmt, ...)							\
+do {										\
+	if (opts.fout)								\
+		fprintf(opts.fout, fmt, ##__VA_ARGS__);				\
+} while (0)
 
-#define pr_debug(fmt, ...)	printf("%s: "fmt, opts.stream_name, ##__VA_ARGS__)
+#define pr_debug(fmt, ...)							\
+do {										\
+	if (opts.fdebug)							\
+		fprintf(opts.fdebug, "%s: "fmt,					\
+			opts.stream_name, ##__VA_ARGS__);			\
+} while (0)
 
-#define pr_err(fmt, ...)	fprintf(stderr, "%s: Error (%s:%d): "fmt, opts.stream_name, __FILE__, __LINE__, ##__VA_ARGS__)
-#define pr_perror(fmt, ...)	fprintf(stderr, "%s: Error (%s:%d): "fmt ": %m\n", opts.stream_name, __FILE__, __LINE__, ##__VA_ARGS__)
+#define pr_err(fmt, ...)							\
+do {										\
+	if (opts.ferr)								\
+		fprintf(opts.ferr, "%s: Error (%s:%d): "fmt,			\
+			opts.stream_name, __FILE__, __LINE__, ##__VA_ARGS__);	\
+} while (0)
+
+#define pr_perror(fmt, ...)							\
+do {										\
+	if (opts.ferr)								\
+		fprintf(opts.ferr, "%s: Error (%s:%d): "fmt ": %m\n",		\
+			opts.stream_name, __FILE__, __LINE__, ##__VA_ARGS__);	\
+} while (0)
 
 extern int handle_binary(void *mem, size_t size);
 
