e569e46b312071c5d8d1a36bfef9be28c444efda
Ruslan Kuprieiev <kupruser@gmail.com>
2016-05-21 16:38:30 +0300

    test: fix manual crit test
    In automated tests we use test/crit-recode.py because of performance
    benefits(no need to launch python interpreter more than once), but
    manual test is still useful for manual testing, so lets make it
    work again.
    
    Signed-off-by: Ruslan Kuprieiev <kupruser@gmail.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- test/others/crit/test.sh
+++ test/others/crit/test.sh
@@ -1,26 +1,49 @@
-images_list=$(ls -1 *.img)
+source ../env.sh
+
+images_list=""
 
 function _exit {
 	if [ $? -ne 0 ]; then
 		echo "FAIL"
-		exit -1
+		exit 1
 	fi
 }
 
-for x in $images_list
-do
-	echo "=== $x"
-	if [[ $x == pages* ]]; then
-		echo "skip"
-		continue
+function gen_imgs {
+	setsid ./loop.sh < /dev/null &> /dev/null &
+	PID=$!
+	$CRIU dump -v4 -o dump.log -D ./ -t $PID
+	if [ $? -ne 0 ]; then
+		kill -9 $PID
+		_exit 1
 	fi
 
-	echo "  -- to json"
-	../../crit decode -o "$x"".json" --pretty < $x || _exit $?
-	echo "  -- to img"
-	../../crit encode -i "$x"".json" > "$x"".json.img" || _exit $?
-	echo "  -- cmp"
-	cmp $x "$x"".json.img" || _exit $?
+	images_list=$(ls -1 *.img)
+	if [ -z "$images_list" ]; then
+		echo "Failed to generate images"
+		_exit 1
+	fi
+}
 
-	echo "=== done"
-done
+function run_test {
+	for x in $images_list
+	do
+		echo "=== $x"
+		if [[ $x == pages* ]]; then
+			echo "skip"
+			continue
+		fi
+
+		echo "  -- to json"
+		$CRIT decode -o "$x"".json" --pretty < $x || _exit $?
+		echo "  -- to img"
+		$CRIT encode -i "$x"".json" > "$x"".json.img" || _exit $?
+		echo "  -- cmp"
+		cmp $x "$x"".json.img" || _exit $?
+
+		echo "=== done"
+	done
+}
+
+gen_imgs
+run_test
--- test/others/env.sh
+++ test/others/env.sh
@@ -2,3 +2,5 @@
 
 CRIU=$(readlink -f `dirname ${BASH_SOURCE[0]}`/../../criu/criu)
 criu=$CRIU
+CRIT=$(readlink -f `dirname ${BASH_SOURCE[0]}`/../../crit/crit)
+crit=$CRIT
