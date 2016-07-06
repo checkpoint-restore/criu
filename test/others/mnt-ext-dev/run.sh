87270c8147d5e0c8d10d9daae82a0f71d2aaa30d
Andrew Vagin <avagin@virtuozzo.com>
2016-07-06 05:01:58 +0300

    others/mnt-ext-dev: Force mke2fs to create a filesystem
    It's required because we specify a file instead of a block device.
    
    Signed-off-by: Andrew Vagin <avagin@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- test/others/mnt-ext-dev/run.sh
+++ test/others/mnt-ext-dev/run.sh
@@ -6,7 +6,7 @@ python ../../zdtm.py run -t zdtm/static/env00 --iter 0 -f ns
 
 truncate -s 0 zdtm.loop
 truncate -s 50M zdtm.loop
-mkfs.ext4 zdtm.loop
+mkfs.ext4 -F zdtm.loop
 dev=`losetup --find --show zdtm.loop`
 mkdir -p ../../dev
 cp -ap $dev ../../dev
