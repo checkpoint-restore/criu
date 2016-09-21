a1d931c3547526c516de43bb3e0cbb0257a8d21a
Dmitry Safonov <dsafonov@virtuozzo.com>
2016-09-21 23:54:26 +0300

    compel: plugins -- Add shmem plugin
    The shmem pluging allows creation of shared memory segment between
    parasite code and the caller.
    
    Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

---
+++ compel/include/shmem.h
@@ -0,0 +1,10 @@
+#ifndef __COMPEL_PLUGIN_SHMEM_PRIV_H__
+#define __COMPEL_PLUGIN_SHMEM_PRIV_H__
+
+struct shmem_plugin_msg {
+	unsigned long start;
+	unsigned long len;
+};
+
+#endif /* __COMPEL_PLUGIN_SHMEM_PRIV_H__ */
+
