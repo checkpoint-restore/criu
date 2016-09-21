31ea5220b1a49e65b210216cdfcd11a4df207b7e
Cyrill Gorcunov <gorcunov@openvz.org>
2016-09-21 23:54:29 +0300

    compel: Get rid of old piegen code
    Use new compel.h header with appropriate types.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/pie/pie-relocs.h
+++ criu/pie/pie-relocs.h
@@ -1,7 +1,7 @@
 #ifndef __PIE_RELOCS_H__
 #define __PIE_RELOCS_H__
 
-#include "compel/include/uapi/types.h"
+#include "compel/include/uapi/compel.h"
 
 #include "common/compiler.h"
 #include "config.h"
@@ -9,7 +9,7 @@
 #ifdef CONFIG_PIEGEN
 
 extern __maybe_unused void elf_relocs_apply(void *mem, void *vbase, size_t size,
-					    elf_reloc_t *elf_relocs, size_t nr_relocs);
+					    compel_reloc_t *elf_relocs, size_t nr_relocs);
 #define pie_size(__pie_name)	(round_up(sizeof(__pie_name##_blob) + \
 			__pie_name ## _nr_gotpcrel * sizeof(long), page_size()))
 #define ELF_RELOCS_APPLY(__pie_name, __mem, __vbase)			\
