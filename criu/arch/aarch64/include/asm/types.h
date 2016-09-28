524aeefdd022ec47cef862f16bbac64dfef60c4d
Cyrill Gorcunov <gorcunov@openvz.org>
2016-09-29 00:34:00 +0300

    criu: arch -- Fix more task_size prototype
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/arch/aarch64/include/asm/types.h
+++ criu/arch/aarch64/include/asm/types.h
@@ -64,7 +64,7 @@ typedef struct user_fpsimd_state user_fpregs_struct_t;
 
 int munmap(void *addr, size_t length);
 
-static inline unsigned long task_size() {
+static inline unsigned long task_size(void) {
 	unsigned long task_size;
 
 	for (task_size = TASK_SIZE_MIN; task_size < TASK_SIZE_MAX; task_size <<= 1)
