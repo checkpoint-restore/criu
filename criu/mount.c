4e3d0f7d687d152ed9d88d9a84824aaa46c57ce3
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-10-06 13:46:00 +0300

    mount: fix error print in case of failed bind-mount
    This is minor issue, but would be nice to have print in sync with actual
    action.
    
    travis-ci: success for mount: fix error print in case of failed bind-mount
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/mount.c
+++ criu/mount.c
@@ -1902,7 +1902,7 @@ static int do_bind_mount(struct mount_info *mi)
 		/* Get a copy of mi->bind without child mounts */
 		if (mount(mnt_path, mnt_clean_path, NULL, MS_BIND, NULL)) {
 			pr_perror("Unable to bind-mount %s to %s",
-					mi->bind->mountpoint, mnt_clean_path);
+					mnt_path, mnt_clean_path);
 			return -1;
 		}
 		mnt_path = mnt_clean_path;
