a25d6c7f99d9e52bd2e1647150d89bb1b39f508f
Tycho Andersen <tycho.andersen@canonical.com>
2016-10-20 10:25:31 -0600

    tests: add a test for macvlan
    Note that this test doesn't run in uns mode, even though we have support
    for that. Without a full container engine, I couldn't think of a nice way
    to pass a macvlan device into the zdtm "container" when in UNS mode.
    
    v2: use the nsid_manip feature flag
    
    travis-ci: success for series starting with [v10,01/11] net: pass the struct nlattrs to dump() functions
    Signed-off-by: Tycho Andersen <tycho.andersen@canonical.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

---
+++ test/zdtm/static/macvlan.c
@@ -0,0 +1,70 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/mount.h>
+#include <linux/limits.h>
+#include <signal.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include "zdtmtst.h"
+
+const char *test_doc	= "check that macvlan interfaces are c/r'd correctly";
+const char *test_author	= "Tycho Andersen <tycho.andersen@canonical.com>";
+
+#define BRIDGE_NAME "zdtmbr0"
+#define IF_NAME "zdtmmvlan0"
+
+static bool wait_for_macvlan(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++) {
+		if (system("ip addr list dev " IF_NAME) == 0)
+			return true;
+
+		sleep(1);
+	}
+
+	return false;
+}
+
+int main(int argc, char **argv)
+{
+	int ret = 1;
+
+	test_init(argc, argv);
+
+	if (!wait_for_macvlan()) {
+		fail("failed to inject macvlan device\n");
+		return 1;
+	}
+
+	if (system("ip addr list dev " IF_NAME " > macvlan.dump.test")) {
+		fail("can't save net config");
+		goto out;
+	}
+
+	test_daemon();
+	test_waitsig();
+
+	if (system("ip addr list dev " IF_NAME " > macvlan.rst.test")) {
+		fail("can't get net config");
+		goto out;
+	}
+
+	if (system("diff macvlan.rst.test macvlan.dump.test")) {
+		fail("Net config differs after restore");
+		goto out;
+	}
+
+	pass();
+	ret = 0;
+
+out:
+	return ret;
+}
