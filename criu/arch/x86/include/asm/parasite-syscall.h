f15dfc61072a57d88275b8a04e76165d1285b1ea
Pavel Emelyanov <xemul@virtuozzo.com>
2016-10-27 15:01:34 +0300

    infect: Move __NR32_mmap into compel headers
    This remove parasite-syscall.h from infect.
    
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/arch/x86/include/asm/parasite-syscall.h
+++ criu/arch/x86/include/asm/parasite-syscall.h
@@ -5,13 +5,4 @@
 
 struct parasite_ctl;
 
-/*
- * For x86_32 __NR_mmap inside the kernel represents old_mmap system
- * call, but since we didn't use it yet lets go further and simply
- * define own alias for __NR_mmap2 which would allow us to unify code
- * between 32 and 64 bits version.
- */
-#define __NR32_mmap __NR32_mmap2
-
-
 #endif
