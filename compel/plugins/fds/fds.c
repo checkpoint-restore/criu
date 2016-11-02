4041f201dfe74a1760c034a439b067604844b852
Pavel Emelyanov <xemul@virtuozzo.com>
2016-11-02 03:41:57 +0300

    scm: Switch compel fd plugin onto common
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- compel/plugins/fds/fds.c
+++ compel/plugins/fds/fds.c
@@ -1,3 +1,5 @@
+#include <errno.h>
+
 #include "uapi/plugins.h"
 
 #include "uapi/std/syscall.h"
@@ -5,22 +7,14 @@
 #include "uapi/plugin-fds.h"
 
 #include "std-priv.h"
+#include "log.h"
 
 #include "common/compiler.h"
+#include "common/bug.h"
 
 #define __sys(foo)	sys_##foo
-#define __std(foo)	std_##foo
+#define __memcpy	std_memcpy
 
-#include "../../src/shared/fds.c"
-
-int fds_send(int *fds, int nr_fds)
-{
-	return fds_send_via(std_ctl_sock(), fds, nr_fds);
-}
-
-int fds_recv(int *fds, int nr_fds)
-{
-	return fds_recv_via(std_ctl_sock(), fds, nr_fds);
-}
+#include "common/scm-code.c"
 
 PLUGIN_REGISTER_DUMMY(fds)
