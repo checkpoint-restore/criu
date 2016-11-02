6606dc722c67da3e62a2eb578fea1d70ae3ee51c
Pavel Emelyanov <xemul@virtuozzo.com>
2016-11-02 03:41:57 +0300

    scm: Switch compel fd plugin onto common
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- compel/plugins/include/uapi/plugin-fds.h
+++ compel/plugins/include/uapi/plugin-fds.h
@@ -5,23 +5,11 @@
 #ifndef __COMPEL_PLUGIN_FDS_H__
 #define __COMPEL_PLUGIN_FDS_H__
 
-extern int fds_send(int *fds, int nr_fds);
-extern int fds_recv(int *fds, int nr_fds);
+#include "common/scm.h"
 
-static inline int fds_send_one(int fd)
+static inline int send_fd(int sock, struct sockaddr_un *saddr, int saddr_len, int fd)
 {
-	return fds_send(&fd, 1);
-}
-
-static inline int fds_recv_one(void)
-{
-	int fd, ret;
-
-	ret = fds_recv(&fd, 1);
-	if (ret)
-		fd = -1;
-
-	return fd;
+	return send_fds(sock, saddr, saddr_len, &fd, 1, false);
 }
 
 #endif /* __COMPEL_PLUGIN_FDS_H__ */
