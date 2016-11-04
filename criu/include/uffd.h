428226a8ac7959bff7130ce1f39d7dd2c769c5f7
Mike Rapoport <rppt@linux.vnet.ibm.com>
2016-11-04 09:08:04 -0600

    lazy-pages: handle zombie processes
    Restore of a zombie process does not call setup_uffd which causes
    lazy-pages daemon to stuck forever waiting for (pid, uffd) pair to arrive.
    Let's extend the protocol between restore and lazy-pages so that for zombie
    process a (0, -1) pair will be sent instead of actual (uffd, pid).
    
    travis-ci: success for lazy-pages: misc fixes (rev4)
    Signed-off-by: Mike Rapoport <rppt@linux.vnet.ibm.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/include/uffd.h
+++ criu/include/uffd.h
@@ -3,6 +3,7 @@
 
 struct task_restore_args;
 extern int setup_uffd(int pid, struct task_restore_args *task_args);
+extern int lazy_pages_setup_zombie(void);
 extern int prepare_lazy_pages_socket(void);
 
 #endif /* __CR_UFFD_H_ */
