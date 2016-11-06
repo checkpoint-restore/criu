fd814de1d3f7b0971f8ca55e761d5c385902a5d3
Cyrill Gorcunov <gorcunov@openvz.org>
2016-11-07 01:20:46 +0300

    infect: Move compel_relocs_apply into engine
    It will move into compel on a final pass.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/pie/pie-relocs.h
+++ criu/pie/pie-relocs.h
@@ -8,12 +8,10 @@
 
 #ifdef CONFIG_PIEGEN
 
-extern __maybe_unused void elf_relocs_apply(void *mem, void *vbase, size_t size,
-					    compel_reloc_t *elf_relocs, size_t nr_relocs);
 #define pie_size(__pie_name)	(round_up(sizeof(__pie_name##_blob) + \
 			__pie_name ## _nr_gotpcrel * sizeof(long), page_size()))
 #define ELF_RELOCS_APPLY(__pie_name, __mem, __vbase)			\
-	elf_relocs_apply(__mem, __vbase, sizeof(__pie_name##_blob),	\
+	compel_relocs_apply(__mem, __vbase, sizeof(__pie_name##_blob),	\
 			 __pie_name##_relocs, ARRAY_SIZE(__pie_name##_relocs))
 
 #else
