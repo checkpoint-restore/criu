b46e5ec1184ff3903370f20ab3eda5a88c6f8a80
Pavel Emelyanov <xemul@virtuozzo.com>
2016-11-14 16:06:15 +0300

    compel: Move relocs stuff into include/uapi/handle-elf.h
    The same as prev patch -- clean up the compel.h
    
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- compel/include/uapi/compel.h
+++ compel/include/uapi/compel.h
@@ -5,18 +5,7 @@
 #include <stdarg.h>
 
 #include <compel/asm/infect-types.h>
-
-#define COMPEL_TYPE_INT		(1u << 0)
-#define COMPEL_TYPE_LONG	(1u << 1)
-#define COMPEL_TYPE_GOTPCREL	(1u << 2)
-
-typedef struct {
-	unsigned int	offset;
-	unsigned int	type;
-	long		addend;
-	long		value;
-} compel_reloc_t;
-
+#include <compel/handle-elf.h>
 #include <compel/log.h>
 #include <compel/infect-util.h>
 #include <compel/infect-rpc.h>
