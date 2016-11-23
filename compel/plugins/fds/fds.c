39ab415ff782a7dfd7567dcb3ced65316c49fc3d
Cyrill Gorcunov <gorcunov@openvz.org>
2016-11-23 18:33:55 +0300

    compel: plugins,fds -- Don't include log.h
    It's libcompel's helper. We need to address this problem later.
    
    travis-ci: success for compel: A few fixes and example
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- compel/plugins/fds/fds.c
+++ compel/plugins/fds/fds.c
@@ -4,10 +4,12 @@
 
 #include "uapi/std/syscall.h"
 #include "uapi/std/string.h"
+#include "uapi/std/log.h"
 #include "uapi/plugin-fds.h"
 
 #include "std-priv.h"
-#include "log.h"
+
+#define pr_err(fmt, ...)
 
 #include "common/compiler.h"
 #include "common/bug.h"
