ed28ab75a2a9fdbbeb652ada5e9c0ab10f5a8962
Pavel Emelyanov <xemul@virtuozzo.com>
2016-11-25 16:20:31 +0300

    compel: Add compel_stop_task() to UAPI headers
    The routine itself is in library, just forgot to putt the
    declaration into UAPI header.
    
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- compel/include/uapi/infect.h
+++ compel/include/uapi/infect.h
@@ -25,6 +25,8 @@ struct seize_task_status {
 extern int compel_wait_task(int pid, int ppid,
 		int (*get_status)(int pid, struct seize_task_status *),
 		struct seize_task_status *st);
+
+extern int compel_stop_task(int pid);
 extern int compel_unseize_task(pid_t pid, int orig_state, int state);
 
 /*
