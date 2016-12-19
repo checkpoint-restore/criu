9a2c99c343a04ab0c28cccefb480c27e6d2621bb
Andrei Vagin <avagin@virtuozzo.com>
2016-12-20 01:21:13 +0300

    netns: dump and restore network namespace ID-s
    In each network namespace we can set an id for another network namespace
    to be able to address it in netlink messages.
    
    For example, we can say that a peer of a veth devices has to be created
    in a network namespace with a specified id. If we request information about
    a veth device, a kernel will report where a peer device lives.
    
    An user are able to set this ID-s, so we have to dump and restore them.
    
    v2: add more commetns
    v3: make a union of nsfd_id and ns_fd, they are not used together
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/namespaces.c
+++ criu/namespaces.c
@@ -299,6 +299,9 @@ struct ns_id *rst_new_ns_id(unsigned int id, pid_t pid,
 		nsid->type = type;
 		nsid_add(nsid, nd, id, pid);
 		nsid->ns_populated = false;
+
+		if (nd == &net_ns_desc)
+			INIT_LIST_HEAD(&nsid->net.ids);
 	}
 
 	return nsid;
@@ -419,6 +422,10 @@ static unsigned int generate_ns_id(int pid, unsigned int kid, struct ns_desc *nd
 	nsid->kid = kid;
 	nsid->ns_populated = true;
 	nsid_add(nsid, nd, ns_next_id++, pid);
+
+	if (nd == &net_ns_desc) {
+		INIT_LIST_HEAD(&nsid->net.ids);
+	}
 
 found:
 	if (ns_ret)
