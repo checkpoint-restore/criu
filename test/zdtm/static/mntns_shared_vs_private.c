7b5a86350484791d758768ba4ca263d12e850e8f
Andrei Vagin <avagin@virtuozzo.com>
2017-01-19 01:37:34 +0300

    zdtm: don't try to umount a mount if its siblings have children
    The kernel had a bug which allowed to do this and it was fixed recently.

--- test/zdtm/static/mntns_shared_vs_private.c
+++ test/zdtm/static/mntns_shared_vs_private.c
@@ -47,7 +47,7 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	if (mount("zdtm_fs", dirname, "tmpfs", 0, NULL)) {
+	if (mount("zdtm_dirname", dirname, "tmpfs", 0, NULL)) {
 		pr_perror("mount");
 		return 1;
 	}
@@ -79,6 +79,11 @@ int main(int argc, char **argv)
 			task_waiter_complete(&t, 1);
 			task_waiter_wait4(&t, 2);
 
+			if (umount(path)) {
+				pr_perror("umount");
+				return 1;
+			}
+
 			return 0;
 		}
 	}
@@ -88,15 +93,6 @@ int main(int argc, char **argv)
 
 	test_daemon();
 	test_waitsig();
-
-	if (umount(path)) {
-		pr_perror("Unable to umount %s", path);
-		return 1;
-	}
-	if (umount(dirname)) {
-		pr_perror("Unable to umount %s", dirname);
-		return 1;
-	}
 
 	for (i = 0; i < 2; i++) {
 		task_waiter_complete(&t, 2);
@@ -110,6 +106,16 @@ int main(int argc, char **argv)
 			pr_err("%d/%d/%d/%d\n", WIFEXITED(status), WEXITSTATUS(status), WIFSIGNALED(status), WTERMSIG(status));
 			return 1;
 		}
+	}
+
+
+	if (umount(path)) {
+		pr_perror("Unable to umount %s", path);
+		return 1;
+	}
+	if (umount(dirname)) {
+		pr_perror("Unable to umount %s", dirname);
+		return 1;
 	}
 
 	pass();
