25d59cb2bf5b8099d29cadcb5509003242b8896c
Andrei Vagin <avagin@virtuozzo.com>
2017-01-24 01:57:43 +0300

    crtools: fix exit code for page-server
    cr_page_server() returns pid, if it is executed as a daemon
    otherwise it returns an error code.
    
    crtools returns 0 only if cr_page_server() returns a positive value,
    what is obviously wrong.
    
    travis-ci: success for crtools: close a signal descriptor after passing a preparation stage (rev6)
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/crtools.c
+++ criu/crtools.c
@@ -775,7 +775,7 @@ int main(int argc, char *argv[], char *envp[])
 	}
 
 	if (!strcmp(argv[optind], "page-server"))
-		return cr_page_server(opts.daemon_mode, -1) > 0 ? 0 : 1;
+		return cr_page_server(opts.daemon_mode, -1) != 0;
 
 	if (!strcmp(argv[optind], "service"))
 		return cr_service(opts.daemon_mode);
