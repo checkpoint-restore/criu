61f78860d29ace86616668954f7b77ad5424095c
rbruno@gsd.inesc-id.pt <rbruno@gsd.inesc-id.pt>
2017-02-11 04:34:43 +0100

    Process Migration using Sockets (p1)
    This patch introduces the --remote option and the necessary code changes to
    support it. This leaves user the option to decide if the checkpoint data is to
    be stored on disk or sent through the network (through the image-proxy).
    The latter forwards the data to the destination node where image-cache
    receives it.
    
    The overall communication is performed as follows:
    src_node CRIU dump -> (sends images through UNIX sockets) ->      image-proxy
    								       |
    								       V
    dst_node: CRIU restore <- (receives images through UNIX sockets)<- image-cache
    
    Communication between image-proxy and image-cache is done through a single
    TCP connection.
    
    Running criu with --remote option is like this:
    
    dst_node# criu image-cache -d --port <port> -o /tmp/image-cache.log
    dst_node# criu restore --remote -o /tmp/image-cache.log
    src_node# criu image-proxy -d --port <port> --address <dst_node> -o /tmp/image-proxy.log
    src_node# criu dump -t <pid> --remote -o /tmp/dump.log
    
        [ xemul:
    here's the list of what should be done with the cache/proxy
    in order to have them merged into master.
    
    0. Document the whole thing :)
       Please, add articles for newly introduced actions and options to
       https://criu.org/CLI page.
       Also, it would be good to have an article describing the protocols
       involved.
    
    1. Make the unix sockets reside in work-dir.
       The good thing is that we've get rid of the socket name option :)
       But looking at do_open_remote_image() I see that it fchdir-s to
       image dir before connecting to proxy/cache. Better solution is to
       put the socket into workdir.
    
       1a. After this the option -D|--images-dir should become optional.
           Provided the --remote is given CRIU should work purely on the
           work-dir and not generate anything in the images-dir.
    
    2. Tune up the image_cache and image_proxy commands to accept the
       --status-fd and --pidfile options.
       Presumably the very cr_daemon() call should be equipped with
       everything that should be done for daemonizing and proxy/cache
       tasks should just call it :)
    
    3. Fix local connections not to generate per-image threads. There
       can be many images and it's not nice to stress the system with
       such amount of threads. Please, look at how criu/uffd.c manages
       multiple descriptors with page-faults using the epoll stuff.
    
       3a. The accept_remote_image_connections() seem not to work well
           with opts.ps_socket scenario as the former just calls accept()
           on whatever socket is passed there, while the opts.ps_socket
           is already an established socket for data transfer.
    
    4. No strings in protocol. Now the hard-coded "RESTORE_FINISH" string
       (and DUMP_FINISHED one) is used to terminate the communication.
       Need to tune up the protobuf objects to send boolean (or integer)
       EOF sign rather that the string.
    
    5. Check how proxy/cache works with incremental dumps. Looking at the
       skip_remote_bytes() I think that image-cache and -proxy still do not
       work well with stacked pages images. Probably for those we'll need
       the page-server or lazy-pages -like protocol that would request the
       needed regions and receive it back rather than read bytes from
       sockets simply to skip those.
    
    6. Add support for cache/proxy into go-phaul code. I haven't yet finished
       with the prototype, but plan to do it soon, so once the above steps
       are done we'll be able to proceed with this one.
    
    ]
    
    Signed-off-by: Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
    Signed-off-by: Katerina Koukiou <k.koukiou@gmail.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/include/protobuf-desc.h
+++ criu/include/protobuf-desc.h
@@ -62,8 +62,12 @@ enum {
 	PB_GHOST_CHUNK,
 	PB_FILE,
 	PB_MEMFD_FILE,
-	PB_MEMFD_INODE,		/* 60 */
+	PB_MEMFD_INODE,
 	PB_TIMENS,
+	PB_REMOTE_IMAGE,        /* Header for images sent from proxy to cache.*/
+	PB_LOCAL_IMAGE,         /* 60 */ /* Header for reading/writing images from/to proxy or cache. */
+	PB_LOCAL_IMAGE_REPLY,	/* Header for reading/writing images reply. */
+	PB_SNAPSHOT_ID,         /* Contains a single id. Used for reading/writing ids from proxy or cache. */
 
 	/* PB_AUTOGEN_STOP */
 
--- criu/pagemap.c
+++ criu/pagemap.c
@@ -18,6 +18,7 @@
 #include "xmalloc.h"
 #include "protobuf.h"
 #include "images/pagemap.pb-c.h"
+#include "img-remote.h"
 
 #ifndef SEEK_DATA
 #define SEEK_DATA	3
@@ -143,8 +144,12 @@ static void skip_pagemap_pages(struct page_read *pr, unsigned long len)
 	if (!len)
 		return;
 
-	if (pagemap_present(pr->pe))
+	if (pagemap_present(pr->pe)) {
+		if (opts.remote)
+			if (skip_remote_bytes(img_raw_fd(pr->pi), len))
+				pr_perror("Error skipping remote bytes");
 		pr->pi_off += len;
+	}
 	pr->cvaddr += len;
 }
 
@@ -161,7 +166,7 @@ static int seek_pagemap(struct page_read *pr, unsigned long vaddr)
 			break;
 
 		if (vaddr >= start && vaddr < end) {
-			skip_pagemap_pages(pr, vaddr - pr->cvaddr);
+			skip_pagemap_pages(pr, vaddr > pr->cvaddr ? vaddr - pr->cvaddr : 0);
 			return 1;
 		}
 
@@ -393,7 +398,7 @@ static int maybe_read_page_local(struct page_read *pr, unsigned long vaddr,
 	 * for us for urgent async read, just do the regular
 	 * cached read.
 	 */
-	if ((flags & (PR_ASYNC|PR_ASAP)) == PR_ASYNC)
+	if ((flags & (PR_ASYNC|PR_ASAP)) == PR_ASYNC && !opts.remote)
 		ret = pagemap_enqueue_iovec(pr, buf, len, &pr->async);
 	else {
 		ret = read_local_page(pr, vaddr, len, buf);
@@ -601,9 +606,24 @@ static int try_open_parent(int dfd, unsigned long id, struct page_read *pr, int 
 	int pfd, ret;
 	struct page_read *parent = NULL;
 
-	pfd = openat(dfd, CR_PARENT_LINK, O_RDONLY);
-	if (pfd < 0 && errno == ENOENT)
-		goto out;
+	if (opts.remote) {
+		/* Note: we are replacing a real directory FD for a snapshot_id
+		 * index. Since we need the parent of the current snapshot_id,
+		 * we want the current snapshot_id index minus one. It is
+		 * possible that dfd is already a snapshot_id index. We test it
+		 * by comparing it to the service FD. When opening an image (see
+		 * do_open_image) we convert the snapshot_id index into a real
+		 * snapshot_id.
+		 */
+		pfd = dfd == get_service_fd(IMG_FD_OFF) ?
+			get_curr_snapshot_id_idx() - 1 : dfd - 1;
+		if (pfd < 0)
+			goto out;
+	} else {
+		pfd = openat(dfd, CR_PARENT_LINK, O_RDONLY);
+		if (pfd < 0 && errno == ENOENT)
+			goto out;
+	}
 
 	parent = xmalloc(sizeof(*parent));
 	if (!parent)
@@ -618,7 +638,8 @@ static int try_open_parent(int dfd, unsigned long id, struct page_read *pr, int 
 		parent = NULL;
 	}
 
-	close(pfd);
+	if (!opts.remote)
+		close(pfd);
 out:
 	pr->parent = parent;
 	return 0;
@@ -626,7 +647,8 @@ out:
 err_free:
 	xfree(parent);
 err_cl:
-	close(pfd);
+	if (!opts.remote)
+		close(pfd);
 	return -1;
 }
 
@@ -657,7 +679,18 @@ static int init_pagemaps(struct page_read *pr)
 	off_t fsize;
 	int nr_pmes, nr_realloc;
 
-	fsize = img_raw_size(pr->pmi);
+	if (!opts.remote)
+		fsize = img_raw_size(pr->pmi);
+	else
+		/*
+		 * FIXME - There is no easy way to estimate the size of the
+		 * pagemap that is still to be read from the socket. Possible
+		 * solution is to ask Image Proxy or Image Cache about the size
+		 * of the image. 1024 is a wild guess (more space is allocated
+		 * if needed).
+		 */
+		fsize = 1024;
+
 	if (fsize < 0)
 		return -1;
 
