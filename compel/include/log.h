8ffed5b4fe69c2baaa1d70a871abfdb5946550a4
Kir Kolyshkin <kir@openvz.org>
2017-02-13 16:59:35 -0800

    compel/include/log.h: whitespace cleanup
    Those macros look twice as long as they should be on my 80-columns
    terminal. As there is nothing here to justify such width, go ahead
    and remove the extra tabs, keeping the code within 80 cols.
    
    Signed-off-by: Kir Kolyshkin <kir@openvz.org>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- compel/include/log.h
+++ compel/include/log.h
@@ -13,49 +13,50 @@ static inline int pr_quelled(unsigned int loglevel)
 	return compel_log_get_loglevel() < loglevel && loglevel != LOG_MSG;
 }
 
-extern void compel_print_on_level(unsigned int loglevel, const char *format, ...);
+extern void compel_print_on_level(unsigned int loglevel,
+		const char *format, ...);
 
-#define pr_msg(fmt, ...)							\
-	compel_print_on_level(LOG_MSG,						\
+#define pr_msg(fmt, ...)						\
+	compel_print_on_level(LOG_MSG,					\
 		       fmt, ##__VA_ARGS__)
 
-#define pr_info(fmt, ...)							\
-	compel_print_on_level(LOG_INFO,						\
+#define pr_info(fmt, ...)						\
+	compel_print_on_level(LOG_INFO,					\
 		       LOG_PREFIX fmt, ##__VA_ARGS__)
 
-#define pr_err(fmt, ...)							\
-	compel_print_on_level(LOG_ERROR,					\
-		       "Error (%s:%d): " LOG_PREFIX fmt,			\
+#define pr_err(fmt, ...)						\
+	compel_print_on_level(LOG_ERROR,				\
+		       "Error (%s:%d): " LOG_PREFIX fmt,		\
 		       __FILE__, __LINE__, ##__VA_ARGS__)
 
-#define pr_err_once(fmt, ...)							\
-	do {									\
-		static bool __printed;						\
-		if (!__printed) {						\
-			pr_err(fmt, ##__VA_ARGS__);				\
-			__printed = 1;						\
-		}								\
+#define pr_err_once(fmt, ...)						\
+	do {								\
+		static bool __printed;					\
+		if (!__printed) {					\
+			pr_err(fmt, ##__VA_ARGS__);			\
+			__printed = 1;					\
+		}							\
 	} while (0)
 
-#define pr_warn(fmt, ...)							\
-	compel_print_on_level(LOG_WARN,						\
-		       "Warn  (%s:%d): " LOG_PREFIX fmt,			\
+#define pr_warn(fmt, ...)						\
+	compel_print_on_level(LOG_WARN,					\
+		       "Warn  (%s:%d): " LOG_PREFIX fmt,		\
 		       __FILE__, __LINE__, ##__VA_ARGS__)
 
-#define pr_warn_once(fmt, ...)							\
-	do {									\
-		static bool __printed;						\
-		if (!__printed) {						\
-			pr_warn(fmt, ##__VA_ARGS__);				\
-			__printed = 1;						\
-		}								\
+#define pr_warn_once(fmt, ...)						\
+	do {								\
+		static bool __printed;					\
+		if (!__printed) {					\
+			pr_warn(fmt, ##__VA_ARGS__);			\
+			__printed = 1;					\
+		}							\
 	} while (0)
 
-#define pr_debug(fmt, ...)							\
-	compel_print_on_level(LOG_DEBUG,					\
+#define pr_debug(fmt, ...)						\
+	compel_print_on_level(LOG_DEBUG,				\
 		       LOG_PREFIX fmt, ##__VA_ARGS__)
 
-#define pr_perror(fmt, ...)							\
+#define pr_perror(fmt, ...)						\
 	pr_err(fmt ": %m\n", ##__VA_ARGS__)
 
 #endif /* COMPEL_LOG_H__ */
