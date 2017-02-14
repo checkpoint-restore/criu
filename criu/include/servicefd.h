2b6ed5bffc7b2b5d32e076529ceaf7d276b7460b
Andrei Vagin <avagin@virtuozzo.com>
2017-02-15 02:59:28 +0300

    util: move open_proc_fd to service_fd
    We need this to avoid conflicts with file descriptors,
    which has to be restored.
    
    Currently open_proc_pid() doesn't used during restoring
    file descriptors, but we are going to use it to restore
    sockets in proper network namespaces.
    
    Acked-by: Pavel Emelyanov <xemul@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/include/servicefd.h
+++ criu/include/servicefd.h
@@ -9,6 +9,7 @@ enum sfd_type {
 	LOG_FD_OFF,
 	IMG_FD_OFF,
 	PROC_FD_OFF,	/* fd with /proc for all proc_ calls */
+	PROC_PID_FD_OFF,
 	CTL_TTY_OFF,
 	SELF_STDIN_OFF,
 	CR_PROC_FD_OFF, /* some other's proc fd.
