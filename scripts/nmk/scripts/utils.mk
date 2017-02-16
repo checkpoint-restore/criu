1510b248687f978e614b0bc8c08c0027c729c8a3
Kir Kolyshkin <kir@openvz.org>
2017-02-15 23:20:26 -0800

    nmk: clean gcov files, too
    When criu is compiled with GCOV=1, .gc* files created are never
    cleaned. While 'git clean -dxf' does the job and is popular
    among the criu devs, it still feels good to have good ol'
    make clean doing the right thing.
    
    Reviewed-by: Dmitry Safonov <dsafonov@virtuozzo.com>
    Signed-off-by: Kir Kolyshkin <kir@openvz.org>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- scripts/nmk/scripts/utils.mk
+++ scripts/nmk/scripts/utils.mk
@@ -19,7 +19,7 @@ uniq = $(strip $(if $1,$(firstword $1) $(call uniq,$(filter-out $(firstword $1),
 objectify = $(foreach o,$(sort $(call uniq,$(1))),$(if $(filter /% ./% ../%,$(o)),$(o),$(obj)/$(o)))
 
 # To cleanup entries.
-cleanify = $(foreach o,$(sort $(call uniq,$(1))),$(o) $(o:.o=.d) $(o:.o=.i) $(o:.o=.s))
+cleanify = $(foreach o,$(sort $(call uniq,$(1))),$(o) $(o:.o=.d) $(o:.o=.i) $(o:.o=.s) $(o:.o=.gcda) $(o:.o=.gcno))
 
 #
 # Footer.
