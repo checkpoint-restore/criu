a5752133c7327a9889092d45f485848471ed5bad
Pavel Emelyanov <xemul@virtuozzo.com>
2017-03-06 21:38:29 +0300

    compel: Add recv fd helper
    The same for libcompel.so user.
    
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- compel/include/uapi/infect-util.h
+++ compel/include/uapi/infect-util.h
@@ -2,4 +2,5 @@
 #define __COMPEL_INFECT_UTIL_H__
 struct parasite_ctl;
 extern int compel_util_send_fd(struct parasite_ctl *ctl, int fd);
+extern int compel_util_recv_fd(struct parasite_ctl *ctl, int *pfd);
 #endif
--- compel/src/lib/infect-util.c
+++ compel/src/lib/infect-util.c
@@ -19,3 +19,14 @@ int compel_util_send_fd(struct parasite_ctl *ctl, int fd)
 	return 0;
 }
 
+int compel_util_recv_fd(struct parasite_ctl *ctl, int *pfd)
+{
+	int sk;
+
+	sk = compel_rpc_sock(ctl);
+	if ((*pfd = recv_fd(sk)) < 0) {
+		pr_perror("Can't send file descriptor");
+		return -1;
+	}
+	return 0;
+}
