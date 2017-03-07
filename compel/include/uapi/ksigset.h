72557edb1506fcab56a6c1390070c3fbee4cdeb6
Dmitry Safonov <dsafonov@virtuozzo.com>
2017-03-07 22:37:17 +0300

    compel/ksigset: fix ksigaddset() UB and flushing mask
    Found by Coverity error:
    > CID 172193 (#1 of 1): Bad bit shift operation (BAD_SHIFT)
    > 1. large_shift: In expression 1 << sig % 64, left shifting
    > by more than 31 bits has undefined behavior. The shift amount,
    > sig % 64, is as much as 63.
    
    That is:
    1. yes, UB
    2. while adding a signal to mask, this has flushed all other
       signals from mask.
    
    Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- compel/include/uapi/ksigset.h
+++ compel/include/uapi/ksigset.h
@@ -20,6 +20,6 @@ static inline void ksigemptyset(k_rtsigset_t *set)
 static inline void ksigaddset(k_rtsigset_t *set, int _sig)
 {
 	int sig = _sig - 1;
-	set->sig[sig / _NSIG_BPW] = 1 << (sig % _NSIG_BPW);
+	set->sig[sig / _NSIG_BPW] |= 1UL << (sig % _NSIG_BPW);
 }
 #endif
