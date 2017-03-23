55a140d48d792099f086d57a4f6568081e152821
Kir Kolyshkin <kir@openvz.org>
2017-03-23 15:02:04 -0700

    Correct an error message after waitpid
    Usually we print the PID we wait for, let's do the same here.
    Probably just a typo.
    
    Cc: Kirill Tkhai <ktkhai@virtuozzo.com>
    Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Kir Kolyshkin <kir@openvz.org>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/namespaces.c
+++ criu/namespaces.c
@@ -896,7 +896,7 @@ static int check_user_ns(int pid)
 	}
 
 	if (waitpid(chld, &status, 0) != chld) {
-		pr_perror("Unable to wait the %d process", pid);
+		pr_perror("Unable to wait for PID %d", chld);
 		return -1;
 	}
 
