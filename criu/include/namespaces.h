749da7a9737034f49febeb806fb11d35e19cf1ab
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-04-10 11:19:11 +0300

    ns: Add MAX_NS_NESTING
    It's maximum number of levels of namespaces found in linux kernel.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- criu/include/namespaces.h
+++ criu/include/namespaces.h
@@ -42,6 +42,7 @@
 /* Nested namespaces are supported only for these types */
 #define CLONE_SUBNS	(CLONE_NEWNS | CLONE_NEWNET | CLONE_NEWUSER)
 
+#define MAX_NS_NESTING	32
 #define EXTRA_SIZE	20
 
 #ifndef NSIO
