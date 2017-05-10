1da22477446965768ff3abcd5f085a33ff44578f
Cyrill Gorcunov <gorcunov@virtuozzo.com>
2017-05-10 16:52:03 +0300

    dump: Show task comm early
    When error happens on file dumping stage the only information about the
    task we dumping is its PID. For debug purpose show task's @comm early.
    
    It proves useful when trying to understand which of dumped applications
    is "guilty" in brokern dump when pid is not there anymore.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- criu/include/seize.h
+++ criu/include/seize.h
@@ -6,4 +6,7 @@ extern void pstree_switch_state(struct pstree_item *root_item, int st);
 extern const char *get_real_freezer_state(void);
 extern bool alarm_timeouted(void);
 
+extern char *task_comm_info(pid_t pid, char *comm, size_t size);
+extern char *__task_comm_info(pid_t pid);
+
 #endif
