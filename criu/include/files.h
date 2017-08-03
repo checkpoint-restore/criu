a95f68e08273aa79a72e9a71590c1814ac878bba
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-08-03 17:14:30 +0300

    files: Reorder fdinfo_list_entry fields
    Keep int pid with short-len fields to avoid
    alignment gaps in structure.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/include/files.h
+++ criu/include/files.h
@@ -83,8 +83,8 @@ struct fdinfo_list_entry {
 	struct list_head	desc_list;	/* To chain on  @fd_info_head */
 	struct file_desc	*desc;		/* Associated file descriptor */
 	struct list_head	ps_list;	/* To chain  per-task files */
-	int			pid;
 	FdinfoEntry		*fe;
+	int			pid;
 	u8			received:1;
 	u8			stage:3;
 	u8			fake:1;
