061a407ce3fdf6a3e3398b43c8c787a1a27d0991
Andrei Vagin <avagin@virtuozzo.com>
2017-10-02 21:45:48 +0300

    alpine: compile all tests
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- scripts/travis/asan.sh
+++ scripts/travis/asan.sh
@@ -8,7 +8,7 @@ chmod 0777 test
 chmod 0777 test/zdtm/transition/
 chmod 0777 test/zdtm/static
 
-./test/zdtm.py run -a --keep-going -k always --parallel 4 -x zdtm/static/rtc
+./test/zdtm.py run -a --keep-going -k always --parallel 4 -x zdtm/static/rtc "$@"
 ret=$?
 
 for i in `find / -name 'asan.log*'`; do
