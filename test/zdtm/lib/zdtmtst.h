820bad96290d61507f8aeec66f76170deab8445d
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-12-28 13:50:37 +0300

    zdtm: Export sys_clone_unified() to headers
    Make it possible to use this function by tests.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- test/zdtm/lib/zdtmtst.h
+++ test/zdtm/lib/zdtmtst.h
@@ -151,5 +151,7 @@ struct zdtm_tcp_opts {
 };
 
 extern int tcp_init_server_with_opts(int family, int *port, struct zdtm_tcp_opts *opts);
+extern pid_t sys_clone_unified(unsigned long flags, void *child_stack, void *parent_tid,
+			       void *child_tid, unsigned long newtls);
 
 #endif /* _VIMITESU_H_ */
