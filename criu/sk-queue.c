1b3a0c8048fe58fe3c36977dad40ff3cc5d764d6
Andrei Vagin <avagin@virtuozzo.com>
2018-03-07 18:44:04 +0300

    criu: add more information about control messages
    We need to know a type of unsupported control messages.
    
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>
    
    (cherry picked from commit 4ab24ee8840b4d879147442f4c65bb7eac77a8bb)
    Signed-off-by: Ahmed Elaidy <elaidya225@gmail.com>
    Reviewed-by: Alexander Mikhalitsyn <aleksandr.mikhalitsyn@futurfusion.io>

--- criu/sk-queue.c
+++ criu/sk-queue.c
@@ -211,6 +211,8 @@ static int dump_packet_cmsg(struct msghdr *mh, SkPacketEntry *pe, int flags)
 			continue;
 		}
 
+		pr_err("cmsg: len %lu type %d level %d\n",
+		       (unsigned long)ch->cmsg_len, ch->cmsg_type, ch->cmsg_level);
 		pr_err("Control messages in queue, not supported\n");
 		return -1;
 	}
