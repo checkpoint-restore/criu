85fd6abea4ac921fd1cb89ae1c1bad409456b73e
Mike Rapoport <rppt@linux.vnet.ibm.com>
2018-04-16 19:58:57 +0300

    test: lazy-pages: exclude maps007
    With userfaultfd we cannot reliably service process_vm_readv calls. The
    maps007 test that uses these calls passed previously by sheer luck.
    
    Signed-off-by: Mike Rapoport <rppt@linux.vnet.ibm.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- test/jenkins/criu-lazy-pages.sh
+++ test/jenkins/criu-lazy-pages.sh
@@ -6,7 +6,7 @@ prep
 KERN_MAJ=`uname -r | cut -d. -f1`
 KERN_MIN=`uname -r | cut -d. -f2`
 if [ $KERN_MAJ -ge "4" ] && [ $KERN_MIN -ge "11" ]; then
-	LAZY_EXCLUDE="-x cmdlinenv00"
+	LAZY_EXCLUDE="-x cmdlinenv00 -x maps007"
 else
 	LAZY_EXCLUDE="-x maps007 -x fork -x fork2 -x uffd-events -x cgroupns
 		      -x socket_listen -x socket_listen6 -x cmdlinenv00
