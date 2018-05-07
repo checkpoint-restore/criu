64ec202356aab0f3ebd58d0150e2a57e3ce27976
Cyrill Gorcunov <gorcunov@gmail.com>
2018-05-07 11:42:48 +0300

    seccomp: Dont forget to suspend filtering on threads
    When considering if we to call PTRACE_O_SUSPEND_SECCOMP
    on the tid we should take into account if there at least
    one thread which has seccomp mode enabled, otherwise
    we might miss filter suspension and restore procedure
    might break due to own criu syscall get filtered out.
    
    Same time we should move seccomp restore for threads
    to take place after CR_STATE_RESTORE_SIGCHLD state
    so that main criu code will attach to threads and
    setup seccomp suspension flag before we start
    restoring the filters.
    
    Reported-by: Andrei Vagin <avagin@virtuozzo.com>
    Reviewed-by: Dmitry Safonov <0x7f454c46@gmail.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/pie/restorer.c
+++ criu/pie/restorer.c
@@ -553,17 +553,6 @@ long __export_restore_thread(struct thread_restore_args *args)
 	if (restore_thread_common(args))
 		goto core_restore_end;
 
-	/*
-	 * Make sure it's before creds, since it's privileged
-	 * operation bound to uid 0 in current user ns.
-	 */
-	if (restore_seccomp(args))
-		goto core_restore_end;
-
-	ret = restore_creds(args->creds_args, args->ta->proc_fd);
-	if (ret)
-		goto core_restore_end;
-
 	ret = restore_dumpable_flag(&args->ta->mm);
 	if (ret)
 		goto core_restore_end;
@@ -578,6 +567,16 @@ long __export_restore_thread(struct thread_restore_args *args)
 	restore_finish_stage(task_entries_local, CR_STATE_RESTORE_SIGCHLD);
 	restore_pdeath_sig(args);
 
+	/*
+	 * Make sure it's before creds, since it's privileged
+	 * operation bound to uid 0 in current user ns.
+	 */
+	if (restore_seccomp(args))
+		goto core_restore_end;
+
+	ret = restore_creds(args->creds_args, args->ta->proc_fd);
+	if (ret)
+		goto core_restore_end;
 	restore_finish_stage(task_entries_local, CR_STATE_RESTORE_CREDS);
 
 	futex_dec_and_wake(&thread_inprogress);
