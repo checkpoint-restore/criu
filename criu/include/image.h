2ea16be20271dae61156a157b9fabd542b328387
Dmitry Safonov <dima@arista.com>
2018-05-10 19:14:41 +0100

    criu: Remove PAGE_IMAGE_SIZE
    It's unused since commit fd3f33f5d23a ("headers: image.h -- Drop unused
    entries"), so let's remove it completely.
    
    Signed-off-by: Dmitry Safonov <dima@arista.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/include/image.h
+++ criu/include/image.h
@@ -12,11 +12,6 @@
 #include "log.h"
 #include "common/bug.h"
 
-#ifdef _ARCH_PPC64
-#define PAGE_IMAGE_SIZE	65536
-#else
-#define PAGE_IMAGE_SIZE	4096
-#endif /* _ARCH_PPC64 */
 #define PAGE_RSS	1
 #define PAGE_ANON	2
 
