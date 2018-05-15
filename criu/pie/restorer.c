e85799de68a8947ee6b81296b5988588ae279999
Cyrill Gorcunov <gorcunov@gmail.com>
2018-05-15 11:12:39 +0300

    seccomp: Move changed ptrace flags setup after creds
    Credential commitment affects dumpable and pdeath signals
    so we have to move their restore after the restore_creds,
    just like we have in __export_restore_task (ie for
    group leader).
    
    https://jira.sw.ru/browse/PSBM-84198
    
    Acked-by: Dmitry Safonov <0x7f454c46@gmail.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/pie/restorer.c
+++ criu/pie/restorer.c
@@ -574,10 +574,6 @@ long __export_restore_thread(struct thread_restore_args *args)
 		sys_close(fd);
 	}
 
-	ret = restore_dumpable_flag(&args->ta->mm);
-	if (ret)
-		goto core_restore_end;
-
 	pr_info("%ld: Restored\n", sys_gettid());
 
 	restore_finish_stage(task_entries_local, CR_STATE_RESTORE);
@@ -586,7 +582,6 @@ long __export_restore_thread(struct thread_restore_args *args)
 		goto core_restore_end;
 
 	restore_finish_stage(task_entries_local, CR_STATE_RESTORE_SIGCHLD);
-	restore_pdeath_sig(args);
 
 	/*
 	 * Make sure it's before creds, since it's privileged
@@ -596,8 +591,11 @@ long __export_restore_thread(struct thread_restore_args *args)
 		goto core_restore_end;
 
 	ret = restore_creds(args->creds_args, args->ta->proc_fd);
+	ret = ret || restore_dumpable_flag(&args->ta->mm);
+	ret = ret || restore_pdeath_sig(args);
 	if (ret)
 		goto core_restore_end;
+
 	restore_finish_stage(task_entries_local, CR_STATE_RESTORE_CREDS);
 
 	futex_dec_and_wake(&thread_inprogress);
