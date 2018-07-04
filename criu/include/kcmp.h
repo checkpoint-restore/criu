7c72478f979af7e305025d93dcb7054a145083b7
Cyrill Gorcunov <gorcunov@gmail.com>
2018-07-04 18:51:32 +0300

    kcmp: Add epoll definitions
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/include/kcmp.h
+++ criu/include/kcmp.h
@@ -1,6 +1,8 @@
 #ifndef __CR_KCMP_H__
 #define __CR_KCMP_H__
 
+#include <stdint.h>
+
 enum kcmp_type {
 	KCMP_FILE,
 	KCMP_VM,
@@ -9,8 +11,16 @@ enum kcmp_type {
 	KCMP_SIGHAND,
 	KCMP_IO,
 	KCMP_SYSVSEM,
+	KCMP_EPOLL_TFD,
 
 	KCMP_TYPES,
 };
+
+/* Slot for KCMP_EPOLL_TFD */
+typedef struct {
+	uint32_t	efd;	/* epoll file descriptor */
+	uint32_t	tfd;	/* target file number */
+	uint32_t	toff;	/* target offset within same numbered sequence */
+} kcmp_epoll_slot_t;
 
 #endif /* __CR_KCMP_H__ */
