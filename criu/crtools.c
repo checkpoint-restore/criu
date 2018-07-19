bc7d57ebadc00bff01e8e90ccc2600687eb6d025
Cyrill Gorcunov <gorcunov@gmail.com>
2018-07-19 15:47:29 +0300

    log: Setup loglevel early
    We're printing messages before the log file
    is set up, so instead of keeping silence lets
    print data in default outputs.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>
    Reviewed-by: Dmitry Safonov <0x7f454c46@gmail.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/crtools.c
+++ criu/crtools.c
@@ -700,6 +700,8 @@ int main(int argc, char *argv[], char *envp[])
 		}
 	}
 
+	log_set_loglevel(log_level);
+
 	if (early_init())
 		return -1;
 
@@ -796,7 +798,6 @@ int main(int argc, char *argv[], char *envp[])
 		return 1;
 	}
 
-	log_set_loglevel(log_level);
 
 	if (log_init(opts.output))
 		return 1;
