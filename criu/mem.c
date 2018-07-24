9bb44e6f11fc1c3a099ea77cc2795164d4f5ccfd
Pawel Stradomski <pstradomski@google.com>
2018-07-24 12:12:27 +0200

    Punch holes in input files when restoring anonymous non-shared memory if --auto-dedup is enabled.
    This reduces memory usage if image files are stored on tmpfs.
    
    Signed-off-by: Pawel Stradomski <pstradomski@google.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/mem.c
+++ criu/mem.c
@@ -1271,7 +1271,11 @@ static int prepare_vma_ios(struct pstree_item *t, struct task_restore_args *ta)
 {
 	struct cr_img *pages;
 
-	pages = open_image(CR_FD_PAGES, O_RSTR, rsti(t)->pages_img_id);
+	/* if auto-dedup is on we need RDWR mode to be able to punch holes
+	 * in the input files (in restorer.c)
+	 */
+	pages = open_image(CR_FD_PAGES, opts.auto_dedup ? O_RDWR : O_RSTR,
+				rsti(t)->pages_img_id);
 	if (!pages)
 		return -1;
 
