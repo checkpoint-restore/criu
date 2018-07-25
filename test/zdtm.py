8f1ba5892dd2b137a24e144febed2464f245ed1a
Andrei Vagin <avagin@virtuozzo.com>
2018-07-25 22:40:27 +0300

    test: check criu restore with --auto-dedup
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- test/zdtm.py
+++ test/zdtm.py
@@ -1130,6 +1130,9 @@ class criu:
 		if self.__remote:
 			r_opts += ["--remote"]
 
+		if self.__dedup:
+			r_opts += ["--auto-dedup"]
+
 		self.__prev_dump_iter = None
 		criu_dir = os.path.dirname(os.getcwd())
 		if os.getenv("GCOV"):
