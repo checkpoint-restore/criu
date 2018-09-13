072778c4acf847c8f5f15798bcfedb9f8d52db9b
Cyrill Gorcunov <gorcunov@gmail.com>
2018-09-13 22:57:53 +0300

    inet: raw -- Split lookup_socket helper
    Raw sockets may have arbitrary protocol so we
    need different helper to lookup socket.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>
    Signed-off-by: Andrei Vagin <avagin@gmail.com>

--- criu/include/sockets.h
+++ criu/include/sockets.h
@@ -45,7 +45,8 @@ extern int unix_note_scm_rights(int id_for, uint32_t *file_ids, int *fds, int n_
 
 extern struct collect_image_info netlink_sk_cinfo;
 
-extern struct socket_desc *lookup_socket(unsigned ino, int family, int proto);
+extern struct socket_desc *lookup_socket_ino(unsigned int ino, int family);
+extern struct socket_desc *lookup_socket(unsigned int ino, int family, int proto);
 
 extern const struct fdtype_ops unix_dump_ops;
 extern const struct fdtype_ops inet_dump_ops;
