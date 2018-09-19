6b8852cfcbd0d8cc7cfcfd0c0116708c9370c3a7
Cyrill Gorcunov <gorcunov@gmail.com>
2018-09-19 14:50:35 +0300

    test: static/socket-tcp -- Check for iptables success
    Reviewed-by: Dmitry Safonov <0x7f454c46@gmail.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- test/zdtm/static/socket-tcp.c
+++ test/zdtm/static/socket-tcp.c
@@ -76,7 +76,8 @@ int main(int argc, char **argv)
 		return 1;
 	if (system("iptables -w -A INPUT -i lo -p tcp -m state --state NEW,ESTABLISHED -j ACCEPT"))
 		return 1;
-	system("iptables -A INPUT -j DROP");
+	if (system("iptables -A INPUT -j DROP"))
+		return 1;
 #endif
 
 #ifdef ZDTM_TCP_LOCAL
