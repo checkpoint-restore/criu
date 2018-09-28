4e1c7f5134e2b88e69846f4d72b4fdabc803c5e1
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2018-09-28 16:11:35 +0300

    zdtm: check criu restore with --auto-dedup
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@gmail.com>

--- test/zdtm.py
+++ test/zdtm.py
@@ -1159,6 +1159,9 @@ class criu:
 		if self.__dedup:
 			r_opts += ["--auto-dedup"]
 
+		if self.__dedup:
+			r_opts += ["--auto-dedup"]
+
 		self.__prev_dump_iter = None
 		criu_dir = os.path.dirname(os.getcwd())
 		if os.getenv("GCOV"):
