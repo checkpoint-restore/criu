fc4c3fc3617a8c76c5db0b1be2c5f897252b7dfa
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2019-02-07 15:17:50 +0300

    ns/restore/image: do not read namespace images for non-namespaced case
    Images for mount and net namespaces are empty if ns does not belong to
    us, thus we don't need to collect on restore.
    
    By adding these checks we will eliminate suspicious messages in logs
    about lack of images:
    
    ./test/zdtm.py run -k always -f h -t zdtm/static/env00
    
    env00/54/2/restore.log:(00.000332) No mountpoints-5.img image
    env00/54/2/restore.log:(00.000342) No netns-2.img image
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- criu/mount.c
+++ criu/mount.c
@@ -2989,6 +2989,11 @@ int read_mnt_ns_img(void)
 	struct mount_info *pms = NULL;
 	struct ns_id *nsid;
 
+	if (!(root_ns_mask & CLONE_NEWNS)) {
+		mntinfo = NULL;
+		return 0;
+	}
+
 	for (nsid = ns_ids; nsid != NULL; nsid = nsid->next) {
 		if (nsid->nd != &mnt_ns_desc)
 			continue;
