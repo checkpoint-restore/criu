67a72f9082c77ecbd88a7c4e18bfd871321776f0
Mitul Karnik <mitulkarnik.92@gmail.com>
2019-03-16 14:52:04 +0000

    Removing Unneeded Return Value Assignments
    Removed return value assignment statements as they are not referenced or used
    anywhere after the assignment is done.
    
    Fixes #334: Removing Unneeded Assignments
    
    Signed-off-by: Mitul Karnik <mitulkarnik.92@gmail.com>
    Signed-off-by: Andrei Vagin <avagin@gmail.com>

--- test/zdtm/static/dumpable02.c
+++ test/zdtm/static/dumpable02.c
@@ -143,8 +143,8 @@ int main(int argc, char **argv)
 			return 1;
 		}
 
-		ret = execl(argv[0], "dumpable_server", NULL);
-		pr_perror("could not execv %s as a dumpable_server", argv[0]);
+		execl(argv[0], "dumpable_server", NULL);
+		pr_perror("could not execv %s as a dumpable_server\nError No: %d", argv[0], errno);
 		return 1;
 	}
 
