4418207ad68faf45ce187e981cd9b86efe6cb577
Cyrill Gorcunov <gorcunov@gmail.com>
2019-04-03 16:22:00 +0300

    sfd: Move service fds to separate file
    It has nothing to do with utils but
    rather a separate service engine.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>
    Signed-off-by: Andrei Vagin <avagin@gmail.com>

--- criu/include/servicefd.h
+++ criu/include/servicefd.h
@@ -8,17 +8,17 @@ enum sfd_type {
 
 	LOG_FD_OFF,
 	IMG_FD_OFF,
-	PROC_FD_OFF,	/* fd with /proc for all proc_ calls */
+	PROC_FD_OFF,		/* fd with /proc for all proc_ calls */
 	PROC_PID_FD_OFF,
-	CR_PROC_FD_OFF, /* some other's proc fd.
-			 *  For dump -- target ns' proc
-			 *  For restore -- CRIU ns' proc
-			 */
-	ROOT_FD_OFF,	/* Root of the namespace we dump/restore */
+	CR_PROC_FD_OFF,		/* some other's proc fd:
+				 *  - For dump -- target ns' proc
+				 *  - For restore -- CRIU ns' proc
+				 */
+	ROOT_FD_OFF,		/* Root of the namespace we dump/restore */
 	CGROUP_YARD,
-	USERNSD_SK,	/* Socket for usernsd */
-	NS_FD_OFF,	/* Node's net namespace fd */
-	TRANSPORT_FD_OFF, /* to transfer file descriptors */
+	USERNSD_SK,		/* Socket for usernsd */
+	NS_FD_OFF,		/* Node's net namespace fd */
+	TRANSPORT_FD_OFF,	/* to transfer file descriptors */
 	RPC_SK_OFF,
 	FDSTORE_SK_OFF,
 
@@ -28,14 +28,13 @@ enum sfd_type {
 struct pstree_item;
 extern bool sfds_protected;
 
-extern void set_proc_self_fd(int fd);
-extern int clone_service_fd(struct pstree_item *me);
 extern int init_service_fd(void);
 extern int get_service_fd(enum sfd_type type);
+extern bool is_any_service_fd(int fd);
+extern bool is_service_fd(int fd, enum sfd_type type);
+extern int service_fd_min_fd(struct pstree_item *item);
 extern int install_service_fd(enum sfd_type type, int fd);
 extern int close_service_fd(enum sfd_type type);
-extern bool is_service_fd(int fd, enum sfd_type type);
-extern bool is_any_service_fd(int fd);
-extern int service_fd_min_fd(struct pstree_item *);
+extern int clone_service_fd(struct pstree_item *me);
 
 #endif /* __CR_SERVICE_FD_H__ */
