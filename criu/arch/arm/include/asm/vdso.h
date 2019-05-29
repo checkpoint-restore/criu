debd2308a6d05ef859f77c6d34a454c6ef8806a7
Dmitry Safonov <dima@arista.com>
2019-05-29 17:15:05 +0100

    vdso/arm: Add vdso symbols from kernel
    Signed-off-by: Dmitry Safonov <dima@arista.com>

---
+++ criu/arch/arm/include/asm/vdso.h
@@ -0,0 +1,17 @@
+#ifndef __CR_ASM_VDSO_H__
+#define __CR_ASM_VDSO_H__
+
+#include "asm/int.h"
+#include "asm-generic/vdso.h"
+
+/* This definition is used in pie/util-vdso.c to initialize the vdso symbol
+ * name string table 'vdso_symbols'
+ *
+ * Poke from kernel file arch/arm/vdso/vdso.lds.S
+ */
+#define VDSO_SYMBOL_MAX		2
+#define ARCH_VDSO_SYMBOLS			\
+	"__vdso_clock_gettime",		\
+	"__vdso_gettimeofday"
+
+#endif /* __CR_ASM_VDSO_H__ */
