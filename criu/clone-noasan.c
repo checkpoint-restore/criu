27535b0a214e577309d483655664863385756aea
Adrian Reber <areber@redhat.com>
2019-12-16 10:42:13 +0000

    Use clone3() with set_tid to create processes
    With the in Linux Kernel 5.4 introduced clone3() with set_tid it is no
    longer necessary to write to to /proc/../ns_last_pid to influence the
    next PID number. clone3() can directly select a PID for the newly
    created process/thread.
    
    After checking for the availability of clone3() with set_tid and adding
    the assembler wrapper for clone3() in previous patches, this extends
    criu/pie/restorer.c and criu/clone-noasan.c to use the newly added
    assembler clone3() wrapper to create processes with a certain PID.
    
    This is a RFC and WIP, but I wanted to share it and run it through CI
    for feedback. As the CI will probably not use a 5.4 based kernel it
    should just keep on working as before.
    
    Signed-off-by: Adrian Reber <areber@redhat.com>

--- criu/clone-noasan.c
+++ criu/clone-noasan.c
@@ -1,4 +1,10 @@
+#include <stdlib.h>
 #include <sched.h>
+#include <unistd.h>
+
+#include <compel/plugins/std/syscall-codes.h>
+
+#include "sched.h"
 #include "common/compiler.h"
 #include "log.h"
 #include "common/bug.h"
@@ -31,10 +37,36 @@
 int clone_noasan(int (*fn)(void *), int flags, void *arg)
 {
 	void *stack_ptr = (void *)round_down((unsigned long)&stack_ptr - 1024, 16);
+
 	BUG_ON((flags & CLONE_VM) && !(flags & CLONE_VFORK));
 	/*
 	 * Reserve some bytes for clone() internal needs
 	 * and use as stack the address above this area.
 	 */
 	return clone(fn, stack_ptr, flags, arg);
+}
+
+int clone3_with_pid_noasan(int (*fn)(void *), void *arg, int flags,
+			   int exit_signal, pid_t pid)
+{
+	struct _clone_args c_args = {};
+
+	BUG_ON(flags & CLONE_VM);
+
+	/*
+	 * Make sure no child signals are requested. clone3() uses
+	 * exit_signal for that.
+	 */
+	BUG_ON(flags & 0xff);
+
+	pr_debug("Creating process using clone3()\n");
+
+	c_args.exit_signal = exit_signal;
+	c_args.flags = flags;
+	c_args.set_tid = ptr_to_u64(&pid);
+	c_args.set_tid_size = 1;
+	pid = syscall(__NR_clone3, &c_args, sizeof(c_args));
+	if (pid == 0)
+		exit(fn(arg));
+	return pid;
 }
