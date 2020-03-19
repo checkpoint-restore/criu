c89039f39a78f06d44bf9fae29c24caf9f1635a7
Prakriti <prakritigoyal19@gmail.com>
2020-03-19 23:06:04 +0530

    Moved sigact dump/restore code into separate file

--- criu/include/sigaction.h
+++ criu/include/sigaction.h
@@ -6,7 +6,6 @@
 #include "images/core.pb-c.h"
 #include <compel/plugins/std/syscall-codes.h>
 #include "compel/include/asm/syscall.h"
-#include "compel/include/asm/syscall-types.h"
 
 extern rt_sigaction_t sigchld_act;
 
