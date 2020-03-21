06dfeb97301826e8ee094c1938361224708883c4
Andrei Vagin <avagin@gmail.com>
2020-03-21 22:08:38 +0300

    test/pipe03: check that pipe size is restored
    Create two pipes with and without queued data.
    
    Signed-off-by: Andrei Vagin <avagin@gmail.com>

--- test/zdtm/static/pipe03.c
+++ test/zdtm/static/pipe03.c
@@ -13,27 +13,28 @@ const char *test_author	= "Andrei Vagin <avagin@gmail.com>";
 
 int main(int argc, char **argv)
 {
-	int p[2], i;
+	int p[2][2], i;
 	uint8_t buf[BUF_SIZE];
 	uint32_t crc;
 
 	test_init(argc, argv);
 
-	if (pipe2(p, O_NONBLOCK)) {
-		pr_perror("pipe");
-		return 1;
-	}
-
-	if (fcntl(p[1], F_SETPIPE_SZ, DATA_SIZE) == -1) {
-		pr_perror("Unable to change a pipe size");
-		return 1;
+	for (i = 0; i < 2; i++) {
+		if (pipe2(p[i], O_NONBLOCK)) {
+			pr_perror("pipe");
+			return 1;
+		}
+		if (fcntl(p[i][1], F_SETPIPE_SZ, DATA_SIZE) == -1) {
+			pr_perror("Unable to change a pipe size");
+			return 1;
+		}
 	}
 
 	crc = ~0;
 	datagen(buf, BUF_SIZE, &crc);
 
 	for (i = 0; i < DATA_SIZE / BUF_SIZE; i++) {
-		if (write(p[1], buf, BUF_SIZE) != BUF_SIZE) {
+		if (write(p[0][1], buf, BUF_SIZE) != BUF_SIZE) {
 			pr_perror("write");
 			return 1;
 		}
@@ -43,8 +44,22 @@ int main(int argc, char **argv)
 	test_waitsig();
 
 	for (i = 0; i < DATA_SIZE / BUF_SIZE; i++) {
-		if (read(p[0], buf, BUF_SIZE) != BUF_SIZE) {
+		if (read(p[0][0], buf, BUF_SIZE) != BUF_SIZE) {
 			pr_perror("read");
+			return 1;
+		}
+	}
+
+	for (i = 0; i < 2; i++) {
+		int size;
+
+		size = fcntl(p[i][1], F_GETPIPE_SZ);
+		if (size < 0) {
+			pr_perror("Unable to get a pipe size");
+			return 1;
+		}
+		if (size != DATA_SIZE) {
+			fail("%d: size %d expected %d", i, size, DATA_SIZE);
 			return 1;
 		}
 	}
