8c7c65fb68d20dd4a40acae9b08973e337bae36f
Guoyun Sun <sunguoyun@loongson.cn>
2020-04-08 10:24:45 +0800

    mips:criu/arch/mips: Add mips parts to criu
    Signed-off-by: Guoyun Sun <sunguoyun@loongson.cn>

---
+++ criu/arch/mips/include/asm/restore.h
@@ -0,0 +1,29 @@
+#ifndef __CR_ASM_RESTORE_H__
+#define __CR_ASM_RESTORE_H__
+
+#include "asm/restorer.h"
+#include "images/core.pb-c.h"
+
+#define JUMP_TO_RESTORER_BLOB(new_sp, restore_task_exec_start,		\
+			      task_args)				\
+	asm volatile(							\
+		     "move $4, %0				    \n" \
+		     "move $25, %1 				    \n" \
+		     "move $5, %2				    \n" \
+		     "move $29, $5				    \n" \
+		     "jalr $25   				    \n"		\
+		     "nop   				    \n"		\
+		     :							\
+		     :"r"(task_args),"r"(restore_task_exec_start),	\
+		      "g"(new_sp)			\
+		     : "$29", "$25", "$4","$5")
+
+static inline void core_get_tls(CoreEntry *pcore, tls_t *ptls)
+{
+	*ptls = pcore->ti_mips->tls;
+}
+
+
+int restore_fpu(struct rt_sigframe *sigframe, CoreEntry *core);
+
+#endif
---
+++ criu/arch/mips/sigaction_compat.c
@@ -0,0 +1,19 @@
+#include "log.h"
+#include "asm/restorer.h"
+#include <compel/asm/fpu.h>
+#include "asm/compat.h"
+#include <compel/plugins/std/syscall-codes.h>
+
+#ifdef CR_NOGLIBC
+# include <compel/plugins/std/string.h>
+#endif
+
+#include "cpu.h"
+
+extern char restore_rt_sigaction;
+
+int arch_compat_rt_sigaction(void *stack32, int sig, rt_sigaction_t_compat *act)
+{
+    return 0;
+}
+
