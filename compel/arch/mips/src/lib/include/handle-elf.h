042628b9a8d6eb44dacbfcb6ab6e1ed5a0fe512f
Guoyun Sun <sunguoyun@loongson.cn>
2020-04-08 10:13:22 +0800

    mips:compel/arch/mips: Add architecture support to compel tool and libraries
         This patch only adds the support but does not enable it for building.
    
    Signed-off-by: Guoyun Sun <sunguoyun@loongson.cn>

---
+++ compel/arch/mips/src/lib/include/handle-elf.h
@@ -0,0 +1,8 @@
+#ifndef COMPEL_HANDLE_ELF_H__
+#define COMPEL_HANDLE_ELF_H__
+
+#include "elf64-types.h"
+
+#define arch_is_machine_supported(e_machine)	(e_machine == EM_MIPS)
+
+#endif /* COMPEL_HANDLE_ELF_H__ */
