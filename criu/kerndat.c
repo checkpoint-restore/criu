281e29d4ff336dc7f58b3ac2065acb4d7ccec9c8
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2020-04-15 11:34:47 +0300

    kerndat remove duplicate call to kerndat_nsid()
    Func kerndat_nsid() is called twice.
    
    v2: leave kerndat_nsid call near kerndat_link_nsid
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- criu/kerndat.c
+++ criu/kerndat.c
@@ -1102,8 +1102,6 @@ int kerndat_init(void)
 	if (!ret)
 		ret = kerndat_socket_netns();
 	if (!ret)
-		ret = kerndat_nsid();
-	if (!ret)
 		ret = kerndat_x86_has_ptrace_fpu_xsave_bug();
 	if (!ret)
 		ret = kerndat_has_inotify_setnextwd();
