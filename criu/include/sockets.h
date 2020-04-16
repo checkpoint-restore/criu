ee9365f7a371a6afb3e277570da111636a3e40de
ZeyadYasser <zeyady98@gmail.com>
2020-04-16 15:58:18 +0200

    criu: optimize find_unix_sk_by_ino()
    Fixes: #339
    Replaced the linear search with a hashtable lookup.
    
    Signed-off-by: Zeyad Yasser <zeyady98@gmail.com>

--- criu/include/sockets.h
+++ criu/include/sockets.h
@@ -62,6 +62,8 @@ extern int unix_sk_id_add(unsigned int ino);
 extern int unix_sk_ids_parse(char *optarg);
 extern int unix_prepare_root_shared(void);
 
+extern void init_sk_info_hash(void);
+
 extern int do_dump_opt(int sk, int level, int name, void *val, int len);
 #define dump_opt(s, l, n, f)	do_dump_opt(s, l, n, f, sizeof(*f))
 extern int do_restore_opt(int sk, int level, int name, void *val, int len);
