0feb569610b32fbe49593370db59f179e618c143
Nicolas Viennot <Nicolas.Viennot@twosigma.com>
2020-05-21 17:40:17 +0000

    img-streamer: introduction of criu-image-streamer
    This adds the ability to stream images with criu-image-streamer
    
    The workflow is the following:
    1) criu-image-streamer is started, and starts listening on a UNIX
       socket.
    2) CRIU is started. img_streamer_init() is invoked, which connects to the
       socket. During dump/restore operations, instead of using local disk to
       open an image file, img_streamer_open() is called to provide a UNIX pipe
       that is sent over the UNIX socket.
    3) Once the operation is done, img_streamer_finish() is called, and the
       UNIX socket is disconnected.
    
    criu-image-streamer can be found at:
    https://github.com/checkpoint-restore/criu-image-streamer
    
    Signed-off-by: Nicolas Viennot <Nicolas.Viennot@twosigma.com>

--- criu/cr-dump.c
+++ criu/cr-dump.c
@@ -82,6 +82,7 @@
 #include "eventpoll.h"
 #include "memfd.h"
 #include "timens.h"
+#include "img-streamer.h"
 
 /*
  * Architectures can overwrite this function to restore register sets that
@@ -1759,6 +1760,7 @@ static int cr_dump_finish(int ret)
 	free_userns_maps();
 
 	close_service_fd(CR_PROC_FD_OFF);
+	close_image_dir();
 
 	if (ret) {
 		pr_err("Dumping FAILED.\n");
