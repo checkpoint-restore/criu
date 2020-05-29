7890a45aaa469aa311c8ea0ae911a3e18605f6b5
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2020-05-29 14:40:01 +0300

    mount: use ns_mountpoint in aufs_parse
    At this point ns_mountpoint is equal to mountpoint.
    
    Cherry-picked from Virtuozzo criu:
    https://src.openvz.org/projects/OVZ/repos/criu/commits/c70bd7de0
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- criu/proc_parse.c
+++ criu/proc_parse.c
@@ -2737,7 +2737,7 @@ int aufs_parse(struct mount_info *new)
 {
 	int ret = 0;
 
-	if (!strcmp(new->mountpoint, "./")) {
+	if (!strcmp(new->ns_mountpoint, "./")) {
 		opts.aufs = true;
 		ret = parse_aufs_branches(new);
 	}
