64e1f9882e9a1cdcf372c0ca52b4bea94c37d875
Andrei Vagin <avagin@gmail.com>
2020-06-19 10:33:49 +0300

    cgroup: Add the initial support for cgroup2
    This change is very straightforward. We don't skip the cgroup2
    controller and dump it as any other controllers.
    
    Signed-off-by: Andrei Vagin <avagin@gmail.com>

--- criu/proc_parse.c
+++ criu/proc_parse.c
@@ -2374,20 +2374,13 @@ int parse_cgroup_file(FILE *f, struct list_head *retl, unsigned int *n)
 		 * 2:name=systemd:/user.slice/user-1000.slice/session-1.scope
 		 */
 		name = strchr(buf, ':');
-		if (name) {
-			path = strchr(++name, ':');
-			if (*name == ':') {
-				/*
-				 * It's unified hierarchy. On kernels with legacy
-				 * tree this item is added automatically, so we
-				 * can just skip one. For those with full unified
-				 * support is on ... we need to write new code.
-				 */
-				xfree(ncc);
-				continue;
-			}
+		if (!name) {
+			pr_err("Failed parsing cgroup %s\n", buf);
+			xfree(ncc);
+			goto err;
 		}
-		if (!name || !path) {
+			path = strchr(++name, ':');
+		if (!path) {
 			pr_err("Failed parsing cgroup %s\n", buf);
 			xfree(ncc);
 			goto err;
@@ -2549,13 +2542,6 @@ int collect_controllers(struct list_head *cgroups, unsigned int *n_cgroups)
 			goto err;
 		}
 		controllers++;
-
-		if (*controllers == ':')
-			/*
-			 * Unified hier. See comment in parse_cgroup_file
-			 * for more details.
-			 */
-			continue;
 
 		off = strchr(controllers, ':');
 		if (!off) {
