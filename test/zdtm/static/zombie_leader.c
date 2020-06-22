99c84878374e90e52ed1723f12564a5d16b1281d
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2020-06-22 14:20:29 +0300

    zdtm: add zombie_leader test
    Create a session leader and it's child - session member, make leader
    zombie. To restore this criu will need to create a helper task a child
    of our zombie so that member can inherit session. Before fixes in this
    patchset we segfault on empty ids and fail to restore cgroups because of
    empty cg_set
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

---
+++ test/zdtm/static/zombie_leader.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+
+#include "zdtmtst.h"
+
+const char *test_doc	= "Check non-empty session with zombie leader";
+const char *test_author	= "Pavel Tikhomirov <ptikhomirov@virtuozzo.com>";
+
+int child(void)
+{
+	while (1)
+		sleep(1);
+
+	return 0;
+}
+
+int zombie_leader(int *cpid)
+{
+	int pid;
+
+	setsid();
+
+	pid = fork();
+	if (pid < 0) {
+		pr_perror("Failed to fork child");
+		return 1;
+	} else if (pid == 0) {
+		exit(child());
+	}
+
+	*cpid = pid;
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	int ret = -1, status;
+	int pid, *cpid;
+	siginfo_t infop;
+
+	test_init(argc, argv);
+
+	cpid = (int *)mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE,
+			   MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+	*cpid = 0;
+
+	pid = fork();
+	if (pid < 0) {
+		pr_perror("Failed to fork zombie");
+		return 1;
+	} else if (pid == 0) {
+		exit(zombie_leader(cpid));
+	}
+
+	if (waitid(P_PID, pid, &infop, WNOWAIT | WEXITED) < 0) {
+		pr_perror("Failed to waitid zombie");
+		goto err;
+	}
+
+	if (!*cpid) {
+		pr_err("Don't know grand child's pid");
+		goto err;
+	}
+
+	test_daemon();
+	test_waitsig();
+
+	ret = 0;
+err:
+	waitpid(pid, &status, 0);
+
+	if (*cpid)
+		kill(*cpid, SIGKILL);
+
+	if (!ret)
+		pass();
+
+	return 0;
+}
