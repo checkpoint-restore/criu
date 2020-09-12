dec64682d6a867c9a4b71fa6066546c86d9d9ab7
Abhishek Vijeev <abhishek.vijeev@gmail.com>
2020-09-12 17:12:38 +0530

    zdtm: adding BPF helper functions as a new library
    This commit adds BPF helper functions needed by tests in a new library.
    It defines new functions that allow verifying BPF map meta-data from
    the procfs as well as using the bpf() system call with
    BPF_OBJ_GET_INFO_BY_FD. It is necessary to verify from procfs and using
    BPF_OBJ_GET_INFO_BY_FD because the information available from both
    these places is disjoint (for example, checking whether a map is frozen
    cannot be performed with BPF_OBJ_GET_INFO_BY_FD).
    
    Source files modified:
    
    * test/zdtm/lib/Makefile - Generating build artifacts
    
    Source files added:
    
    * test/zdtm/lib/bpfmap_zdtm.c - Provides definitions for 3 new
    functions:
        (a) parse_bpfmap_fdinfo() - Parses information about the BPF map
        from procfs
        (b) cmp_bpf_map_info() - Compares the attributes of a BPF map file
        obtained from BPF_OBJ_GET_INFO_BY_FD. This function is typically
        used to verify that the attributes of a BPF map remain the same
        before checkpoint and after restore
        (c) cmp_bpfmap_fdinfo() - Compares the attributes of a BPF map file
        obtained from procfs. This function is typically used to verify
        that the attributes of a BPF map remain the same before checkpoint
        and after restore
    
    * test/zdtm/lib/bpfmap_zdtm.h - Structure and function declarations.
    Declares struct bpfmap_fdinfo_obj, which stores information about BPF
    maps parsed from procfs
    
    Signed-off-by: Abhishek Vijeev <abhishek.vijeev@gmail.com>

---
+++ test/zdtm/lib/bpfmap_zdtm.h
@@ -0,0 +1,21 @@
+#include <bpf/bpf.h>
+#include <inttypes.h>
+
+#include "zdtmtst.h"
+
+#define fdinfo_field(str, field)	!strncmp(str, field":", sizeof(field))
+
+struct bpfmap_fdinfo_obj {
+	uint32_t	map_type;
+	uint32_t	key_size;
+	uint32_t	value_size;
+	uint32_t	max_entries;
+	uint32_t	map_flags;
+	uint64_t	memlock;
+	uint32_t	map_id;
+	uint32_t	frozen;
+};
+
+extern int parse_bpfmap_fdinfo(int, struct bpfmap_fdinfo_obj *, uint32_t);
+extern int cmp_bpf_map_info(struct bpf_map_info *, struct bpf_map_info *);
+extern int cmp_bpfmap_fdinfo(struct bpfmap_fdinfo_obj *, struct bpfmap_fdinfo_obj *);
