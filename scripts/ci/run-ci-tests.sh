0103935ca8c0be07cedc98d98aad517f410751a5
Adrian Reber <areber@redhat.com>
2020-11-07 12:40:22 +0000

    ci: switch centos7 to github actions
    Signed-off-by: Adrian Reber <areber@redhat.com>

--- scripts/ci/run-ci-tests.sh
+++ scripts/ci/run-ci-tests.sh
@@ -193,7 +193,11 @@ if [ -z "$SKIP_EXT_DEV_TEST" ]; then
 fi
 #make -C test/others/exec/ run
 make -C test/others/make/ run CC="$CC"
-make -C test/others/shell-job/ run
+if [ -n "$TRAVIS" ]; then
+       # GitHub Actions does not provide a real TTY and CRIU will fail with:
+       # Error (criu/tty.c:1014): tty: Don't have tty to inherit session from, aborting
+       make -C test/others/shell-job/ run
+fi
 make -C test/others/rpc/ run
 
 ./test/zdtm.py run -t zdtm/static/env00 --sibling
@@ -223,5 +227,3 @@ make -C test/others/libcriu run
 
 # external namespace testing
 make -C test/others/ns_ext run
-
-make -C test/others/shell-job
