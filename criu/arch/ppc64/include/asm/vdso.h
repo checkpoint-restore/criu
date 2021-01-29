0945fbb4e5647985b73277b3f1c711537a0f2066
anatasluo <luolongjuna@gmail.com>
2021-01-29 13:48:57 +0000

    vdso: fix segmentation fault caused by char pointer array
    When I compile criu with "make DEBUG=1" and run it to restore my
    program, it produces a segmentation fault.
    
    In aarch64, with compile flag "-O0", when criu executes the code in pie,
    it is unable to visit the content of ARCH_VDSO_SYMBOLS. So I put these
    variables into the stack.
    
    Signed-off-by: anatasluo <luolongjuna@gmail.com>

--- criu/arch/ppc64/include/asm/vdso.h
+++ criu/arch/ppc64/include/asm/vdso.h
@@ -14,16 +14,28 @@
  */
 #define VDSO_SYMBOL_MAX		10
 #define VDSO_SYMBOL_GTOD	5
-#define ARCH_VDSO_SYMBOLS			\
-	"__kernel_clock_getres",		\
-	"__kernel_clock_gettime",		\
-	"__kernel_get_syscall_map",		\
-	"__kernel_get_tbfreq",			\
-	"__kernel_getcpu",			\
-	"__kernel_gettimeofday",		\
-	"__kernel_sigtramp_rt64",		\
-	"__kernel_sync_dicache",		\
-	"__kernel_sync_dicache_p5",		\
-	"__kernel_time"
+#define ARCH_VDSO_SYMBOLS_LIST \
+	const char* aarch_vdso_symbol1 = "__kernel_clock_getres"; \
+	const char* aarch_vdso_symbol2 = "__kernel_clock_gettime"; \
+	const char* aarch_vdso_symbol3 = "__kernel_get_syscall_map"; \
+	const char* aarch_vdso_symbol4 = "__kernel_get_tbfreq"; \
+	const char* aarch_vdso_symbol5 = "__kernel_getcpu"; \
+	const char* aarch_vdso_symbol6 = "__kernel_gettimeofday"; \
+	const char* aarch_vdso_symbol7 = "__kernel_sigtramp_rt64"; \
+	const char* aarch_vdso_symbol8 = "__kernel_sync_dicache"; \
+	const char* aarch_vdso_symbol9 = "__kernel_sync_dicache_p5"; \
+	const char* aarch_vdso_symbol10 = "__kernel_time";
+
+#define ARCH_VDSO_SYMBOLS \
+	aarch_vdso_symbol1, \
+	aarch_vdso_symbol2, \
+	aarch_vdso_symbol3, \
+	aarch_vdso_symbol4, \
+	aarch_vdso_symbol5, \
+	aarch_vdso_symbol6, \
+	aarch_vdso_symbol7, \
+	aarch_vdso_symbol8, \
+	aarch_vdso_symbol9, \
+	aarch_vdso_symbol10
 
 #endif /* __CR_ASM_VDSO_H__ */
