7accc44546a7e41d9107ba2733e8f2f4b851f5fe
Adrian Reber <areber@redhat.com>
2021-03-15 14:03:25 +0000

    test: fix crit test and extend it
    This fixes the others/crit test to work again and extends it to make
    sure all possible input and output options are correctly handled by
    crit.
    
    Signed-off-by: Adrian Reber <areber@redhat.com>

--- test/others/env.sh
+++ test/others/env.sh
@@ -2,7 +2,16 @@
 
 CRIU=$(readlink -f `dirname ${BASH_SOURCE[0]}`/../../criu/criu)
 criu=$CRIU
-CRIT=$(readlink -f `dirname ${BASH_SOURCE[0]}`/../../crit/crit)
+if [ $(which python3) ]; then
+	PYTHON=python3
+elif [ $(which python2) ]; then
+	PYTHON=python2
+else
+	echo "FAIL: Neither python3 nor python2"
+	exit 1
+fi
+#export PYTHON
+CRIT=$(readlink -f `dirname ${BASH_SOURCE[0]}`/../../crit/crit-"${PYTHON}")
 crit=$CRIT
 CRIU_COREDUMP=$(readlink -f `dirname ${BASH_SOURCE[0]}`/../../criu-coredump/criu-coredump)
 criu_coredump=$CRIU_COREDUMP
