24fa6f6f4f337ed36b1b10b21b58cc0b3756d7e9
Zeyad Yasser <zeyady98@gmail.com>
2021-03-16 15:12:10 +0200

    criu: add pidfd based pid reuse detection for RPC clients
    Closes: #717
    
    This increases the reliability of pid reuse detection using pidfds,
    currently through RPC migration tools like P.Haul.
    
    A connectionless unix socket is passed to criu in RPC mode through
    the RPC option pidfd_store_sk.
    If this option is set, the socket is initialized in
    init_pidfd_store_sk() to be used as a queue for task pidfds.
    criu then sends tasks pidfds to this socket in send_pidfd_entry()
    and receives them in the next pre-dump/dump iteration to build
    the pidfds hashtable in init_pidfd_store_hash().
    These pidfds will be used later in detect_pid_reuse().
    
    How it should be used in migration tools like P.Haul:
    	- Open a connectionless unix socket
    	- Pass the socket fd in the RPC option pidfd_store_sk when
    	  doing a pre-dump or dump
    
    This will fail if the kernel does not support pidfd_open or
    pidfd_getfd syscalls, so pidfd_store_sk should not be set if the
    kernel does not support pidfd_open.
    This could be checked with:
    	CLI: criu check --feature pidfd_store
    	RPC: CRIU_REQ_TYPE__FEATURE_CHECK and set pidfd_store to
    	     true in the "features" field of the request
    
    v2:
    	- add reasonable polling restart limit in check_pidfd_entry_state
    	  to avoid getting stuck
    	- avoid leaking pidfd in send_pidfd_entry when entry is NULL,
    	  otherwise pidfds are freed in free_pidfd_store
    
    v3:
    	- check that the passed pidfd store is not empty after
    	  the first iteration (i.e. --prev-images-dir option set).
    
    v4:
    	- clear pidfd_hash heads
    	- check entry allocation error in init_pidfd_store_hash()
    
    Signed-off-by: Zeyad Yasser <zeyady98@gmail.com>

--- criu/mem.c
+++ criu/mem.c
@@ -31,11 +31,20 @@
 #include "prctl.h"
 #include "compel/infect-util.h"
 #include "compel/plugins/std/syscall-codes.h"
+#include "common/scm.h"
 
 #include "protobuf.h"
 #include "images/pagemap.pb-c.h"
 
+struct pidfd_entry {
+	pid_t				pid;
+	int				pidfd;
+	struct hlist_node		hash; /* To lookup pidfd by pid */
+};
+
 static int pidfd_store_sk = -1;
+#define PIDFD_HASH_SIZE	32
+static struct hlist_head pidfd_hash[PIDFD_HASH_SIZE];
 
 int init_pidfd_store_sk(pid_t pid, int sk)
 {
@@ -129,6 +138,175 @@ int init_pidfd_store_sk(pid_t pid, int sk)
 err:
 	close_safe(&pidfd_store_sk);
 	return -1;
+}
+
+void free_pidfd_store(void)
+{
+	int i;
+	struct pidfd_entry *entry;
+	struct hlist_node *tmp;
+
+	for (i = 0; i < PIDFD_HASH_SIZE; i++) {
+		hlist_for_each_entry_safe(entry, tmp, &pidfd_hash[i], hash) {
+			close(entry->pidfd);
+			xfree(entry);
+		}
+		INIT_HLIST_HEAD(&pidfd_hash[i]);
+	}
+
+	close_safe(&pidfd_store_sk);
+}
+
+int init_pidfd_store_hash(void)
+{
+	int i, cnt, ret;
+	struct pidfd_entry *entry;
+
+	for (i = 0; i < PIDFD_HASH_SIZE; i++)
+		INIT_HLIST_HEAD(&pidfd_hash[i]);
+
+	/* Skip building pidfd_hash if pidfd_store_sk is not initialized */
+	if (pidfd_store_sk == -1)
+		return 0;
+
+	/*
+	 * Drain all queued pidfd entries in pidfd_store_sk from
+	 * the last predump into pidfd_hash.
+	 */
+	cnt = 0;
+	while (1) {
+		entry = xmalloc(sizeof(struct pidfd_entry));
+		if (entry == NULL) {
+			return -1;
+		}
+		INIT_HLIST_NODE(&entry->hash);
+
+		ret = __recv_fds(pidfd_store_sk, &entry->pidfd, 1, &entry->pid, sizeof(pid_t), MSG_DONTWAIT);
+		if (ret == -EAGAIN || ret == -EWOULDBLOCK) {
+			/* No more fds to read */
+			xfree(entry);
+			goto check_empty;
+		} else if (ret) {
+			pr_perror("Can't read pidfd");
+			xfree(entry);
+			free_pidfd_store();
+			return ret;
+		}
+
+		cnt++;
+		hlist_add_head(&entry->hash, &pidfd_hash[entry->pid % PIDFD_HASH_SIZE]);
+	}
+
+check_empty:
+	/*
+	 * If no pidfds exist in pidfd_store. This would cause full page
+	 * dumps which goes against the purpose of the pidfd store.
+	 * This is probably due to sending a different pidfd_store socket.
+	 */
+	if (cnt == 0 && opts.img_parent) {
+		pr_err("No pidfds found in pidfd store\n");
+		pr_err("The same socket from the previous iteration should be passed\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+static struct pidfd_entry *find_pidfd_entry_by_pid(pid_t pid)
+{
+	struct pidfd_entry *entry;
+	struct hlist_head *chain;
+
+	chain = &pidfd_hash[pid % PIDFD_HASH_SIZE];
+	hlist_for_each_entry(entry, chain, hash) {
+		if (entry->pid == pid)
+			return entry;
+	}
+
+	return NULL;
+}
+
+/*
+ * 1 - task closed
+ * 0 - task still running
+ * -1 - error
+ */
+static int check_pidfd_entry_state(struct pidfd_entry *entry)
+{
+	struct pollfd pollfd;
+	int ret, restart_cnt = 0;
+	const int MAX_RESTARTS = 10; /* Reasonable limit to avoid getting stuck */
+
+	/*
+	 * When there is data to read from the pidfd, it means
+	 * that the task associated with this pidfd is closed.
+	 */
+	pollfd.fd = entry->pidfd;
+	pollfd.events = POLLIN;
+
+	while (1) {
+		ret = poll(&pollfd, 1, 0);
+		if (errno == EINTR && restart_cnt < MAX_RESTARTS) {
+			restart_cnt++;
+			continue; /* restart polling */
+		}
+
+		return ret;
+	}
+}
+
+int send_pidfd_entry(pid_t pid)
+{
+	int pidfd, entry_state;
+	struct pidfd_entry *entry;
+
+	/* Skip sending if pidfd_store_sk is not initialized */
+	if (pidfd_store_sk == -1)
+		return 0;
+
+	/*
+	 * Use existing pidfd entry or create pidfd for task.
+	 * If entry exists with same pid we must check that
+	 * it is not a case of pid reuse (i.e. task is closed).
+	 */
+	entry = find_pidfd_entry_by_pid(pid);
+	if (entry != NULL) {
+		entry_state = check_pidfd_entry_state(entry);
+		if (entry_state == -1) {
+			pr_perror("Can't get state of pidfd entry of pid %d", pid);
+			return -1;
+		} else if(entry_state == 1) {
+			/* Task is closed, We need to create a new pidfd for task. */
+			entry = NULL;
+		}
+	}
+
+	if (entry == NULL) {
+		if (!kdat.has_pidfd_open) {
+			pr_err("pidfd_open syscall is not supported\n");
+			return -1;
+		}
+
+		pidfd = syscall(SYS_pidfd_open, pid, 0);
+		if (pidfd == -1) {
+			pr_perror("Can't get pidfd of pid %d", pid);
+			return -1;
+		}
+	} else {
+		pidfd = entry->pidfd;
+	}
+
+	if (send_fds(pidfd_store_sk, NULL, 0, &pidfd, 1, &pid, sizeof(pid_t))) {
+		pr_perror("Can't send pidfd %d of pid %d", pidfd, pid);
+		if(!entry)
+			close(pidfd);
+		return -1;
+	}
+
+	if(!entry)
+		close(pidfd);
+
+	return 0;
 }
 
 static int task_reset_dirty_track(int pid)
@@ -410,8 +588,33 @@ static int detect_pid_reuse(struct pstree_item *item,
 {
 	unsigned long long dump_ticks;
 	struct proc_pid_stat pps_buf;
+	struct pidfd_entry *entry;
 	unsigned long long tps; /* ticks per second */
 	int ret;
+
+	/* Check pid reuse using pidfds */
+	if (pidfd_store_sk != -1) {
+		entry = find_pidfd_entry_by_pid(item->pid->real);
+		if (entry == NULL) {
+			/*
+			 * This task was created between two iteration so it
+			 * should be marked as a pid reuse to make a full memory dump.
+			 */
+			pr_warn("Pid reuse detected for pid %d\n", item->pid->real);
+			return 1;
+		}
+
+		ret = check_pidfd_entry_state(entry);
+		if (ret < 0) {
+			pr_err("Failed to get pidfd entry state for pid %d\n", item->pid->real);
+			return -1;
+		}
+
+		if (ret == 1)
+			pr_warn("Pid reuse detected for pid %d\n", item->pid->real);
+
+		return ret;
+	}
 
 	if (!parent_ie) {
 		pr_err("Pid-reuse detection failed: no parent inventory, " \
