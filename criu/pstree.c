checkpoint-restore/criu

commit 77968d43c3641baa189212ee3547e932fff7f634

pstree: check for pid collision before switching to new sid/gid
Without this check we can hit the BUG in lookup_create_item just a few
steps later (if one thread in images has same pid with new sid/gid). And
also this check saves us from different sorts of unexpected errors on
restore (if one non-thread task in images has same pid/sid/gid already).

Fixes: #1332
Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/pstree.c
@@ -339,6 +339,7 @@ static int prepare_pstree_for_shell_job(pid_t pid)
 	pid_t current_gid = getpgid(pid);
 
 	struct pstree_item *pi;
+	struct pid *tmp;
 
 	pid_t old_sid;
 	pid_t old_gid;
@@ -371,6 +372,13 @@ static int prepare_pstree_for_shell_job(pid_t pid)
 	pr_info("Migrating process tree (SID %d->%d)\n",
 		old_sid, current_sid);
 
+	tmp = pstree_pid_by_virt(current_sid);
+	if (tmp) {
+		pr_err("Current sid %d intersects with pid (%d) in images",
+		       current_sid, tmp->state);
+		return -1;
+	}
+
 	for_each_pstree_item(pi) {
 		if (pi->sid == old_sid)
 			pi->sid = current_sid;
@@ -383,6 +391,13 @@ static int prepare_pstree_for_shell_job(pid_t pid)
 
 		pr_info("Migrating process tree (GID %d->%d)\n",
 			old_gid, current_gid);
+
+		tmp = pstree_pid_by_virt(current_gid);
+		if (tmp) {
+			pr_err("Current gid %d intersects with pid (%d) in images",
+			       current_gid, tmp->state);
+			return -1;
+		}
 
 		for_each_pstree_item(pi) {
 			if (pi->pgid == old_gid)

commit 6ba66394f9b870ba29586cd4c74688483516dc50

pstree: don't change sid/gid-s if current sid/gid is the same
Previously we only skipped replacing sid for shell job if root_item was
session leader, but there is other case where root_item sid is the same
as current_sid we can safely skip replacing for this case. Same applies
to gid-s.

Now after we have pid collision check we not only "can" but should skip
pid collision checks for the latter case. It is quite obvious that
there are tasks in tree with sid==current_sid if current_sid==old_sid.

Fixes: #1400
Fixes: 77968d43c ("pstree: check for pid collision before switching to
new sid/gid")
Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/pstree.c
@@ -347,6 +347,7 @@ static int prepare_pstree_for_shell_job(pid_t pid)
 	if (!opts.shell_job)
 		return 0;
 
+	/* root_item is a session leader */
 	if (root_item->sid == vpid(root_item))
 		return 0;
 
@@ -368,27 +369,32 @@ static int prepare_pstree_for_shell_job(pid_t pid)
 	 */
 
 	old_sid = root_item->sid;
+	if (old_sid != current_sid) {
+		pr_info("Migrating process tree (SID %d->%d)\n",
+			old_sid, current_sid);
 
-	pr_info("Migrating process tree (SID %d->%d)\n",
-		old_sid, current_sid);
+		tmp = pstree_pid_by_virt(current_sid);
+		if (tmp) {
+			pr_err("Current sid %d intersects with pid (%d) in images",
+			       current_sid, tmp->state);
+			return -1;
+		}
 
-	tmp = pstree_pid_by_virt(current_sid);
-	if (tmp) {
-		pr_err("Current sid %d intersects with pid (%d) in images",
-		       current_sid, tmp->state);
-		return -1;
-	}
+		for_each_pstree_item(pi) {
+			if (pi->sid == old_sid)
+				pi->sid = current_sid;
+		}
 
-	for_each_pstree_item(pi) {
-		if (pi->sid == old_sid)
-			pi->sid = current_sid;
-	}
-
-	old_gid = root_item->pgid;
-	if (old_gid != vpid(root_item)) {
 		if (lookup_create_item(current_sid) == NULL)
 			return -1;
+	}
 
+	/* root_item is a group leader */
+	if (root_item->pgid == vpid(root_item))
+		return 0;
+
+	old_gid = root_item->pgid;
+	if (old_gid != current_gid) {
 		pr_info("Migrating process tree (GID %d->%d)\n",
 			old_gid, current_gid);
 