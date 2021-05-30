3eed143b61c74904f9bd7a52d8bebf03fc2e048a
Radostin Stoyanov <rstoyanov@fedoraproject.org>
2021-05-30 15:17:34 +0100

    test/others/criu-coredump: fix race condition
    In Alpine and Fedora Rawhide the commands were not executed in correct
    order resulting `PID=$!` being executed before `setsid ./loop.sh`.
    
    To solve this problem, we use command substitution. Bash performs
    expansion of `$( (command) )` in a subshell environment it would
    replace it with the standard output of the command.
    
    Signed-off-by: Radostin Stoyanov <rstoyanov@fedoraproject.org>

--- test/others/criu-coredump/test.sh
+++ test/others/criu-coredump/test.sh
@@ -8,8 +8,8 @@ function _exit {
 }
 
 function gen_imgs {
-	setsid ./loop.sh < /dev/null &> /dev/null &
-	PID=$!
+	# Assign PID after the background task has started
+	PID=$( (setsid ./loop.sh < /dev/null &> /dev/null) & jobs -r -p)
 	$CRIU dump -v4 -o dump.log -D ./ -t $PID
 	if [ $? -ne 0 ]; then
 		kill -9 $PID
