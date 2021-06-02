checkpoint-restore/criu

commit cfdeac4a4d672eaea8c7d8a8f965145c0c6dcfd6

kerndat: Handle non-root mode when checking uffd
When criu is run as user it fails and exits because of kerndat_uffd() returning -1.
This, in turn, happens after uffd = syscall(SYS_userfaultfd, flags); which only works
for root.

In the change it ignores the permission error and proceeds further just like it's done
for e.g. pagemap checking.

Signed-off-by: Nithin Jaikar J <jaikar006@gmail.com>

criu/kerndat.c
@@ -919,7 +919,10 @@ static int kerndat_uffd(void)
 	if (uffd < 0) {
 		if (uffd == -ENOSYS)
 			return 0;
-
+		if (uffd == -EPERM) {
+			pr_info("Lazy pages are disabled\n");
+			return 0;
+		}
 		pr_err("Lazy pages are not available\n");
 		return -1;
 	}

commit 960fe1240263c10677ce8bc44bdf89b25f73f95c

kdat: make uffd_open return errno from syscall separately
Previousely kerndat_uffd could not differentiate -EPERM and -1 returned
from uffd_open(). That way "Failed to get uffd API" and "Incompatible
uffd API ..." errors were just ignored, which is probably not what we
want.

v2: rework with extra argument of uffd_open for errno, rename err
label in uffd_open for readability

Fixes: cfdeac4a4 ("kerndat: Handle non-root mode when checking uffd")
Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/include/uffd.h
@@ -3,7 +3,7 @@
 
 struct task_restore_args;
 
-extern int uffd_open(int flags, unsigned long *features);
+extern int uffd_open(int flags, unsigned long *features, int *err);
 extern bool uffd_noncooperative(void);
 extern int setup_uffd(int pid, struct task_restore_args *task_args);
 extern int lazy_pages_setup_zombie(int pid);

criu/kerndat.c
@@ -905,22 +905,24 @@ unl:
 
 static int kerndat_uffd(void)
 {
-	int uffd;
+	int uffd, err = 0;
 
 	kdat.uffd_features = 0;
-	uffd = uffd_open(0, &kdat.uffd_features);
+	uffd = uffd_open(0, &kdat.uffd_features, &err);
 
 	/*
-	 * uffd == -ENOSYS means userfaultfd is not supported on this
-	 * system and we just happily return with kdat.has_uffd = false.
-	 * Error other than -ENOSYS would mean "Houston, Houston, we
+	 * err == ENOSYS means userfaultfd is not supported on this system and
+	 * we just happily return with kdat.has_uffd = false.
+	 * err == EPERM means that userfaultfd is not allowed as we are
+	 * non-root user, so we also return with kdat.has_uffd = false.
+	 * Errors other than ENOSYS and EPERM would mean "Houston, Houston, we
 	 * have a problem!"
 	 */
 	if (uffd < 0) {
-		if (uffd == -ENOSYS)
+		if (err == ENOSYS)
 			return 0;
-		if (uffd == -EPERM) {
-			pr_info("Lazy pages are disabled\n");
+		if (err == EPERM) {
+			pr_info("Lazy pages are not permited\n");
 			return 0;
 		}
 		pr_err("Lazy pages are not available\n");

criu/uffd.c
@@ -262,7 +262,7 @@ static int uffd_api_ioctl(void *arg, int fd, pid_t pid)
 	return ioctl(fd, UFFDIO_API, uffdio_api);
 }
 
-int uffd_open(int flags, unsigned long *features)
+int uffd_open(int flags, unsigned long *features, int *err)
 {
 	struct uffdio_api uffdio_api = { 0 };
 	int uffd;
@@ -270,7 +270,9 @@ int uffd_open(int flags, unsigned long *features)
 	uffd = syscall(SYS_userfaultfd, flags);
 	if (uffd == -1) {
 		pr_info("Lazy pages are not available: %s\n", strerror(errno));
-		return -errno;
+		if (err)
+			*err = errno;
+		return -1;
 	}
 
 	uffdio_api.api = UFFD_API;
@@ -280,13 +282,13 @@ int uffd_open(int flags, unsigned long *features)
 	if (userns_call(uffd_api_ioctl, 0, &uffdio_api, sizeof(uffdio_api),
 			uffd)) {
 		pr_perror("Failed to get uffd API");
-		goto err;
+		goto close;
 	}
 
 	if (uffdio_api.api != UFFD_API) {
 		pr_err("Incompatible uffd API: expected %Lu, got %Lu\n",
 		       UFFD_API, uffdio_api.api);
-		goto err;
+		goto close;
 	}
 
 	if (features)
@@ -294,7 +296,7 @@ int uffd_open(int flags, unsigned long *features)
 
 	return uffd;
 
-err:
+close:
 	close(uffd);
 	return -1;
 }
@@ -313,7 +315,7 @@ int setup_uffd(int pid, struct task_restore_args *task_args)
 	 * Open userfaulfd FD which is passed to the restorer blob and
 	 * to a second process handling the userfaultfd page faults.
 	 */
-	task_args->uffd = uffd_open(O_CLOEXEC | O_NONBLOCK, &features);
+	task_args->uffd = uffd_open(O_CLOEXEC | O_NONBLOCK, &features, NULL);
 	if (task_args->uffd < 0) {
 		pr_perror("Unable to open an userfaultfd descriptor");
 		return -1;