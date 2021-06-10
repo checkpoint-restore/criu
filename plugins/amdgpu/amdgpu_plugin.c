d8389429415bc20fcafafae92f9c3816615006b0
David Yat Sin <david.yatsin@amd.com>
2021-06-10 15:07:13 -0400

    criu/plugin: Restore libhsakmt shared memory files
    Libhsakmt(thunk) uses a shared memory file in /dev/shm/hsakmt_shared_mem
    and its semaphore in /dev/shm/hsakmt_shared_mem. Adding a check during
    checkpoint to see if these two files exist. If they exist then the
    plugin will try to restore them during restore.
    
    Signed-off-by: David Yat Sin <david.yatsin@amd.com>

--- plugins/amdgpu/amdgpu_plugin.c
+++ plugins/amdgpu/amdgpu_plugin.c
@@ -14,6 +14,7 @@
 #include <sys/types.h>
 #include <stdint.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 #include "criu-plugin.h"
 #include "criu-amdgpu.pb-c.h"
@@ -29,6 +30,11 @@
 #define DRM_LAST_RENDER_NODE 255
 
 #define PROCPIDMEM      "/proc/%d/mem"
+#define HSAKMT_SHM_PATH "/dev/shm/hsakmt_shared_mem"
+#define HSAKMT_SHM      "/hsakmt_shared_mem"
+#define HSAKMT_SEM_PATH "/dev/shm/sem.hsakmt_semaphore"
+#define HSAKMT_SEM      "hsakmt_semaphore"
+
 
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE 1
@@ -697,6 +703,71 @@ exit:
 	return NULL;
 };
 
+int check_hsakmt_shared_mem(uint64_t *shared_mem_size, uint32_t *shared_mem_magic)
+{
+	int ret;
+	struct stat st;
+
+	ret = stat(HSAKMT_SHM_PATH, &st);
+	if (ret) {
+		*shared_mem_size = 0;
+		return 0;
+	}
+
+	*shared_mem_size = st.st_size;
+
+	/* First 4 bytes of shared file is the magic */
+	ret = read_file(HSAKMT_SHM_PATH, shared_mem_magic, sizeof(*shared_mem_magic));
+	if (ret)
+		pr_perror("amdgpu_plugin: Failed to read shared mem magic\n");
+	else
+		plugin_log_msg("amdgpu_plugin: Shared mem magic:0x%x\n", *shared_mem_magic);
+
+	return 0;
+}
+
+int restore_hsakmt_shared_mem(const uint64_t shared_mem_size, const uint32_t shared_mem_magic)
+{
+	int ret, fd;
+	struct stat st;
+	sem_t *sem = SEM_FAILED;
+
+	if (!shared_mem_size)
+		return 0;
+
+	if (!stat(HSAKMT_SHM_PATH, &st)) {
+		pr_debug("amdgpu_plugin: %s already exists\n", HSAKMT_SHM_PATH);
+	} else {
+		pr_info("Warning:%s was missing. Re-creating new file but we may lose perf counters\n",
+										HSAKMT_SHM_PATH);
+		fd = shm_open(HSAKMT_SHM, O_CREAT | O_RDWR, 0666);
+
+		ret = ftruncate(fd, shared_mem_size);
+		if (ret < 0) {
+			pr_err("amdgpu_plugin: Failed to truncate shared mem %s\n", HSAKMT_SHM);
+			close(fd);
+			return -errno;
+		}
+
+		ret = write(fd, &shared_mem_magic, sizeof(shared_mem_magic));
+		if (ret != sizeof(shared_mem_magic)) {
+			pr_perror("amdgpu_plugin: Failed to restore shared mem magic\n");
+			close(fd);
+			return -errno;
+		}
+
+		close(fd);
+	}
+
+	sem = sem_open(HSAKMT_SEM, O_CREAT, 0666, 1);
+	if (sem == SEM_FAILED) {
+		pr_perror("Failed to create %s\n", HSAKMT_SEM);
+		return -EACCES;
+	}
+	sem_close(sem);
+	return 0;
+}
+
 int amdgpu_plugin_dump_file(int fd, int id)
 {
 	struct kfd_ioctl_criu_helper_args helper_args = {0};
@@ -1077,6 +1148,10 @@ int amdgpu_plugin_dump_file(int fd, int id)
 			}
 		}
 	}
+
+	ret = check_hsakmt_shared_mem(&e->shared_mem_size, &e->shared_mem_magic);
+	if (ret)
+		goto failed;
 
 	snprintf(img_path, sizeof(img_path), "kfd.%d.img", id);
 	pr_info("amdgpu_plugin: img_path = %s", img_path);
@@ -1528,6 +1603,11 @@ fail:
 			list_add_tail(&vma_md->list, &update_vma_info_list);
 		}
 	} /* mmap done for VRAM BO */
+
+	if (restore_hsakmt_shared_mem(e->shared_mem_size, e->shared_mem_magic)) {
+		fd = -EBADFD;
+		goto clean;
+	}
 
 	for (int i = 0; i < e->num_of_gpus; i++) {
 		int ret_thread = 0;
