1a197d4d86ba50012d7808773fa5b361017f658b
Adrian Reber <areber@redhat.com>
2021-06-22 14:52:09 +0000

    criu: add unit testing for config file parser
    This tries to add a unit test for the configuration file parser.
    
    Signed-off-by: Adrian Reber <areber@redhat.com>

---
+++ criu/unittest/unit.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+#include "log.h"
+#include "criu-log.h"
+
+int parse_statement(int i, char *line, char **configuration);
+
+int main(int argc, char *argv[], char *envp[])
+{
+	char **configuration;
+	int i;
+
+	configuration = malloc(10 * sizeof(char *));
+	log_init(NULL);
+
+	i = parse_statement(0, "", configuration);
+	assert(i == 0);
+
+	i = parse_statement(0, "\n", configuration);
+	assert(i == 0);
+
+	i = parse_statement(0, "# comment\n", configuration);
+	assert(i == 0);
+
+	i = parse_statement(0, "#comment\n", configuration);
+	assert(i == 0);
+
+	i = parse_statement(0, "tcp-close #comment\n", configuration);
+	assert(i == 1);
+	assert(!strcmp(configuration[0], "--tcp-close"));
+
+	i = parse_statement(0, " tcp-close #comment\n", configuration);
+	assert(i == 1);
+	assert(!strcmp(configuration[0], "--tcp-close"));
+
+	i = parse_statement(0, "test \"test\"\n", configuration);
+	assert(i == 2);
+	assert(!strcmp(configuration[0], "--test"));
+	assert(!strcmp(configuration[1], "test"));
+
+	i = parse_statement(0, "dsfa \"aaaaa \\\"bbbbbb\\\"\"\n", configuration);
+	assert(i == 2);
+	assert(!strcmp(configuration[0], "--dsfa"));
+	assert(!strcmp(configuration[1], "aaaaa \"bbbbbb\""));
+
+
+	i = parse_statement(0, "verbosity 4\n", configuration);
+	assert(i == 2);
+	assert(!strcmp(configuration[0], "--verbosity"));
+	assert(!strcmp(configuration[1], "4"));
+
+	i = parse_statement(0, "verbosity \"\n", configuration);
+	assert(i == -1);
+
+	i = parse_statement(0, "verbosity 4#comment\n", configuration);
+	assert(i == 2);
+	assert(!strcmp(configuration[0], "--verbosity"));
+	assert(!strcmp(configuration[1], "4"));
+
+	i = parse_statement(0, "verbosity 4 #comment\n", configuration);
+	assert(i == 2);
+	assert(!strcmp(configuration[0], "--verbosity"));
+	assert(!strcmp(configuration[1], "4"));
+
+	i = parse_statement(0, "verbosity 4  #comment\n", configuration);
+	assert(i == 2);
+	assert(!strcmp(configuration[0], "--verbosity"));
+	assert(!strcmp(configuration[1], "4"));
+
+	i = parse_statement(0, "verbosity 4 no-comment\n", configuration);
+	assert(i == -1);
+
+	i = parse_statement(0, "lsm-profile \"\" # more comments\n", configuration);
+	assert(i == 2);
+	assert(!strcmp(configuration[0], "--lsm-profile"));
+	assert(!strcmp(configuration[1], ""));
+
+	i = parse_statement(0, "lsm-profile \"something\"# comment\n", configuration);
+	assert(i == 2);
+	assert(!strcmp(configuration[0], "--lsm-profile"));
+	assert(!strcmp(configuration[1], "something"));
+
+	i = parse_statement(0, "#\n", configuration);
+	assert(i == 0);
+
+	i = parse_statement(0, "lsm-profile \"selinux:something\\\"with\\\"quotes\"\n", configuration);
+	assert(i == 2);
+	assert(!strcmp(configuration[0], "--lsm-profile"));
+	assert(!strcmp(configuration[1], "selinux:something\"with\"quotes"));
+
+	i = parse_statement(0, "work-dir \"/tmp with spaces\" no-comment\n", configuration);
+	assert(i == -1);
+
+	i = parse_statement(0, "work-dir \"/tmp with spaces\"\n", configuration);
+	assert(i == 2);
+	assert(!strcmp(configuration[0], "--work-dir"));
+	assert(!strcmp(configuration[1], "/tmp with spaces"));
+
+	i = parse_statement(0, "a b c d e f g h i\n", configuration);
+	assert(i == -1);
+
+	pr_msg("OK\n");
+	return 0;
+}
