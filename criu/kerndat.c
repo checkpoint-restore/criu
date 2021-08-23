3c29a88e3a8c04d9c2a00511547bc2d46a38bc6d
Zeyad Yasser <zeyady98@gmail.com>
2021-08-23 20:59:11 +0200

    kerndat: create separate netns for has_nftables_concat check
    There was a race window in kerndat_has_nftables_concat(void) when
    two or more instances of CRIU are running in parallel.
    One instance would create the table normally, and another would
    fail when trying to create a table with the same name and wrongly
    set kdat.has_nftables_concat = false, and will save it to kerndat
    cache.
    
    A separate netns is created to avoid table collisions.
    
    Signed-off-by: Zeyad Yasser <zeyady98@gmail.com>

--- criu/kerndat.c
+++ criu/kerndat.c
@@ -13,6 +13,8 @@
 #include <arpa/inet.h> /* for sockaddr_in and inet_ntoa() */
 #include <sys/prctl.h>
 #include <sys/inotify.h>
+#include <sys/wait.h>
+#include <sched.h>
 
 #if defined(CONFIG_HAS_NFTABLES_LIB_API_0) || defined(CONFIG_HAS_NFTABLES_LIB_API_1)
 #include <nftables/libnftables.h>
@@ -1153,29 +1155,59 @@ close:
 static int kerndat_has_nftables_concat(void)
 {
 #if defined(CONFIG_HAS_NFTABLES_LIB_API_0) || defined(CONFIG_HAS_NFTABLES_LIB_API_1)
+	pid_t pid;
 	struct nft_ctx *nft;
-	int ret = 0;
+	int ret, status;
 
-	nft = nft_ctx_new(NFT_CTX_DEFAULT);
-	if (!nft)
+	pid = fork();
+	if (pid < 0) {
+		pr_perror("Can't fork");
 		return -1;
-
-	if (NFT_RUN_CMD(nft, "create table inet CRIU-kerndat-test")) {
-		ret = -1;
-		goto nft_ctx_free_out;
 	}
 
-	if (NFT_RUN_CMD(nft, "add set inet CRIU-kerndat-test conn { type ipv4_addr . inet_service ;}"))
-		kdat.has_nftables_concat = false;
-	else
-		kdat.has_nftables_concat = true;
+	if (pid == 0) {
+		/*
+		 * Create a separate network namespace to avoid
+		 * collisions between two CRIU instances.
+		 */
+		if (unshare(CLONE_NEWNET)) {
+			pr_perror("Unable create a network namespace");
+			exit(-1);
+		}
 
-	/* Clean up */
-	NFT_RUN_CMD(nft, "delete table inet CRIU-kerndat-test");
+		nft = nft_ctx_new(NFT_CTX_DEFAULT);
+		if (!nft)
+			exit(-1);
 
-nft_ctx_free_out:
-	nft_ctx_free(nft);
-	return ret;
+		if (NFT_RUN_CMD(nft, "create table inet CRIU")) {
+			ret = -1;
+			pr_err("Can't create nftables table\n");
+			goto nft_ctx_free_out;
+		}
+
+		if (NFT_RUN_CMD(nft, "add set inet CRIU conn { type ipv4_addr . inet_service ;}"))
+			ret = false; /* kdat.has_nftables_concat = false */
+		else
+			ret = true; /* kdat.has_nftables_concat = true */
+
+		/* Clean up */
+		NFT_RUN_CMD(nft, "delete table inet CRIU");
+
+	nft_ctx_free_out:
+		nft_ctx_free(nft);
+		exit(ret);
+	}
+
+	if (waitpid(pid, &status, 0) != pid) {
+		pr_perror("Unable to wait %d", pid);
+		return -1;
+	}
+
+	if (status < 0)
+		return -1;
+
+	kdat.has_nftables_concat = status;
+	return 0;
 #else
 	pr_warn("CRIU was built without libnftables support\n");
 	kdat.has_nftables_concat = false;
