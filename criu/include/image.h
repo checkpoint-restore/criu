3963434828b16d09744f51da622184e14aa91c6a
David Yat Sin <david.yatsin@amd.com>
2021-09-30 16:46:06 -0400

    criu/plugin: Add dedicated flag for plugins
    Adding a dedicated flag for vma's that are handled by an external plugin
    as previously used VMA_UNSUPP flag depends on vma not having
    VMA_FILE_SHARED flag.
    
    Signed-off-by: David Yat Sin <david.yatsin@amd.com>

--- criu/include/image.h
+++ criu/include/image.h
@@ -85,6 +85,7 @@
 #define VMA_AREA_AIORING (1 << 13)
 #define VMA_AREA_MEMFD	 (1 << 14)
 
+#define VMA_EXT_PLUGIN	  (1 << 27)
 #define VMA_CLOSE	  (1 << 28)
 #define VMA_NO_PROT_WRITE (1 << 29)
 #define VMA_PREMMAPED	  (1 << 30)
