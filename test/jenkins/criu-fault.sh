6ce6b60132c61582df4bcb4300883c1abf4dcac7
Adrian Reber <areber@redhat.com>
2021-12-05 16:45:20 +0000

    test: do not use --keep-going for single zdtm tests
    Looking at CI logs there are often messages like:
    
     "[WARNING] Option --keep-going is more useful when running multiple tests"
    
    This commit removes '--keep-going' from single zdtm test runs.
    
    Signed-off-by: Adrian Reber <areber@redhat.com>

--- test/jenkins/criu-fault.sh
+++ test/jenkins/criu-fault.sh
@@ -4,10 +4,10 @@
 set -e
 source `dirname $0`/criu-lib.sh
 prep
-./test/zdtm.py run -t zdtm/static/env00 --fault 1 --keep-going --report report -f h || fail
-./test/zdtm.py run -t zdtm/static/unlink_fstat00 --fault 2 --keep-going --report report -f h || fail
-./test/zdtm.py run -t zdtm/static/maps00 --fault 3 --keep-going --report report -f h || fail
-./test/zdtm.py run -t zdtm/static/inotify_irmap --fault 128 --keep-going --pre 2 -f uns || fail
+./test/zdtm.py run -t zdtm/static/env00 --fault 1 --report report -f h || fail
+./test/zdtm.py run -t zdtm/static/unlink_fstat00 --fault 2 --report report -f h || fail
+./test/zdtm.py run -t zdtm/static/maps00 --fault 3 --report report -f h || fail
+./test/zdtm.py run -t zdtm/static/inotify_irmap --fault 128 --pre 2 -f uns || fail
 ./test/zdtm.py run -t zdtm/static/env00 --fault 129 -f uns || fail
 ./test/zdtm.py run -t zdtm/transition/fork --fault 130 -f h || fail
 ./test/zdtm.py run -t zdtm/static/vdso01 --fault 127 || fail
@@ -17,16 +17,16 @@ if [ "${COMPAT_TEST}" != "y" ] ; then
 	./test/zdtm.py run -t zdtm/static/vdso01 --fault 133 -f h || fail
 fi
 
-./test/zdtm.py run -t zdtm/static/mntns_ghost --fault 2 --keep-going --report report || fail
-./test/zdtm.py run -t zdtm/static/mntns_ghost --fault 4 --keep-going --report report || fail
+./test/zdtm.py run -t zdtm/static/mntns_ghost --fault 2 --report report || fail
+./test/zdtm.py run -t zdtm/static/mntns_ghost --fault 4 --report report || fail
 
 ./test/zdtm.py run -t zdtm/static/mntns_ghost --fault 6 --report report || fail
 ./test/zdtm.py run -t zdtm/static/mntns_link_remap --fault 6 --report report || fail
 ./test/zdtm.py run -t zdtm/static/unlink_fstat03 --fault 6 --report report || fail
 
-./test/zdtm.py run -t zdtm/static/env00 --fault 5 --keep-going --report report || fail
-./test/zdtm.py run -t zdtm/static/maps04 --fault 131 --keep-going --report report --pre 2:1 || fail
-./test/zdtm.py run -t zdtm/transition/maps008 --fault 131 --keep-going --report report --pre 2:1 || fail
+./test/zdtm.py run -t zdtm/static/env00 --fault 5 --report report || fail
+./test/zdtm.py run -t zdtm/static/maps04 --fault 131 --report report --pre 2:1 || fail
+./test/zdtm.py run -t zdtm/transition/maps008 --fault 131 --report report --pre 2:1 || fail
 ./test/zdtm.py run -t zdtm/static/maps01 --fault 132 -f h || fail
 
 # Error injection with --fault 134 fails on newer CPUs used in Circle CI on EC2
