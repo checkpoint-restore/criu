8941b63a4bf9c57bbb63443101d9a3f1e6a82d1b
Bui Quang Minh <minhquangbui99@gmail.com>
2021-12-15 23:03:29 +0700

    proc_parse, files: Add support for hugetlb memory mapping
    When memfd can be used with hugetlb, we use memfd for checkpoint/restore
    anonymous shared memory. Otherwise, map_files symlinks is used for
    checkpoint/restore anonymous shared memory.
    
    Signed-off-by: Bui Quang Minh <minhquangbui99@gmail.com>

--- criu/files.c
+++ criu/files.c
@@ -548,7 +548,8 @@ static int dump_one_file(struct pid *pid, int fd, int lfd, struct fd_opts *opts,
 
 		p.link = &link;
 
-		if (is_memfd(p.stat.st_dev))
+		/* TODO: Dump for hugetlb fd when memfd hugetlb is not supported */
+		if (is_memfd(p.stat.st_dev) || (kdat.has_memfd_hugetlb && is_hugetlb_dev(p.stat.st_dev, NULL)))
 			ops = &memfd_dump_ops;
 		else if (link.name[1] == '/')
 			ops = &regfile_dump_ops;
