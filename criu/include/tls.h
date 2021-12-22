fa03dcfff6a5215ef06c1e579eddb0f5c09f8798
Andrei Vagin <avagin@gmail.com>
2021-12-22 09:36:09 -0800

    tls: allow to terminate connections synchronously
    GNUTLS_SHUT_RDWR sends an alert containing a close request and waits for
    the peer to reply with the same message.
    
    Signed-off-by: Andrei Vagin <avagin@gmail.com>

--- criu/include/tls.h
+++ criu/include/tls.h
@@ -4,7 +4,7 @@
 #ifdef CONFIG_GNUTLS
 
 int tls_x509_init(int sockfd, bool is_server);
-void tls_terminate_session(void);
+void tls_terminate_session(bool async);
 
 ssize_t tls_send(const void *buf, size_t len, int flags);
 ssize_t tls_recv(void *buf, size_t len, int flags);
@@ -19,7 +19,7 @@ int tls_recv_data_to_fd(int fd, unsigned long len);
 #define tls_recv(buf, len, flags)	 (-1)
 #define tls_send_data_from_fd(fd, len)	 (-1)
 #define tls_recv_data_to_fd(fd, len)	 (-1)
-#define tls_terminate_session()
+#define tls_terminate_session(async)
 
 #endif /* CONFIG_HAS_GNUTLS */
 
