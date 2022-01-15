809028dfe5e547c8dc020a7242c73018b48df919
rushi47 <www.rushi47@gmail.com>
2022-01-15 03:15:41 -0500

    using abi instead of reg

--- compel/arch/riscv64/src/lib/include/uapi/asm/sigframe.h
+++ compel/arch/riscv64/src/lib/include/uapi/asm/sigframe.h
@@ -37,7 +37,7 @@ struct rt_sigframe {
 #define ARCH_RT_SIGRETURN(new_sp, rt_sigframe)					\
 	asm volatile(								\
 			"mov sp, %0					\n"	\
-			"mov x8, #"__stringify(__NR_rt_sigreturn)"	\n"	\
+			"mov s0, #"__stringify(__NR_rt_sigreturn)"	\n"	\
 			"ecall #0						\n"	\
 			:							\
 			: "r"(new_sp)						\
