d2b07001d5ea0ee2ba5b29fa90d04749afcc88f7
Kir Kolyshkin <kolyshkin@gmail.com>
2022-03-30 17:35:21 -0700

    criu/arch/s390/include/asm/restorer.h: fix comments
    Use "Parameter" instead of "Parm" as in other places.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- criu/arch/s390/include/asm/restorer.h
+++ criu/arch/s390/include/asm/restorer.h
@@ -17,15 +17,15 @@
 	asm volatile(							\
 	"lgr	%%r0,%6\n"	/* Save thread_args in %r0 */		\
 	"lgr	%%r1,%5\n"	/* Save clone_restore_fn in %r1 */	\
-	"lgr	%%r2,%2\n"	/* Parm 1: new_sp (child stack) */	\
-	"lgr	%%r3,%1\n"	/* Parm 2: clone_flags */		\
-	"lgr	%%r4,%3\n"	/* Parm 3: &parent_tid */		\
-	"lgr	%%r5,%4\n"	/* Parm 4: &thread_args[i].pid */	\
-	"lghi	%%r6,0\n"	/* Parm 5: tls = 0 */			\
+	"lgr	%%r2,%2\n"	/* Parameter 1: new_sp (child stack) */	\
+	"lgr	%%r3,%1\n"	/* Parameter 2: clone_flags */		\
+	"lgr	%%r4,%3\n"	/* Parameter 3: &parent_tid */		\
+	"lgr	%%r5,%4\n"	/* Parameter 4: &thread_args[i].pid */	\
+	"lghi	%%r6,0\n"	/* Parameter 5: tls = 0 */		\
 	"svc	"__stringify(__NR_clone)"\n"				\
 	"ltgr	%0,%%r2\n"	/* Set and check "ret" */		\
 	"jnz	0f\n"		/* ret != 0: Continue caller */		\
-	"lgr	%%r2,%%r0\n"	/* Parm 1: &thread_args */		\
+	"lgr	%%r2,%%r0\n"	/* Parameter 1: &thread_args */		\
 	"aghi	%%r15,-160\n"	/* Prepare stack frame */		\
 	"xc	0(8,%%r15),0(%%r15)\n"					\
 	"basr	%%r14,%%r1\n"	/* Jump to clone_restore_fn() */	\
