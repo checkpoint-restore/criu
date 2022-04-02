bed20cefcba95d263d1fe380cf4b9610f74b1204
Adrian Reber <areber@redhat.com>
2022-04-02 15:58:25 +0000

    cgroup: test for --manage-cgroups=ignore
    Test to ensure that --manage-cgroups=ignore works correctly.
    
    Signed-off-by: Adrian Reber <areber@redhat.com>

--- test/jenkins/criu-dump.sh
+++ test/jenkins/criu-dump.sh
@@ -5,4 +5,4 @@ set -e
 source `dirname $0`/criu-lib.sh
 prep
 mount_tmpfs_to_dump
-./test/zdtm.py run --all --keep-going --report report --parallel 4 --norst -x 'maps04' -x 'cgroup02' || fail
+./test/zdtm.py run --all --keep-going --report report --parallel 4 --norst -x 'maps04' -x 'cgroup02' -x 'cgroup_ignore' || fail
