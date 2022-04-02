4adec8e8ef3682186fd21e1b04da3e309e1ddce7
Adrian Reber <areber@redhat.com>
2022-04-02 15:58:25 +0000

    cgroup: test for --manage-cgroups=ignore
    Test to ensure that --manage-cgroups=ignore works correctly.
    
    Signed-off-by: Adrian Reber <areber@redhat.com>

--- test/jenkins/crit.sh
+++ test/jenkins/crit.sh
@@ -2,6 +2,6 @@
 set -e
 source `dirname $0`/criu-lib.sh
 prep
-./test/zdtm.py run --all -f best -x maps04 -x cgroup02 --norst --keep-img always || fail
+./test/zdtm.py run --all -f best -x maps04 -x cgroup02 -x cgroup_ignore --norst --keep-img always || fail
 PYTHONPATH="$(pwd)/lib/" ./test/crit-recode.py || fail
 exit 0
--- test/zdtm/lib/zdtmtst.h
+++ test/zdtm/lib/zdtmtst.h
@@ -176,4 +176,30 @@ extern dev_t get_mapping_dev(void *addr);
 		___ret;                                              \
 	})
 
+#ifndef TEMP_FAILURE_RETRY
+#define TEMP_FAILURE_RETRY(expression)                     \
+	(__extension__({                                   \
+		long int __result;                         \
+		do                                         \
+			__result = (long int)(expression); \
+		while (__result < 0 && errno == EINTR);    \
+		__result;                                  \
+	}))
+#endif
+
+#define cleanup_close __attribute__((cleanup(cleanup_closep)))
+#define cleanup_free  __attribute__((cleanup(cleanup_freep)))
+static inline void cleanup_freep(void *p)
+{
+	void **pp = (void **)p;
+	free(*pp);
+}
+
+static inline void cleanup_closep(void *p)
+{
+	int *pp = (int *)p;
+	if (*pp >= 0)
+		TEMP_FAILURE_RETRY(close(*pp));
+}
+
 #endif /* _VIMITESU_H_ */
