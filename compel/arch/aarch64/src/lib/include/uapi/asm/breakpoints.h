b7953c6c7f020ea4a2f4221c5dd9c8314a96f733
fu.lin <fulin10@huawei.com>
2022-04-15 13:00:04 +0800

    compel: switch breakpoint functions to non-inline at arm64 platform
    Signed-off-by: fu.lin <fulin10@huawei.com>
    Signed-off-by: Andrei Vagin <avagin@gmail.com>

--- compel/arch/aarch64/src/lib/include/uapi/asm/breakpoints.h
+++ compel/arch/aarch64/src/lib/include/uapi/asm/breakpoints.h
@@ -2,14 +2,7 @@
 #define __COMPEL_BREAKPOINTS_H__
 #define ARCH_SI_TRAP TRAP_BRKPT
 
-static inline int ptrace_set_breakpoint(pid_t pid, void *addr)
-{
-	return 0;
-}
-
-static inline int ptrace_flush_breakpoints(pid_t pid)
-{
-	return 0;
-}
+int ptrace_set_breakpoint(pid_t pid, void *addr);
+int ptrace_flush_breakpoints(pid_t pid);
 
 #endif
