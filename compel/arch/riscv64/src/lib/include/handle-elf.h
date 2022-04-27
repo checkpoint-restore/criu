5dcbaf6512caaab53d2ffae2ab47f38cebe15e6c
rushi47 <www.rushi47@gmail.com>
2022-04-27 14:37:35 -0400

    macro for RISCV

--- compel/arch/riscv64/src/lib/include/handle-elf.h
+++ compel/arch/riscv64/src/lib/include/handle-elf.h
@@ -4,6 +4,7 @@
 #include "elf64-types.h"
 
 #define __handle_elf			     handle_elf_riscv64
+#define ELF_RISCV
 #define arch_is_machine_supported(e_machine) (e_machine == EM_RISCV)
 
 extern int handle_elf_riscv64(void *mem, size_t size);
