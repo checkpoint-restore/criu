991f27c841f4db7587b0247da46c1d51fca0b8d4
Alexander Mikhalitsyn <alexander.mikhalitsyn@virtuozzo.com>
2022-05-05 11:25:40 +0300

    ci: skip new hugetlb maps09/maps10 tests for pre-dump
    This commit has to be reverted once we fix the issue.
    
    Issue: https://github.com/checkpoint-restore/criu/issues/1868
    
    Reported-by: Mr. Jenkins
    Signed-off-by: Alexander Mikhalitsyn <alexander.mikhalitsyn@virtuozzo.com>

--- test/jenkins/criu-snap.sh
+++ test/jenkins/criu-snap.sh
@@ -5,5 +5,5 @@ set -e
 source `dirname $0`/criu-lib.sh
 prep
 mount_tmpfs_to_dump
-./test/zdtm.py run --all --keep-going --report report --parallel 4 --pre 3 --snaps -x 'maps04' || fail
-./test/zdtm.py run --all --keep-going --report report --parallel 4 --pre 3 --snaps --page-server -x 'maps04' || fail
+./test/zdtm.py run --all --keep-going --report report --parallel 4 --pre 3 --snaps -x 'maps04' -x 'maps09' -x 'maps10' || fail
+./test/zdtm.py run --all --keep-going --report report --parallel 4 --pre 3 --snaps --page-server -x 'maps04' -x 'maps09' -x 'maps10' || fail
