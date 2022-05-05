checkpoint-restore/criu

commit 4245e6b02fadb601324312a3a8a4d467264ba16e

check: Add a check for using memfd with hugetlb
Signed-off-by: Bui Quang Minh <minhquangbui99@gmail.com>

criu/kerndat.c
@@ -420,6 +420,29 @@ static bool kerndat_has_memfd_create(void)
 	return 0;
 }
 
+static bool kerndat_has_memfd_hugetlb(void)
+{
+	int ret;
+
+	if (!kdat.has_memfd) {
+		kdat.has_memfd_hugetlb = false;
+		return 0;
+	}
+
+	ret = memfd_create("", MFD_HUGETLB);
+	if (ret >= 0) {
+		kdat.has_memfd_hugetlb = true;
+		close(ret);
+	} else if (ret == -1 && errno == EINVAL) {
+		kdat.has_memfd_hugetlb = false;
+	} else {
+		pr_perror("Unexpected error from memfd_create(\"\", MFD_HUGETLB)");
+		return -1;
+	}
+
+	return 0;
+}
+
 static int get_task_size(void)
 {
 	kdat.task_size = compel_task_size();
@@ -1318,6 +1341,10 @@ int kerndat_init(void)
 	}
 	if (!ret && kerndat_has_memfd_create()) {
 		pr_err("kerndat_has_memfd_create failed when initializing kerndat.\n");
+		ret = -1;
+	}
+	if (!ret && kerndat_has_memfd_hugetlb()) {
+		pr_err("kerndat_has_memfd_hugetlb failed when initializing kerndat.\n");
 		ret = -1;
 	}
 	if (!ret && kerndat_detect_stack_guard_gap()) {

criu/cr-check.c
@@ -1368,6 +1368,14 @@ static int check_ns_pid(void)
 	return 0;
 }
 
+static int check_memfd_hugetlb(void)
+{
+	if (!kdat.has_memfd_hugetlb)
+		return -1;
+
+	return 0;
+}
+
 static int check_network_lock_nftables(void)
 {
 	if (!kdat.has_nftables_concat) {
@@ -1505,6 +1513,7 @@ int cr_check(void)
 		ret |= check_apparmor_stacking();
 		ret |= check_network_lock_nftables();
 		ret |= check_sockopt_buf_lock();
+		ret |= check_memfd_hugetlb();
 	}
 
 	/*
@@ -1618,6 +1627,7 @@ static struct feature_list feature_list[] = {
 	{ "apparmor_stacking", check_apparmor_stacking },
 	{ "network_lock_nftables", check_network_lock_nftables },
 	{ "sockopt_buf_lock", check_sockopt_buf_lock },
+	{ "memfd_hugetlb", check_memfd_hugetlb },
 	{ NULL, NULL },
 };
 

criu/include/kerndat.h
@@ -36,6 +36,7 @@ struct kerndat_s {
 	u64 zero_page_pfn;
 	bool has_dirty_track;
 	bool has_memfd;
+	bool has_memfd_hugetlb;
 	bool has_fdinfo_lock;
 	unsigned long task_size;
 	bool ipv6;

commit 994f8e73f60d70882c00d9a4060a09b0f30e990a

kerndat: handle the case when hugetlb isn't supported
Currently we check memfd_hugetlb by doing memfd_create("", MFD_HUGETLB).
If we see EINVAL we report that it's not supported, but we can also
get ENOENT error in such case in hugetlb_file_setup() while trying
to find proper hugetlbfs mount.

Reference:
https://github.com/torvalds/linux/blob/06fb4ecfeac/fs/hugetlbfs/inode.c#L1465

Fixes: 4245e6b02fa ("check: Add a check for using memfd with hugetlb")

Reported-by: Mr. Jenkins (ppc64le)
Signed-off-by: Alexander Mikhalitsyn <alexander.mikhalitsyn@virtuozzo.com>

criu/kerndat.c
@@ -502,7 +502,7 @@ static bool kerndat_has_memfd_hugetlb(void)
 	if (ret >= 0) {
 		kdat.has_memfd_hugetlb = true;
 		close(ret);
-	} else if (ret == -1 && errno == EINVAL) {
+	} else if (ret == -1 && (errno == EINVAL || errno == ENOENT)) {
 		kdat.has_memfd_hugetlb = false;
 	} else {
 		pr_perror("Unexpected error from memfd_create(\"\", MFD_HUGETLB)");