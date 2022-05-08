042992ea250ece94883f1262a0907f1940dece96
Bui Quang Minh <minhquangbui99@gmail.com>
2022-05-08 16:19:45 +0700

    Revert "ci: skip new hugetlb maps09/maps10 tests for pre-dump"
    This reverts commit 37ea8c5fcfef2108800b6d53054f3a7c4f710752.
    
    Signed-off-by: Bui Quang Minh <minhquangbui99@gmail.com>

--- test/jenkins/criu-dedup.sh
+++ test/jenkins/criu-dedup.sh
@@ -4,7 +4,7 @@
 set -e
 source `dirname $0`/criu-lib.sh
 prep
-./test/zdtm.py run --all --keep-going --report report --parallel 4 -f h --pre 2 --dedup -x maps04 -x maps007 -x maps09 -x maps10 || fail
+./test/zdtm.py run --all --keep-going --report report --parallel 4 -f h --pre 2 --dedup -x maps04 -x maps007 || fail
 
 # Additionally run these tests as they touch a lot of
 # memory and it makes sense to additionally check it
