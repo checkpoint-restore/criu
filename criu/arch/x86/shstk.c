7dd583002368acedec40d90757a4d47672c6180b
Mike Rapoport (IBM) <rppt@kernel.org>
2022-05-31 12:45:09 +0300

    restore: add infrastructure to enable shadow stack
    There are several gotachs when restoring a task with shadow stack:
    * depending on the compiler options, glibc version and glibc tunables
      CRIU can run with or without shadow stack.
    * shadow stack VMAs are special, they must be created using a dedicated
      map_shadow_stack() system call and can be modified only by a special
      instruction (wrss) that is only available when shadow stack is
      enabled.
    * once shadow stack is enabled, it is not writable even with wrss;
      writes to shadow stack can be only enabled with ptrace() and only when
      shadow stack is enabled in the tracee.
    * if the shadow stack is enabled during restore rather than by glibc,
      calling retq after arch_prctl() that enables the shadow stack causes
      #CP, so the function that enables shadow stack can never return.
    
    Add the infrastructure required to cope with all of those:
    
    * modify the restore code to allow trampoline (arch_shstk_trampoline)
      that will enable shadow stack and call restore_task_with_children().
    * add call to arch_shstk_unlock() right after the tasks are clone()ed;
      this will allow unlocking shadow stack features and making shadow
      stack writable.
    * add stubs for architectures that do not support shadow stacks
    * add implementation of arch_shstk_trampoline() and arch_shstk_unlock()
      for x86, but keep it disabled; it will be enabled along with addtion
      of the code that will restore shadow stack in the restorer blob
    
    Signed-off-by: Mike Rapoport (IBM) <rppt@kernel.org>

--- criu/arch/x86/shstk.c
+++ criu/arch/x86/shstk.c
@@ -1,3 +1,6 @@
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+
 #include <common/list.h>
 
 #include <compel/cpu.h>
@@ -87,4 +90,134 @@ int arch_shstk_prepare(struct pstree_item *item, CoreEntry *core,
 	}
 
 	return 0;
+}
+
+int arch_shstk_unlock(struct pstree_item *item, CoreEntry *core, pid_t pid)
+{
+	unsigned long features;
+	int status;
+	int ret = -1;
+
+	/*
+	 * CRIU runs with no shadow stack and the task does not need one,
+	 * nothing to do.
+	 */
+	if (!kdat.has_shstk && !task_needs_shstk(item, core))
+		return 0;
+
+	futex_wait_until(&rsti(item)->shstk_enable, 1);
+
+	if (ptrace(PTRACE_SEIZE, pid, 0, 0)) {
+		pr_perror("Cannot attach to %d", pid);
+		goto futex_wake;
+	}
+
+	if (ptrace(PTRACE_INTERRUPT, pid, 0, 0)) {
+		pr_perror("Cannot interrupt the %d task", pid);
+		goto detach;
+	}
+
+	if (wait4(pid, &status, __WALL, NULL) != pid) {
+		pr_perror("waitpid(%d) failed", pid);
+		goto detach;
+	}
+
+	features = ARCH_SHSTK_SHSTK | ARCH_SHSTK_WRSS;
+	if (ptrace(PTRACE_ARCH_PRCTL, pid, features, ARCH_SHSTK_UNLOCK)) {
+		pr_perror("Cannot unlock CET for %d task", pid);
+		goto detach;
+	}
+
+detach:
+	if (ptrace(PTRACE_DETACH, pid, NULL, 0)) {
+		pr_perror("Unable to detach %d", pid);
+		goto futex_wake;
+	}
+
+	ret = 0;
+
+futex_wake:
+	futex_set_and_wake(&rsti(item)->shstk_unlock, 1);
+
+	return ret;
+}
+
+static void shstk_sync_unlock(struct pstree_item *item)
+{
+	/* notify parent that shadow stack is enabled ... */
+	futex_set_and_wake(&rsti(item)->shstk_enable, 1);
+
+	/* ... and wait until it unlocks its features with ptrace */
+	futex_wait_until(&rsti(item)->shstk_unlock, 1);
+}
+
+static void __arch_shstk_enable(struct pstree_item *item,
+				int (*func)(void *arg), void *arg)
+{
+	int ret;
+
+	shstk_sync_unlock(item);
+
+	/* return here would cause #CP, use exit() instead */
+	ret = func(arg);
+	exit(ret);
+}
+
+static int shstk_disable(struct pstree_item *item)
+{
+	shstk_sync_unlock(item);
+
+	/* disable shadow stack, implicitly clears ARCH_SHSTK_WRSS */
+	if (syscall(__NR_arch_prctl, ARCH_SHSTK_DISABLE, ARCH_SHSTK_SHSTK)) {
+		pr_perror("Failed to disable shadow stack");
+		return -1;
+	}
+
+	if (syscall(__NR_arch_prctl, ARCH_SHSTK_LOCK,
+		    ARCH_SHSTK_SHSTK | ARCH_SHSTK_WRSS)) {
+		pr_perror("Failed to lock shadow stack controls");
+		return -1;
+	}
+
+	return 0;
+}
+
+int arch_shstk_trampoline(struct pstree_item *item, CoreEntry *core,
+		      int (*func)(void *arg), void *arg)
+{
+	unsigned long features = ARCH_SHSTK_SHSTK;
+	int code = ARCH_SHSTK_ENABLE;
+
+	/*
+	 * If task does not need shadow stack but CRIU runs with shadow
+	 * stack enabled, we should disable it before continuing with
+	 * restore
+	 */
+	if (!task_needs_shstk(item, core)) {
+		if (kdat.has_shstk && shstk_disable(item))
+			return -1;
+		return func(arg);
+	}
+
+	/*
+	 * Calling sys_arch_prctl() means there will be use of retq
+	 * instruction after shadow stack is enabled and this will cause
+	 * Control Protectiond fault. Open code sys_arch_prctl() in
+	 * assembly.
+	 *
+	 * code and addr should be in %rdi and %rsi and will be passed to
+	 * the system call as is.
+	 */
+	asm volatile("movq $"__stringify(__NR_arch_prctl)", %%rax	\n"
+		     "syscall						\n"
+		     "cmpq $0, %%rax					\n"
+		     "je 1f						\n"
+		     "retq						\n"
+		     "1:						\n"
+		     :: "D"(code), "S"(features));
+
+	__arch_shstk_enable(item, func, arg);
+
+	/* never reached */
+	return -1;
 }
