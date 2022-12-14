3a08a23b13e2b9f0273a4edba227e38fb8fb1ffa
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2022-12-14 15:00:43 +0300

    sk-unix: print message when adding fake queuers
    This would help to understand restore logs of unix sockets.
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    (cherry picked from commit ae1bc673960ce7679527a7fb13cb5dcf8aaf5890)
    Signed-off-by: Alexander Mikhalitsyn <aleksandr.mikhalitsyn@futurfusion.io>

--- criu/sk-unix.c
+++ criu/sk-unix.c
@@ -2259,6 +2259,8 @@ int add_fake_unix_queuers(void)
 {
 	struct unix_sk_info *ui;
 
+	pr_info("Adding fake unix queuers\n");
+
 	list_for_each_entry(ui, &unix_sockets, list) {
 		if ((ui->ue->uflags & (USK_EXTERN | USK_CALLBACK)) || ui->queuer)
 			continue;
