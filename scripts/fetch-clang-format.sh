abfe0b5d2469f5697a9afa0bd8089ced95d3a246
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2023-01-16 18:22:07 +0300

    clang-format: add for_each_bit macros to ForEachMacros
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- scripts/fetch-clang-format.sh
+++ scripts/fetch-clang-format.sh
@@ -10,6 +10,7 @@ curl -s "${URL}" | sed -e "
 	s,ControlStatements,ControlStatementsExceptForEachMacros,g;
 	s,ColumnLimit: 80,ColumnLimit: 120,g;
 	s,Intended for clang-format >= 4,Intended for clang-format >= 11,g;
+	s,ForEachMacros:,ForEachMacros:\n  - 'for_each_bit',g;
 	s,ForEachMacros:,ForEachMacros:\n  - 'for_each_pstree_item',g;
 	s,\(AlignTrailingComments:.*\)$,\1\nAlignConsecutiveMacros: true,g;
 	s,AlignTrailingComments: false,AlignTrailingComments: true,g;
