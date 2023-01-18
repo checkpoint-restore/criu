2640489b7af2c2a5323580d3f5e0db0ae66f0fed
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2023-01-18 14:41:32 +0300

    string: define wrapers __setproctitle and __setproctitle_init to hide bsd headers
    We see that libbsd redefines __has_include to be always true, which
    breaks such checks for rseq. The idea behind this patch is to put all
    uses of libbsd functions to separate c files and only export wrapper
    functions for them.
    
    Using __setproctitle and __setproctitle_init everywhere in existing
    code:
    
    git grep --files-with-matches "setproctitle" | xargs sed -i 's/setproctitle/__setproctitle/g'
    git grep --files-with-matches "setproctitle_init" | xargs sed -i 's/setproctitle_init/__setproctitle_init/g'
    
    Fixes: #2036
    Suggested-by: Andrei Vagin <avagin@google.com>
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- criu/include/setproctitle.h
+++ criu/include/setproctitle.h
@@ -1,19 +1,7 @@
 #ifndef __CR_SETPROCTITLE_H__
 #define __CR_SETPROCTITLE_H__
 
-#ifdef CONFIG_HAS_LIBBSD
-#include <bsd/unistd.h>
-#else
-
-/*
- * setproctitle_init is in the libbsd since v0.6.0. This macro allows to
- * compile criu with libbsd<0.6.0.
- */
-#ifndef CONFIG_HAS_SETPROCTITLE_INIT
-#define setproctitle_init(argc, argv, envp)
-#endif
-
-#define setproctitle(fmt, ...)
-#endif
+extern void __setproctitle_init(int argc, char *argv[], char *envp[]);
+extern void __setproctitle(const char *fmt, ...);
 
 #endif /* __CR_SETPROCTITLE_H__ */
