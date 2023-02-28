checkpoint-restore/criu

commit 37b99ebe5bc65bf48218d8bfe079bc59bc0a39a9

files: Do setup_newborn_fds() later
This patch makes the call of service fds relocation after
root_prepare_shared()->prepare_fd_pid(). Next patches
will make service_fd_base depend on task's max fd used,
and for root_item we need to read all fles to know
the maximum of them.

Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

criu/cr-restore.c
@@ -1051,9 +1051,6 @@ static int setup_newborn_fds(struct pstree_item *me)
 			return -1;
 	}
 
-	if (log_init_by_pid(vpid(me)))
-		return -1;
-
 	return 0;
 }
 
@@ -1607,15 +1604,15 @@ static int restore_task_with_children(void *_arg)
 	if ( !(ca->clone_flags & CLONE_FILES))
 		close_safe(&ca->fd);
 
-	if (setup_newborn_fds(current))
-		goto err;
-
 	pid = getpid();
 	if (vpid(current) != pid) {
 		pr_err("Pid %d do not match expected %d\n", pid, vpid(current));
 		set_task_cr_err(EEXIST);
 		goto err;
 	}
+
+	if (log_init_by_pid(vpid(current)))
+		return -1;
 
 	if (current->parent == NULL) {
 		/*
@@ -1682,6 +1679,9 @@ static int restore_task_with_children(void *_arg)
 		if (populate_root_fd_off())
 			goto err;
 	}
+
+	if (setup_newborn_fds(current))
+		goto err;
 
 	if (restore_task_mnt_ns(current))
 		goto err;

commit 7f48b913e3c0b627a01a0d96be86c97c90b9d052

restore: don't miss futex abort in restore_task_with_children
Fixes: 37b99ebe5 ("files: Do setup_newborn_fds() later")

Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/cr-restore.c
@@ -1790,7 +1790,7 @@ static int restore_task_with_children(void *_arg)
 	}
 
 	if (log_init_by_pid(vpid(current)))
-		return -1;
+		goto err;
 
 	if (current->parent == NULL) {
 		/*