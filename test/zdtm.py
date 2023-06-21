d1455d558637180c163279f2829fe4a514626cbb
Radostin Stoyanov <rstoyanov@fedoraproject.org>
2023-06-21 23:48:32 +0100

    remove python-future dependency
    This commit removes the dependency on the __future__ module, which was
    used to enable Python 3 features in Python 2 code. With support for
    Python 2 being dropped, it is no longer necessary to maintain backward
    compatibility.
    
    Signed-off-by: Radostin Stoyanov <rstoyanov@fedoraproject.org>

--- test/zdtm.py
+++ test/zdtm.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python
-from __future__ import (
-    absolute_import,
-    division,
-    print_function,
-    unicode_literals
-)
+#!/usr/bin/env python3
 
 import argparse
 import atexit
