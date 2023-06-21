4f649fec2409a6135a2eb8958fe60003a523d713
Radostin Stoyanov <rstoyanov@fedoraproject.org>
2023-06-22 00:06:53 +0100

    make: remove checks for python 2 binary
    This commit removes the checks for the Python 2 binary in the makefile
    and makes sure that ZDTM tests always use python3. Since support for
    Python 2 has been dropped, these checks are no longer needed.
    
    Signed-off-by: Radostin Stoyanov <rstoyanov@fedoraproject.org>

--- test/others/mnt-ext-dev/run.sh
+++ test/others/mnt-ext-dev/run.sh
@@ -2,7 +2,7 @@
 set -e -x
 
 # construct root
-python ../../zdtm.py run -t zdtm/static/env00 --iter 0 -f ns
+python3 ../../zdtm.py run -t zdtm/static/env00 --iter 0 -f ns
 
 truncate -s 0 zdtm.loop
 truncate -s 50M zdtm.loop
@@ -11,7 +11,7 @@ dev=`losetup --find --show zdtm.loop`
 mkdir -p ../../dev
 cp -ap $dev ../../dev
 export ZDTM_MNT_EXT_DEV=$dev
-python ../../zdtm.py run $EXTRA_OPTS -t zdtm/static/mnt_ext_dev || ret=$?
+python3 ../../zdtm.py run $EXTRA_OPTS -t zdtm/static/mnt_ext_dev || ret=$?
 losetup -d $dev
 unlink zdtm.loop
 exit $ret
--- test/others/rpc/ps_test.py
+++ test/others/rpc/ps_test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import socket, os, sys, errno
 import rpc_pb2 as rpc
