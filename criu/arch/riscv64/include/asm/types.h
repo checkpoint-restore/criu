6d970ed047592a2dacb51d81338ca7e9ecc21005
Haorong Lu <ancientmodern4@gmail.com>
2023-08-01 12:06:00 -0700

    criu: add riscv64 support to parasite and restorer
    Co-authored-by: Yixue Zhao <felicitia2010@gmail.com>
    Co-authored-by: stove <stove@rivosinc.com>
    Signed-off-by: Haorong Lu <ancientmodern4@gmail.com>

---
+++ criu/arch/riscv64/include/asm/types.h
@@ -0,0 +1,40 @@
+#ifndef __CR_ASM_TYPES_H__
+#define __CR_ASM_TYPES_H__
+
+#include <stdbool.h>
+#include <signal.h>
+#include <asm/ptrace.h>
+#include "images/core.pb-c.h"
+
+#include "page.h"
+#include "bitops.h"
+#include "asm/int.h"
+
+#include <compel/plugins/std/asm/syscall-types.h>
+
+#define core_is_compat(core) false
+
+typedef UserRiscv64RegsEntry UserRegsEntry;
+
+#define CORE_ENTRY__MARCH CORE_ENTRY__MARCH__RISCV64
+
+#define CORE_THREAD_ARCH_INFO(core) core->ti_riscv64
+
+#define TI_SP(core) ((core)->ti_riscv64->gpregs->sp)
+
+#define TI_IP(core) ((core)->ti_riscv64->gpregs->pc)
+
+static inline void *decode_pointer(uint64_t v)
+{
+	return (void *)v;
+}
+static inline uint64_t encode_pointer(void *p)
+{
+	return (uint64_t)p;
+}
+
+#define AT_VECTOR_SIZE 64
+typedef uint64_t auxv_t;
+typedef uint64_t tls_t;
+
+#endif /* __CR_ASM_TYPES_H__ */
