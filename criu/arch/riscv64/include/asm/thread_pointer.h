2d467ac429e52d0839b087efaffaaa9e60a145a4
Haorong Lu <ancientmodern4@gmail.com>
2023-08-01 12:06:00 -0700

    criu: add riscv64 support to parasite and restorer
    Co-authored-by: Yixue Zhao <felicitia2010@gmail.com>
    Co-authored-by: stove <stove@rivosinc.com>
    Signed-off-by: Haorong Lu <ancientmodern4@gmail.com>

---
+++ criu/arch/riscv64/include/asm/thread_pointer.h
@@ -0,0 +1,27 @@
+/* __thread_pointer definition.  Generic version.
+   Copyright (C) 2021 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library.  If not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#ifndef _SYS_THREAD_POINTER_H
+#define _SYS_THREAD_POINTER_H
+
+static inline void *__criu_thread_pointer(void)
+{
+	return __builtin_thread_pointer();
+}
+
+#endif /* _SYS_THREAD_POINTER_H */
