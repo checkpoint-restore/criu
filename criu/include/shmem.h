6e686e2873ca97fc7151c7b6816e8d2bf86c5cd6
Andrei Vagin <avagin@gmail.com>
2023-10-23 18:52:13 +0000

    page-cache: use the PAGEMAP_SCAN ioctl when it is available
    Signed-off-by: Andrei Vagin <avagin@gmail.com>

--- criu/include/shmem.h
+++ criu/include/shmem.h
@@ -4,13 +4,14 @@
 #include "int.h"
 #include "common/lock.h"
 #include "images/vma.pb-c.h"
+#include "pagemap-cache.h"
 
 struct vma_area;
 
 extern int collect_shmem(int pid, struct vma_area *vma);
 extern int collect_sysv_shmem(unsigned long shmid, unsigned long size);
 extern int cr_dump_shmem(void);
-extern int add_shmem_area(pid_t pid, VmaEntry *vma, u64 *map);
+extern int add_shmem_area(pid_t pid, VmaEntry *vma, pmc_t *pmc);
 extern int fixup_sysv_shmems(void);
 extern int dump_one_memfd_shmem(int fd, unsigned long shmid, unsigned long size);
 extern int dump_one_sysv_shmem(void *addr, unsigned long size, unsigned long shmid);
