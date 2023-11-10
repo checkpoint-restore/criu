0d5923c95ef703da0a59dd03e8526af0c152b2a0
Ramesh Errabolu <Ramesh.Errabolu@amd.com>
2023-11-10 13:02:49 -0600

    amdgpu_plugin: Refactor code used to implement Checkpoint
    Refactor code used to Checkpoint DRM devices. Code is moved
    into amdgpu_plugin_drm.c file which hosts various methods to
    checkpoint and restore a workload.
    
    Signed-off-by: Ramesh Errabolu <Ramesh.Errabolu@amd.com>

--- plugins/amdgpu/amdgpu_plugin_drm.h
+++ plugins/amdgpu/amdgpu_plugin_drm.h
@@ -17,6 +17,12 @@
  */
 int amdgpu_plugin_drm_handle_device_vma(int fd, const struct stat *drm);
 
+/**
+ * Serialize meta-data about a particular DRM device, its number of BOs,
+ * etc into a file. The serialized filename has in it the value ID that
+ * is passed in as a parameter
+ */
+int amdgpu_plugin_drm_dump_file(int fd, int id, struct stat *drm);
 
 #endif		/* __AMDGPU_PLUGIN_DRM_H__ */
 
