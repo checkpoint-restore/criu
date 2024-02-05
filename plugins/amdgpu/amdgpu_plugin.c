checkpoint-restore/criu

commit a9cbdad76fa6daf95ea38bd1d5f912d260a84aed

plugin/amdgpu: Don't print error for "No such process" during resume
During the late stages of restore, each process being resumed gets
an ioctl call to KFD_CRIU_OP_RESUME. If the process has no kfd
process info, this call with fail with -ESRCH. This is normal
behaviour, so we shouldn't print an error message for it.

Signed-off-by: David Francis <David.Francis@amd.com>

plugins/amdgpu/amdgpu_plugin.c
@@ -1999,7 +1999,10 @@ int amdgpu_plugin_resume_devices_late(int target_pid)
 	args.op = KFD_CRIU_OP_RESUME;
 	pr_info("Calling IOCTL to start notifiers and queues\n");
 	if (kmtIoctl(fd, AMDKFD_IOC_CRIU_OP, &args) == -1) {
-		pr_perror("restore late ioctl failed");
+		if (errno == ESRCH)
+			pr_info("Pid %d has no kfd process info\n", target_pid);
+		else
+			pr_perror("restore late ioctl failed");
 		ret = -1;
 	}
 

commit 12108c44c3980e02baf1abd6014e3ec9f810bbfc

plugin/amdgpu: Also don't print 'plugin failed' in criu
We already don't treat it as error in the plugin itself, but after
returning -1 from RESUME_DEVICES_LATE hook we print debug message in
criu about failed plugin, let's return 0 instead.

While on it let's replace ret to exit_code.

Fixes: a9cbdad76 ("plugin/amdgpu: Don't print error for "No such process" during resume")
Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

plugins/amdgpu/amdgpu_plugin.c
@@ -1985,7 +1985,7 @@ CR_PLUGIN_REGISTER_HOOK(CR_PLUGIN_HOOK__UPDATE_VMA_MAP, amdgpu_plugin_update_vma
 int amdgpu_plugin_resume_devices_late(int target_pid)
 {
 	struct kfd_ioctl_criu_args args = { 0 };
-	int fd, ret = 0;
+	int fd, exit_code = 0;
 
 	pr_info("Inside %s for target pid = %d\n", __func__, target_pid);
 
@@ -1999,15 +1999,16 @@ int amdgpu_plugin_resume_devices_late(int target_pid)
 	args.op = KFD_CRIU_OP_RESUME;
 	pr_info("Calling IOCTL to start notifiers and queues\n");
 	if (kmtIoctl(fd, AMDKFD_IOC_CRIU_OP, &args) == -1) {
-		if (errno == ESRCH)
+		if (errno == ESRCH) {
 			pr_info("Pid %d has no kfd process info\n", target_pid);
-		else
+		} else {
 			pr_perror("restore late ioctl failed");
-		ret = -1;
+			exit_code = -1;
+		}
 	}
 
 	close(fd);
-	return ret;
+	return exit_code;
 }
 
 CR_PLUGIN_REGISTER_HOOK(CR_PLUGIN_HOOK__RESUME_DEVICES_LATE, amdgpu_plugin_resume_devices_late)