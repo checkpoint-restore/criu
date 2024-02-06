checkpoint-restore/criu

commit 66cab1f49e54791275fcc4d592fedf5825bd94d4

sk-inet: Added IP_TTL socket option
Signed-off-by: rahulk789 <rahul.u.india@gmail.com>

criu/sk-inet.c
@@ -417,10 +417,12 @@ static int dump_ip_opts(int sk, int family, int type, int proto, IpOptsEntry *io
 		ret |= dump_opt(sk, SOL_IP, IP_FREEBIND, &ioe->freebind);
 		ret |= dump_opt(sk, SOL_IP, IP_PKTINFO, &ioe->pktinfo);
 		ret |= dump_opt(sk, SOL_IP, IP_TOS, &ioe->tos);
+		ret |= dump_opt(sk, SOL_IP, IP_TTL, &ioe->ttl);
 	}
 	ioe->has_freebind = ioe->freebind;
 	ioe->has_pktinfo = !!ioe->pktinfo;
 	ioe->has_tos = !!ioe->tos;
+	ioe->has_ttl = !!ioe->ttl;
 
 	return ret;
 }
@@ -817,7 +819,10 @@ int restore_ip_opts(int sk, int family, int proto, IpOptsEntry *ioe)
 			ret |= restore_opt(sk, SOL_IP, IP_PKTINFO, &ioe->pktinfo);
 		if (ioe->has_tos)
 			ret |= restore_opt(sk, SOL_IP, IP_TOS, &ioe->tos);
-	}
+		if (ioe->has_ttl)
+			ret |= restore_opt(sk, SOL_IP, IP_TTL, &ioe->ttl);
+
+    }
 
 	if (ioe->raw)
 		ret |= restore_ip_raw_opts(sk, family, proto, ioe->raw);

images/sk-inet.proto
@@ -20,6 +20,7 @@ message ip_opts_entry {
 
 	optional bool			pktinfo		= 5;
 	optional uint32			tos		= 6;
+	optional uint32			ttl		= 7;
 }
 
 message inet_sk_entry {

commit 7a742481524476a48af2d073c0c3feb5f189d773

sk-inet: fix codding style in restore_ip_opts
Commit [1] introduced codding-style breackage, let's fix it.

Fixes: 66cab1f49 ("sk-inet: Added IP_TTL socket option") [1]
Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/sk-inet.c
@@ -821,8 +821,7 @@ int restore_ip_opts(int sk, int family, int proto, IpOptsEntry *ioe)
 			ret |= restore_opt(sk, SOL_IP, IP_TOS, &ioe->tos);
 		if (ioe->has_ttl)
 			ret |= restore_opt(sk, SOL_IP, IP_TTL, &ioe->ttl);
-
-    }
+	}
 
 	if (ioe->raw)
 		ret |= restore_ip_raw_opts(sk, family, proto, ioe->raw);