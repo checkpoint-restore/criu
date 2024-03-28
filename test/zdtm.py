62ae1402f7bbc44eeddbafc8f251169ffd3a0099
Bhavik Sachdev <b.sachdev1904@gmail.com>
2024-03-29 00:06:58 +0530

    zdtm: Distinguish between fail and crash of dump
    Adds a exit_signal static method to criu_cli, criu_config and criu_rpc
    used to detect a crash.
    
    Fixes: #350
    
    Signed-off-by: Bhavik Sachdev <b.sachdev1904@gmail.com>

--- test/zdtm.py
+++ test/zdtm.py
@@ -899,6 +899,10 @@ class criu_cli:
             return cr
         return cr.wait(timeout=timeout)
 
+    @staticmethod
+    def exit_signal(ret):
+        return ret < 0
+
 
 class criu_rpc_process:
     def wait(self):
@@ -1018,8 +1022,11 @@ class criu_rpc:
             else:
                 raise test_fail_exc('RPC for %s required' % action)
         except crpc.CRIUExceptionExternal as e:
-            print("Fail", e)
-            ret = -1
+            if e.typ != e.resp_typ:
+                ret = -2
+            else:
+                print("Fail", e)
+                ret = -1
         else:
             ret = 0
 
@@ -1031,6 +1038,12 @@ class criu_rpc:
             return p
 
         return ret
+
+    @staticmethod
+    def exit_signal(ret):
+        if ret == -2:
+            return True
+        return False
 
 
 class criu:
@@ -1233,7 +1246,7 @@ class criu:
                     return
             rst_succeeded = os.access(
                 os.path.join(__ddir, "restore-succeeded"), os.F_OK)
-            if self.__test.blocking() or (self.__sat and action == 'restore' and
+            if (self.__test.blocking() and not self.__criu.exit_signal(ret)) or (self.__sat and action == 'restore' and
                                           rst_succeeded):
                 raise test_fail_expected_exc(action)
             else:
