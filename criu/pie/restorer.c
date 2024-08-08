checkpoint-restore/criu

commit 22963d282729a8c31a6a833654b865634a435203

Hide asm/restorer.h from sources
travis-ci: success for criu: Remove asm/ includes from non-arch sources
Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

criu/pie/restorer.c
@@ -38,7 +38,7 @@
 #include "images/mm.pb-c.h"
 
 #include "shmem.h"
-#include "asm/restorer.h"
+#include "restorer.h"
 
 #ifndef PR_SET_PDEATHSIG
 #define PR_SET_PDEATHSIG 1

criu/cr-restore.c
@@ -26,7 +26,6 @@
 #include "types.h"
 #include "ptrace.h"
 #include "compiler.h"
-#include "asm/restorer.h"
 
 #include "cr_options.h"
 #include "servicefd.h"

criu/kerndat.c
@@ -8,9 +8,8 @@
 #include <errno.h>
 #include <sys/syscall.h>
 
-#include "int.h"
 #include "log.h"
-#include "bug.h"
+#include "restorer.h"
 #include "kerndat.h"
 #include "fs-magic.h"
 #include "mem.h"

criu/parasite-syscall.c
@@ -38,7 +38,7 @@
 
 #include "asm/parasite-syscall.h"
 #include "asm/dump.h"
-#include "asm/restorer.h"
+#include "restorer.h"
 #include "pie/pie-relocs.h"
 
 #define MEMFD_FNAME	"CRIUMFD"

criu/pie/parasite.c
@@ -22,7 +22,7 @@
 #include <string.h>
 
 #include "asm/parasite.h"
-#include "asm/restorer.h"
+#include "restorer.h"
 
 static int tsock = -1;
 

criu/sigframe.c
@@ -2,7 +2,6 @@
 #include <string.h>
 
 #include "asm/restore.h"
-#include "asm/restorer.h"
 
 #include "images/core.pb-c.h"
 

commit ea5a20db4ce0c84d22cc654729e40ad99cb70510

delete redundant include header files
restorer.h has been included in line 43.

Fixes: 22963d282729 ("Hide asm/restorer.h from sources")

Signed-off-by: liuchao173 <liuchao173@huawei.com>

criu/pie/restorer.c
@@ -49,7 +49,6 @@
 #include "images/inventory.pb-c.h"
 
 #include "shmem.h"
-#include "restorer.h"
 
 /*
  * sys_getgroups() buffer size. Not too much, to avoid stack overflow.