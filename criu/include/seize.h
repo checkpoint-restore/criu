895a7a73da8e82d7403054189d98a00438fdb51b
Andrei Vagin <avagin@google.com>
2024-09-12 11:17:56 -0700

    fault: allow to check dont_use_freeze_cgroup
    Adds a new "fault" to call dont_use_freeze_cgroup.

--- criu/include/seize.h
+++ criu/include/seize.h
@@ -2,6 +2,7 @@
 #define __CR_SEIZE_H__
 
 extern int collect_pstree(void);
+struct pstree_item;
 extern void pstree_switch_state(struct pstree_item *root_item, int st);
 extern const char *get_real_freezer_state(void);
 extern bool alarm_timeouted(void);
