e87c03d884b585425dc16cf4c33c61c29bb46f84
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2025-01-16 11:56:00 +0800

    zdtm/netns_sub_sysctl: add ipv4/ping_group_range sysctl check
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- test/zdtm/lib/sysctl.h
+++ test/zdtm/lib/sysctl.h
@@ -3,5 +3,7 @@
 
 extern int sysctl_read_int(const char *name, int *data);
 extern int sysctl_write_int(const char *name, int val);
+extern int sysctl_read_str(const char *name, char *data, size_t size);
+extern int sysctl_write_str(const char *name, char *data);
 
 #endif
--- test/zdtm/static/netns_sub_sysctl.c
+++ test/zdtm/static/netns_sub_sysctl.c
@@ -3,18 +3,33 @@
 #include "zdtmtst.h"
 #include "sysctl.h"
 
-const char *test_doc = "Check dump and restore a net.unix.max_dgram_qlen sysctl parameter in subns";
+const char *test_doc = "Check dump and restore of sysctls in subns";
 const char *test_author = "Alexander Mikhalitsyn <alexander@mihalicyn.com>";
+
+#define MAX_STR_SYSCTL_LEN 200
+
+enum {
+	SYSCTL_INT,
+	SYSCTL_STR,
+};
 
 typedef struct {
 	const char *path;
+	int type;
 	int old;
 	int new;
+	char s_old[MAX_STR_SYSCTL_LEN];
+	char s_new[MAX_STR_SYSCTL_LEN];
 } sysctl_opt_t;
 
 #define CONF_UNIX_BASE "/proc/sys/net/unix"
+#define IPV4_SYSCTL_BASE "/proc/sys/net/ipv4"
 
-static sysctl_opt_t net_unix_params[] = { { CONF_UNIX_BASE "/max_dgram_qlen", 0, 0 }, { NULL, 0, 0 } };
+static sysctl_opt_t net_unix_params[] = {
+	{CONF_UNIX_BASE "/max_dgram_qlen", SYSCTL_INT},
+	{IPV4_SYSCTL_BASE "/ping_group_range", SYSCTL_STR, 0, 0, "40000\t50000\n"},
+	{NULL, 0, 0}
+};
 
 int main(int argc, char **argv)
 {
@@ -23,10 +38,17 @@ int main(int argc, char **argv)
 	test_init(argc, argv);
 
 	for (p = net_unix_params; p->path != NULL; p++) {
-		p->old = (((unsigned)lrand48()) % 1023) + 1;
-		if (sysctl_write_int(p->path, p->old)) {
-			pr_perror("Can't change %s", p->path);
-			return -1;
+		if (p->type == SYSCTL_INT) {
+			p->old = (((unsigned)lrand48()) % 1023) + 1;
+			if (sysctl_write_int(p->path, p->old)) {
+				pr_perror("Can't change %s", p->path);
+				return -1;
+			}
+		} else if (p->type == SYSCTL_STR) {
+			if (sysctl_write_str(p->path, p->s_old)) {
+				pr_perror("Can't change %s", p->path);
+				return -1;
+			}
 		}
 	}
 
@@ -34,13 +56,25 @@ int main(int argc, char **argv)
 	test_waitsig();
 
 	for (p = net_unix_params; p->path != NULL; p++) {
-		if (sysctl_read_int(p->path, &p->new))
-			ret = 1;
+		if (p->type == SYSCTL_INT) {
+			if (sysctl_read_int(p->path, &p->new))
+				ret = 1;
 
-		if (p->old != p->new) {
-			errno = EINVAL;
-			pr_perror("%s changed: %d ---> %d", p->path, p->old, p->new);
-			ret = 1;
+			if (p->old != p->new) {
+				errno = EINVAL;
+				pr_perror("%s changed: %d ---> %d", p->path, p->old, p->new);
+				ret = 1;
+			}
+		} else if (p->type == SYSCTL_STR) {
+			if (sysctl_read_str(p->path, p->s_new, MAX_STR_SYSCTL_LEN)) {
+				ret = 1;
+			} else {
+				if (strcmp(p->s_old, p->s_new)) {
+					errno = EINVAL;
+					pr_perror("%s changed: %s ---> %s", p->path, p->s_old, p->s_new);
+					ret = 1;
+				}
+			}
 		}
 	}
 
