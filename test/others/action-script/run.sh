3ee38564b191a1779e680c6736ac1a9c03e9ffaa
Radostin Stoyanov <rstoyanov@fedoraproject.org>
2025-10-21 12:11:45 +0100

    ci: verify call order of action-script hooks
    The existing test collects all action-script hooks triggered during
    `h`, `ns`, and `uns` runs with ZDTM into `actions_called.txt`, then
    verifies that each hook appears at least once. However, the test does
    not verify that hooks are invoked *exactly once* or in *correct order*.
    
    This change updates the test to run ZDTM only with ns flavour as this
    seems to cover all action-script hooks, and checks that all hooks are
    called correctly.
    
    Signed-off-by: Radostin Stoyanov <rstoyanov@fedoraproject.org>

--- test/others/action-script/run.sh
+++ test/others/action-script/run.sh
@@ -5,7 +5,7 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 rm -f "${SCRIPT_DIR}"/actions_called.txt
-"${SCRIPT_DIR}"/../../zdtm.py run -t zdtm/static/env00 --script "$SCRIPT_DIR/show_action.sh" || exit 1
+"${SCRIPT_DIR}"/../../zdtm.py run -t zdtm/static/env00 -f ns --script "$SCRIPT_DIR/show_action.sh" || exit 1
 "${SCRIPT_DIR}"/check_actions.py || exit 1
 
 exit 0
