3c841af2cf1f1769c2fa1527bf2706b705da1202
Radostin Stoyanov <rstoyanov@fedoraproject.org>
2025-10-25 12:35:19 +0100

    pycriu: use explicit imports for __init__
    _init__.py defines the public API for pycriu. It is important to use
    explicit imports to avoid leaking every symbol from criu.py into the
    pycriu namespace. This avoids import-time side effects, prevents name
    collisions, and circular-import traps.
    
    Fixes the following lint error:
      F403 `from .criu import *` used; unable to detect undefined names
    
    Signed-off-by: Radostin Stoyanov <rstoyanov@fedoraproject.org>

--- lib/pycriu/__init__.py
+++ lib/pycriu/__init__.py
@@ -1,4 +1,15 @@
 from . import rpc_pb2 as rpc
 from . import images
-from .criu import *
+from .criu import criu, CRIUExceptionExternal, CRIUException
+from .criu import CR_DEFAULT_SERVICE_ADDRESS
 from .version import __version__
+
+__all__ = (
+    "rpc",
+    "images",
+    "criu",
+    "CRIUExceptionExternal",
+    "CRIUException",
+    "CR_DEFAULT_SERVICE_ADDRESS",
+    "__version__",
+)
