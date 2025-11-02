ff35a9126e3a2d4e6f5f9f2ca89b032f9ae5bc22
David Francis <David.Francis@amd.com>
2025-11-02 07:26:44 -0800

    plugins/amdgpu: remove excessive debug messages
    These pr_info lines begin with "CC3" and "TWI" were not meant to be
    included in the patch.
    
    Co-authored-by: Andrei Vagin <avagin@google.com>
    Signed-off-by: David Francis <David.Francis@amd.com>
    Signed-off-by: Radostin Stoyanov <rstoyanov@fedoraproject.org>

--- plugins/amdgpu/amdgpu_plugin_dmabuf.c
+++ plugins/amdgpu/amdgpu_plugin_dmabuf.c
@@ -55,8 +55,6 @@ int __amdgpu_plugin_dmabuf_dump(int dmabuf_fd, int id)
 	unsigned char *buf = NULL;
 	int gem_handle;
 
-	pr_info("TWI: Dumping dmabuf fd = %d\n", dmabuf_fd);
-
 	gem_handle = handle_for_shared_bo_fd(dmabuf_fd);
 	if (gem_handle < 0) {
 		pr_err("Failed to get handle for dmabuf_fd = %d\n", dmabuf_fd);
@@ -107,8 +105,6 @@ int amdgpu_plugin_dmabuf_restore(int id)
 
 	snprintf(path, sizeof(path), IMG_DMABUF_FILE, id);
 
-	pr_info("TWI: Restoring dmabuf fd, id = %d\n", id);
-
 	/* Read serialized metadata */
 	img_fp = open_img_file(path, false, &img_size);
 	if (!img_fp) {
@@ -139,29 +135,23 @@ int amdgpu_plugin_dmabuf_restore(int id)
 	}
 	fclose(img_fp);
 
-	pr_info("TWI: dmabuf node gem_handle = %d\n", rd->gem_handle);
-
 	/* Match GEM handle with shared_dmabuf list */
 	fd_id = amdgpu_id_for_handle(rd->gem_handle);
 	if (fd_id == -1) {
-		pr_err("Failed to find dmabuf_fd for GEM handle = %d\n",
-		       rd->gem_handle);
+		pr_err("Failed to find dmabuf_fd for GEM handle = %d\n", rd->gem_handle);
 		return 1;
 	}
+
 	int dmabuf_fd = fdstore_get(fd_id);
-	pr_info("TWI: dmabuf node fd_id = %d, dmabuf_fd = %d\n", fd_id, dmabuf_fd);
 	if (dmabuf_fd == -1) {
-		pr_err("Failed to find dmabuf_fd for GEM handle = %d\n",
-		       rd->gem_handle);
+		pr_err("Failed to find dmabuf_fd for GEM handle = %d\n", rd->gem_handle);
 		return 1; /* Retry needed */
-	} else {
-		pr_info("Restored dmabuf_fd = %d for GEM handle = %d\n",
-			dmabuf_fd, rd->gem_handle);
 	}
+
+	pr_info("Restored dmabuf_fd = %d for GEM handle = %d\n", dmabuf_fd, rd->gem_handle);
 	ret = dmabuf_fd;
 
-	pr_info("Successfully restored dmabuf_fd %d\n",
-		dmabuf_fd);
+	pr_info("Successfully restored dmabuf_fd %d\n", dmabuf_fd);
 	criu_dmabuf_node__free_unpacked(rd, NULL);
 	xfree(buf);
 	return ret;
