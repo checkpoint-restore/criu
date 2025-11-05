1826fc5e2613b7310804722ee89730ed79bdf2fa
asafpamzn <asafp@amazon.com>
2025-11-05 18:02:58 +0200

    cow first commit

--- criu/pie/parasite.c
+++ criu/pie/parasite.c
@@ -888,7 +888,7 @@ static int parasite_cow_dump_init(struct parasite_cow_dump_args *args)
 	if (ret < 0) {
 		int e = (ret < 0) ? -ret : ret;     /* convert to +errno code */
 
-		pr_err("Failed to initialize userfaultfd API: %d uffd=%d\n", e, uffd);
+		pr_err("Failed to initialize userfaultfd API: %d uffd=%d errno=%d\n", e, uffd, errno);
 		sys_close(uffd);
 		return -1;
 	}
