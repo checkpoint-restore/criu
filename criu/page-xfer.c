4874dd79b175b0d37f2653bc3593e02c88fe5522
asafpamzn <asafp@amazon.com>
2025-11-19 09:39:21 +0200

    updates to design

--- criu/page-xfer.c
+++ criu/page-xfer.c
@@ -1681,8 +1681,8 @@ static void *unified_page_server_thread(void *arg)
 				remove_active_image(img);
 				continue;
 			}
-			pr_info("Start loop Image dst_id=%lu complete: %lu total pages (%lu COW + %lu requested + %lu regular)\n",
-					img->dst_id, img->total_pages, img->total_cow_pages, img->total_req_pages,
+			pr_info("Start loop Image dst_id=%lu total_pages: %lu img->remaining_pages: %lu total pages (%lu COW + %lu requested + %lu regular)\n",
+					img->dst_id, img->total_pages, img->remaining_pages, img->total_cow_pages, img->total_req_pages,
 					img->total_pages - img->total_cow_pages - img->total_req_pages);
 			pp = dmpi(item)->mem_pp;
 			
