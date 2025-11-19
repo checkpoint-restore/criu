01aa3e3c9c0109dbacca212d64fd8bf5b133034e
asafpamzn <asafp@amazon.com>
2025-11-19 10:59:47 +0200

    updates to design

--- criu/page-pipe.c
+++ criu/page-pipe.c
@@ -430,6 +430,10 @@ int page_pipe_read(struct page_pipe *pp, struct pipe_read_dest *prd, unsigned lo
 	avail = fcntl(prd->p[1], F_GETPIPE_SZ);
 	pr_debug("prd pipe size = %d\n", avail);
 
+	if (avail == 0)
+	{
+		return 0;
+	}
 	ret = tee(ppb->p[0], prd->p[1], len, 0);
 	if (ret != len) {
 		pr_perror("tee: %zd", ret);
