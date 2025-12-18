59b985947c8acd229345c471ffa7c304202bcae3
asafpamzn <asafp@amazon.com>
2025-12-18 16:03:32 +0200

    partial implementation 1

--- criu/mem.c
+++ criu/mem.c
@@ -67,6 +67,8 @@ struct lazy_vma_entry *find_lazy_vma_for_addr(unsigned long vaddr, u64 dst_id)
 	
 	pthread_spin_lock(&lazy_vmas_lock);
 	list_for_each_entry(lve, &global_lazy_vmas, list) {
+		pr_err("VMA start=0x%lx end=%lx \n", lve->vma->e->start, lve->vma->e->end);
+
 		if (vaddr >= lve->vma->e->start && 
 		    vaddr < lve->vma->e->end) { // && 		    lve->dst_id == dst_id) {
 			pthread_spin_unlock(&lazy_vmas_lock);
