71a740c2004f12dc06c67eb7485d36c2f4f2745e
euphoricair7 <shailjashaktawat04@gmail.com>
2026-02-06 15:13:37 +0530

    zdtm: add test for UDP multicast socket membership restoration
    This test documents that CRIU currently does not restore
    IP_ADD_MEMBERSHIP socket options (multicast group memberships).
    The test is marked as broken in its .desc file until support
    is implemented.
    
    Multicast memberships are stored as socket state in the kernel
    but are not currently captured by CRIU's socket dumping code.
    This means processes using multicast may lose group memberships
    after checkpoint/restore.
    
    The test demonstrates this limitation and serves as a starting
    point for implementing multicast support in the future.
    
    Signed-off-by: euphoricair7 <shailjashaktawat04@gmail.com>

---
+++ test/zdtm/static/socket_udp_multicast.c
@@ -0,0 +1,154 @@
+#define _DEFAULT_SOURCE
+
+#include "zdtmtst.h"
+#include <sys/socket.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+const char *test_doc = "Test for UDP multicast socket - DOCUMENTS that CRIU does NOT restore IP_ADD_MEMBERSHIP\n";
+const char *test_author = "Shailja Shaktawat";
+
+#define MCAST_ADDR "224.0.0.250"
+#define PORT	   8888
+#define MSG1	   "msg1"
+
+static int port = PORT;
+static char buf[8];
+
+int main(int argc, char **argv)
+{
+	int ret, recv_sk, send_sk;
+	int yes = 1;
+	unsigned char loop = 1;
+	socklen_t len = sizeof(struct sockaddr_in);
+	struct sockaddr_in mcast_addr, recv_addr, send_addr, from_addr;
+	struct ip_mreqn mreq;
+	struct timeval tv;
+
+	test_init(argc, argv);
+
+	/* Enable multicast routing on loopback - CRITICAL! */
+	system("ip route add 224.0.0.0/4 dev lo 2>/dev/null");
+
+	/* Receiver socket setup */
+	recv_sk = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (recv_sk < 0) {
+		pr_perror("Can't create receiver socket");
+		return 1;
+	}
+
+	if (setsockopt(recv_sk, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) < 0) {
+		pr_perror("Can't set SO_REUSEADDR");
+		return 1;
+	}
+
+	memset(&recv_addr, 0, sizeof(recv_addr));
+	recv_addr.sin_family = AF_INET;
+	recv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	recv_addr.sin_port = htons(PORT);
+
+	ret = bind(recv_sk, (struct sockaddr *)&recv_addr, len);
+	if (ret < 0) {
+		pr_perror("Can't bind receiver socket");
+		return 1;
+	}
+
+	/* Join multicast group */
+	memset(&mreq, 0, sizeof(mreq));
+	mreq.imr_multiaddr.s_addr = inet_addr(MCAST_ADDR);
+	mreq.imr_ifindex = if_nametoindex("lo");
+
+	if (setsockopt(recv_sk, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
+		pr_perror("Can't join multicast group");
+		return 1;
+	}
+
+	/* Sending socket setup */
+	send_sk = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (send_sk < 0) {
+		pr_perror("Can't create sender socket");
+		return 1;
+	}
+
+	/* Enable multicast loopback so we can send to ourselves */
+	if (setsockopt(send_sk, IPPROTO_IP, IP_MULTICAST_LOOP, &loop, sizeof(loop)) < 0) {
+		pr_perror("Can't set multicast loopback");
+		return 1;
+	}
+
+	memset(&send_addr, 0, sizeof(send_addr));
+	send_addr.sin_family = AF_INET;
+	send_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	send_addr.sin_port = htons(port + 1);
+
+	ret = bind(send_sk, (struct sockaddr *)&send_addr, len);
+	if (ret < 0) {
+		pr_perror("Can't bind sender socket");
+		return 1;
+	}
+
+	memset(&mcast_addr, 0, sizeof(mcast_addr));
+	mcast_addr.sin_family = AF_INET;
+	mcast_addr.sin_addr.s_addr = inet_addr(MCAST_ADDR);
+	mcast_addr.sin_port = htons(PORT);
+
+	/* Set receive timeout to avoid hanging */
+	tv.tv_sec = 3;
+	tv.tv_usec = 0;
+	if (setsockopt(recv_sk, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
+		pr_perror("Can't set receive timeout");
+		return 1;
+	}
+
+	test_daemon();
+	test_waitsig();
+
+	/* Small delay to ensure everything is ready */
+	usleep(100000);
+
+	/* Send from sender to multicast, receive on receiver */
+	ret = sendto(send_sk, MSG1, sizeof(MSG1), 0,
+		     (struct sockaddr *)&mcast_addr, len);
+	if (ret < 0) {
+		pr_perror("Can't send multicast");
+		return 1;
+	}
+
+	ret = recvfrom(recv_sk, buf, sizeof(buf) - 1, 0,
+		       (struct sockaddr *)&from_addr, &len);
+
+	if (ret <= 0) {
+		if (errno == EAGAIN || errno == EWOULDBLOCK) {
+			fail("Multicast membership NOT restored (expected - CRIU doesn't support IP_ADD_MEMBERSHIP)");
+			test_msg("This test demonstrates that CRIU currently does NOT restore multicast group memberships\n");
+			test_msg("To fix this, implement IP_ADD_MEMBERSHIP support in criu/sk-inet.c\n");
+			test_msg("See https://github.com/checkpoint-restore/criu/issues/2873\n");
+		} else {
+			pr_perror("Error receiving");
+			return 1;
+		}
+	} else {
+		if (ret != sizeof(MSG1) || memcmp(buf, MSG1, ret)) {
+			fail("Wrong multicast message");
+			return 1;
+		}
+		pass();
+	}
+
+	/* Drop multicast membership */
+	if (setsockopt(recv_sk, IPPROTO_IP, IP_DROP_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
+		pr_perror("Can't drop multicast membership");
+		return 1;
+	}
+
+	close(send_sk);
+	close(recv_sk);
+
+	return 0;
+}
