e89ef5ee2793a33a6c0a9e5bff34028e40c1a565
Abdullah1Badawy1 <abdullahalbadawy1@gmail.com>
2026-03-03 23:42:28 +0200

    contrib: Add Void Linux xbps dependency installation script
    Void Linux was not covered by the existing distro-specific scripts
    in contrib/dependencies/. Add xbps-packages.sh following the same
    structure as apt-packages.sh and dnf-packages.sh.
    
    Void Linux requires special package naming:
    - libuuid-devel instead of util-linux-devel
    - libnftables-devel instead of nftables-devel
    - protobuf must be installed alongside protobuf-devel to get
      the protoc compiler binary
    
    Tested in my main void distro also Void Docker container. CRIU built
    sucessfule and ./criu/criu check => looks good.
    
    Signed-off-by: Abdullah Badawy <abdullahalbadawy1@gmail.com>

---
+++ contrib/dependencies/xbps-packages.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+sudo xbps-install -Sy \
+
+    gcc \
+    make \
+    git \
+    pkg-config \
+    binutils \
+    protobuf \
+    protobuf-devel \
+    protobuf-c-devel \
+    python3 \
+    python3-protobuf \
+    python3-setuptools \
+    python3-pip \
+    python3-yaml \
+    libnet-devel \
+    libnl3-devel \
+    libcap-devel \
+    libaio-devel \
+    gnutls-devel \
+    libbsd-devel \
+    libuuid-devel \
+    elfutils-devel \
+    libbpf-devel \
+    libdrm-devel \
+    libnftables-devel \
+    libselinux-devel \
+    iproute2 \
+    libtraceevent-devel \
+    libtracefs-devel \
+    xmlto \
+    asciidoc
