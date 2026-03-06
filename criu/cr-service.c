checkpoint-restore/criu

commit 622b43392fcb330343243a0f2842dd2919f977cc

criu: Initialize util before service worker starts
When restoring dumps in new mount + pid namespaces where multiple dumps
share the same network namespace, CRIU may fail due to conflicting
unix socket names. This happens because the service worker creates
sockets using a pattern that includes criu_run_id, but util_init()
is called after cr_service_work() starts.

The socket naming pattern "crtools-fd-%d-%d" uses the restore PID
and criu_run_id, however criu_run_id is always 0 when not initialized,
leading to conflicts when multiple restores run simultaneously either
in the same CRIU process or because of multiple CRIU processes
doing the same operation in different PID namespaces.

Fix this by:

- Moving util_init() before cr_service_work() starts
- Adding a second util_init() call in the service worker fork
to ensure unique IDs across multiple worker runs
- Making sure that dump and restore operations have util_init() called
early to generate unique socket names

With this fix, socket names always include the namespace ID, preventing
conflicts when multiple processes with the same pid share a network
namespace.

Fixes #2499

[ avagin: minore code changes ]

Signed-off-by: Lorenzo Fontana <fontanalorenz@gmail.com>
Signed-off-by: Andrei Vagin <avagin@google.com>

criu/cr-service.c
@@ -1310,6 +1310,14 @@ int cr_service_work(int sk)
 	int ret = -1;
 	CriuReq *msg = 0;
 
+	/*
+	 * util_init initializes criu_run_id and compel_run_id so that sockets
+	 * are generated with an unique name identifying the specific process
+	 * even in cases where multiple processes with the same pid in
+	 * different pid namespaces are sharing the same network namespace.
+	 */
+	util_init();
+
 more:
 	opts.mode = CR_SWRK;
 

criu/crtools.c
@@ -169,7 +169,13 @@ int main(int argc, char *argv[], char *envp[])
 		pr_err("unknown command: %s\n", argv[optind]);
 		goto usage;
 	}
-
+	/*
+	 * util_init initializes criu_run_id and compel_run_id so that sockets
+	 * are generated with an unique name identifying the specific process
+	 * even in cases where multiple processes with the same pid in
+	 * different pid namespaces are sharing the same network namespace.
+	 */
+	util_init();
 	if (opts.mode == CR_SWRK) {
 		if (argc != optind + 2) {
 			fprintf(stderr, "Usage: criu swrk <fd>\n");
@@ -253,8 +259,6 @@ int main(int argc, char *argv[], char *envp[])
 		pr_perror("Can't change directory to %s", opts.work_dir);
 		return 1;
 	}
-
-	util_init();
 
 	if (log_init(opts.output))
 		return 1;

commit e61a11c9cc67bcb63ee5ff978316ee47955d43cc

cr-service: generate uuid only once
When checkpointing containers, the function `util_init()` appears to be
called twice, once in `crtools.c` then again in `cr-service.c`:

(00.000000) Parsing config file /etc/criu/default.conf
(00.000000) Unable to get $HOME directory, local configuration file will not be used.
(00.000000) CRIU run id = 1c9f31c3-aa70-4e8b-9b0c-19410efd92fe
(00.000000) CRIU run id = f19c528e-593c-4f29-b8e5-a2ff9c6b4d73
(00.000000) Would overwrite RPC settings with values from /etc/criu/runc.conf
(00.000000) Parsing config file /etc/criu/runc.conf
(00.000082) Version: 4.2 (gitid v4.2-93-gd691ed685)

This appears to be unnecessary as `cr_service()` is always
called in `crtools.c` after `util_init()`.

Fixes: 622b433 ("criu: Initialize util before service worker starts")

Signed-off-by: Radostin Stoyanov <rstoyanov@fedoraproject.org>

criu/cr-service.c
@@ -1358,14 +1358,6 @@ int cr_service_work(int sk)
 	int ret = -1;
 	CriuReq *msg = 0;
 
-	/*
-	 * util_init initializes criu_run_id and compel_run_id so that sockets
-	 * are generated with an unique name identifying the specific process
-	 * even in cases where multiple processes with the same pid in
-	 * different pid namespaces are sharing the same network namespace.
-	 */
-	util_init();
-
 more:
 	opts.mode = CR_SWRK;
 