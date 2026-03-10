1ecc688a28c1fb94bc976329c6f9ad4fe8c4159a
Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
2026-03-10 15:47:25 +0000

    plugins/amdgpu: Do not eat the errno in kmtIoctl
    Error logging for just a single error condition will eat the errno so lets
    just remove it. All callers use pr_perror() and rely on the helper not
    munching it up so no information is lost by removing the internal log.
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
    Reviewed-By: David Francis <David.Francis@amd.com>

--- plugins/amdgpu/amdgpu_plugin.c
+++ plugins/amdgpu/amdgpu_plugin.c
@@ -102,11 +102,6 @@ int kmtIoctl(int fd, unsigned long request, void *arg)
 		ret = ioctl(fd, request, arg);
 	} while (ret == -1 && max_retries-- > 0 && (errno == EINTR || errno == EAGAIN));
 
-	if (ret == -1 && errno == EBADF)
-		/* In case pthread_atfork didn't catch it, this will
-		 * make any subsequent hsaKmt calls fail in CHECK_KFD_OPEN.
-		 */
-		pr_perror("KFD file descriptor not valid in this process");
 	return ret;
 }
 
