checkpoint-restore/criu

commit 2d2168fc9c142a2eaf18f319b0d21825775d5660

vdso: relax EI_OSABI check to support linux in ELF header
On some ARM/aarch64 systems, the VDSO ELF header sets EI_OSABI to 3 (Linux),
while CRIU expects 0 (System V). This strict check causes restore to fail
with "ELF header magic mismatch"

This patch relaxes the check to accept both values, improving compatibility
with modern toolchains and kernels (e.g. Linux 6.12+)

Fixes: #2751
Signed-off-by: dong sunchao <dongsunchao@gmail.com>

criu/pie/util-vdso.c
@@ -98,25 +98,45 @@ static unsigned long elf_gnu_hash(const unsigned char *name)
 
 static int has_elf_identity(Ehdr_t *ehdr)
 {
-	/*
-	 * See Elf specification for this magic values.
-	 */
+	/* check ELF magic */
+
+	if (ehdr->e_ident[EI_MAG0] != ELFMAG0 ||
+	    ehdr->e_ident[EI_MAG1] != ELFMAG1 ||
+	    ehdr->e_ident[EI_MAG2] != ELFMAG2 ||
+	    ehdr->e_ident[EI_MAG3] != ELFMAG3) {
+		pr_err("Invalid ELF magic\n");
+		return false;
+	};
+
+	/* check ELF class */
 #if defined(CONFIG_VDSO_32)
-	static const char elf_ident[] = {
-		0x7f, 0x45, 0x4c, 0x46, 0x01, BORD, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	if (ehdr->e_ident[EI_CLASS] != ELFCLASS32) {
+		pr_err("Unsupported ELF class: %d\n", ehdr->e_ident[EI_CLASS]);
+		return false;
 	};
 #else
-	static const char elf_ident[] = {
-		0x7f, 0x45, 0x4c, 0x46, 0x02, BORD, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	if (ehdr->e_ident[EI_CLASS] != ELFCLASS64) {
+		pr_err("Unsupported ELF class: %d\n", ehdr->e_ident[EI_CLASS]);
+		return false;
 	};
 #endif
 
-	BUILD_BUG_ON(sizeof(elf_ident) != sizeof(ehdr->e_ident));
-
-	if (memcmp(ehdr->e_ident, elf_ident, sizeof(elf_ident))) {
-		pr_err("ELF header magic mismatch\n");
+	/* check ELF data encoding */
+	if (ehdr->e_ident[EI_DATA] != ELFDATA2LSB) {
+		pr_err("Unsupported ELF data encoding: %d\n", ehdr->e_ident[EI_DATA]);
 		return false;
-	}
+	};
+	/* check ELF version */
+	if (ehdr->e_ident[EI_VERSION] != EV_CURRENT) {
+		pr_err("Unsupported ELF version: %d\n", ehdr->e_ident[EI_VERSION]);
+		return false;
+	};
+	/* check ELF OSABI */
+	if (ehdr->e_ident[EI_OSABI] != ELFOSABI_NONE &&
+	    ehdr->e_ident[EI_OSABI] != ELFOSABI_LINUX) {
+		pr_err("Unsupported OSABI version: %d\n", ehdr->e_ident[EI_OSABI]);
+		return false;
+	};
 
 	return true;
 }

commit bf2225965e04bd38ac09ede9b8047382724a4764

vdso: replace ELFDATA2LSB with BORD in has_elf_identity
Commit 2d2168fc9 hardcodes ELFDATA2LSB in place of a previously used
endian-aware macro BORD, causing issues on big endian architectures,
e.g., on s390x:

    $ ./criu/criu check -v4
    (00.000000) CRIU run id = 6b0cc8b9-0cbe-44e7-aeb4-7387862d9670
    (00.000016) Version: 4.2 (gitid v4.2-211-g4d76d1acd)
    [...]
    (00.061309) vdso: Parsing at 3ffc75ae000 3ffc75b0000
    (00.061319) Error (criu/pie-util-vdso.c:126): vdso: Unsupported ELF data encoding: 2
    (00.061320) Error (criu/vdso.c:633): vdso: Failed to fill self vdso symtable
    (00.061321) Error (criu/kerndat.c:2043): kerndat_vdso_fill_symtable failed when initializing kerndat.
    (00.061367) Adjust mmap_min_addr 0x1000 -> 0x10000
    (00.061368) Found mmap_min_addr 0x10000
    (00.061374) files stat: fs/nr_open 1073741816
    (00.061375) Error (criu/crtools.c:280): Could not initialize kernel features detection.

Fixes: 2d2168fc9 ("vdso: relax EI_OSABI check to support linux in ELF header")

Signed-off-by: Čestmír Kalina <ckalina@redhat.com>

criu/pie/util-vdso.c
@@ -122,7 +122,7 @@ static int has_elf_identity(Ehdr_t *ehdr)
 #endif
 
 	/* check ELF data encoding */
-	if (ehdr->e_ident[EI_DATA] != ELFDATA2LSB) {
+	if (ehdr->e_ident[EI_DATA] != BORD) {
 		pr_err("Unsupported ELF data encoding: %d\n", ehdr->e_ident[EI_DATA]);
 		return false;
 	};