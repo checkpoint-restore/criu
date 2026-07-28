90852503c97f01744f8ced9afd56a06eead278fa
Adrian Reber <areber@redhat.com>
2026-07-28 20:38:29 +0000

    ci: allow user namespace for unprivileged users
    On Ubuntu >= 24.04 user namespaces for unprivileged users need to be
    turned on explicitly.
    
    Signed-off-by: Adrian Reber <areber@redhat.com>

--- scripts/ci/run-ci-tests.sh
+++ scripts/ci/run-ci-tests.sh
@@ -52,10 +52,6 @@ ci_prep () {
 	# This can fail on aarch64
 	service apport stop || :
 
-	# Ubuntu has set up AppArmor in 24.04 so that it blocks use of user
-	# namespaces by unprivileged users. We need this for some of our tests.
-	sysctl kernel.apparmor_restrict_unprivileged_userns=0 || :
-
 	if [ "$CLANG" = "1" ]; then
 		# clang support
 		CC=clang
@@ -148,6 +144,10 @@ modprobe -v sit || :
 print_env
 
 ci_prep
+
+# Ubuntu has set up AppArmor in >= 24.04 so that it blocks use of user
+# namespaces by unprivileged users. We need this for some of our tests.
+sysctl kernel.apparmor_restrict_unprivileged_userns=0 || :
 
 if [ "${CD_TO_TOP}" = "1" ]; then
 	cd ../../
