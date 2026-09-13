checkpoint-restore/criu

commit 7d79a58f4daa9ddda015fc0aaca2a60d156d12cc

img-streamer: introduction of criu-image-streamer
This adds the ability to stream images with criu-image-streamer

The workflow is the following:
1) criu-image-streamer is started, and starts listening on a UNIX
   socket.
2) CRIU is started. img_streamer_init() is invoked, which connects to the
   socket. During dump/restore operations, instead of using local disk to
   open an image file, img_streamer_open() is called to provide a UNIX pipe
   that is sent over the UNIX socket.
3) Once the operation is done, img_streamer_finish() is called, and the
   UNIX socket is disconnected.

criu-image-streamer can be found at:
https://github.com/checkpoint-restore/criu-image-streamer

Signed-off-by: Nicolas Viennot <Nicolas.Viennot@twosigma.com>

criu/pagemap.c
@@ -406,6 +406,49 @@ static int maybe_read_page_local(struct page_read *pr, unsigned long vaddr,
 	return ret;
 }
 
+/*
+ * We cannot use maybe_read_page_local() for streaming images as it uses
+ * pread(), seeking in the file. Instead, we use this custom page reader.
+ */
+static int maybe_read_page_img_streamer(struct page_read *pr, unsigned long vaddr,
+					int nr, void *buf, unsigned flags)
+{
+	unsigned long len = nr * PAGE_SIZE;
+	int fd = img_raw_fd(pr->pi);
+	int ret;
+	size_t curr = 0;
+
+	pr_debug("\tpr%lu-%u Read page from self %lx/%"PRIx64"\n",
+		 pr->img_id, pr->id, pr->cvaddr, pr->pi_off);
+
+	/* We can't seek. The requested address better match */
+	BUG_ON(pr->cvaddr != vaddr);
+
+	while (1) {
+		ret = read(fd, buf + curr, len - curr);
+		if (ret == 0) {
+			pr_err("Reached EOF unexpectedly while reading page from image\n");
+			return -1;
+		} else if (ret < 0) {
+			pr_perror("Can't read mapping page %d", ret);
+			return -1;
+		}
+		curr += ret;
+		if (curr == len)
+			break;
+	}
+
+	if (opts.auto_dedup)
+		pr_warn_once("Can't dedup when streaming images\n");
+
+	if (ret == 0 && pr->io_complete)
+		ret = pr->io_complete(pr, vaddr, nr);
+
+	pr->pi_off += len;
+
+	return ret;
+}
+
 static int read_page_complete(unsigned long img_id, unsigned long vaddr, int nr_pages, void *priv)
 {
 	int ret = 0;
@@ -601,6 +644,10 @@ static int try_open_parent(int dfd, unsigned long id, struct page_read *pr, int 
 	int pfd, ret;
 	struct page_read *parent = NULL;
 
+	/* Image streaming lacks support for incremental images */
+	if (opts.stream)
+		goto out;
+
 	pfd = openat(dfd, CR_PARENT_LINK, O_RDONLY);
 	if (pfd < 0 && errno == ENOENT)
 		goto out;
@@ -657,7 +704,19 @@ static int init_pagemaps(struct page_read *pr)
 	off_t fsize;
 	int nr_pmes, nr_realloc;
 
-	fsize = img_raw_size(pr->pmi);
+	if (opts.stream) {
+		/*
+		 * TODO - There is no easy way to estimate the size of the
+		 * pagemap that is still to be read from the pipe. Possible
+		 * solution is to ask the image streamer for the size of the
+		 * image. 1024 is a wild guess (more space is allocated if
+		 * needed).
+		 */
+		fsize = 1024;
+	} else {
+		fsize = img_raw_size(pr->pmi);
+	}
+
 	if (fsize < 0)
 		return -1;
 
@@ -781,6 +840,8 @@ int open_page_read_at(int dfd, unsigned long img_id, struct page_read *pr, int p
 
 	if (remote)
 		pr->maybe_read_page = maybe_read_page_remote;
+	else if (opts.stream)
+		pr->maybe_read_page = maybe_read_page_img_streamer;
 	else {
 		pr->maybe_read_page = maybe_read_page_local;
 		if (!pr->parent && !opts.lazy_pages)

criu/Makefile.crtools
@@ -29,6 +29,7 @@ obj-y			+= files-reg.o
 obj-y			+= fsnotify.o
 obj-y			+= image-desc.o
 obj-y			+= image.o
+obj-y			+= img-streamer.o
 obj-y			+= ipc_ns.o
 obj-y			+= irmap.o
 obj-y			+= kcmp-ids.o

criu/config.c
@@ -510,6 +510,7 @@ int parse_options(int argc, char **argv, bool *usage_error,
 		BOOL_OPT(SK_CLOSE_PARAM, &opts.tcp_close),
 		{ "verbosity",			optional_argument,	0, 'v'	},
 		{ "ps-socket",			required_argument,	0, 1091},
+		BOOL_OPT("stream", &opts.stream),
 		{ "config",			required_argument,	0, 1089},
 		{ "no-default-config",		no_argument,		0, 1090},
 		{ "tls-cacert",			required_argument,	0, 1092},

criu/cr-dump.c
@@ -82,6 +82,7 @@
 #include "eventpoll.h"
 #include "memfd.h"
 #include "timens.h"
+#include "img-streamer.h"
 
 /*
  * Architectures can overwrite this function to restore register sets that
@@ -1759,6 +1760,7 @@ static int cr_dump_finish(int ret)
 	free_userns_maps();
 
 	close_service_fd(CR_PROC_FD_OFF);
+	close_image_dir();
 
 	if (ret) {
 		pr_err("Dumping FAILED.\n");

criu/cr-restore.c
@@ -29,6 +29,7 @@
 #include "cr_options.h"
 #include "servicefd.h"
 #include "image.h"
+#include "img-streamer.h"
 #include "util.h"
 #include "util-pie.h"
 #include "criu-log.h"
@@ -2354,6 +2355,9 @@ skip_ns_bouncing:
 
 	pr_info("Restore finished successfully. Tasks resumed.\n");
 	write_stats(RESTORE_STATS);
+
+	/* This has the effect of dismissing the image streamer */
+	close_image_dir();
 
 	ret = run_scripts(ACT_POST_RESUME);
 	if (ret != 0)

criu/cr-service.c
@@ -343,7 +343,14 @@ static int setup_opts_from_req(int sk, CriuOpts *req)
 	if (req->parent_img)
 		SET_CHAR_OPTS(img_parent, req->parent_img);
 
-	if (open_image_dir(images_dir_path) < 0) {
+	/*
+	 * Image streaming is not supported with CRIU's service feature as
+	 * the streamer must be started for each dump/restore operation.
+	 * It is unclear how to do that with RPC, so we punt for now.
+	 * This explains why we provide the argument mode=-1 instead of
+	 * O_RSTR or O_DUMP.
+	 */
+	if (open_image_dir(images_dir_path, -1) < 0) {
 		pr_perror("Can't open images directory");
 		goto err;
 	}

criu/crtools.c
@@ -54,6 +54,20 @@ void flush_early_log_to_stderr(void)
 	flush_early_log_buffer(STDERR_FILENO);
 }
 
+static int image_dir_mode(char *argv[], int optind)
+{
+	if (!strcmp(argv[optind], "dump") ||
+	    !strcmp(argv[optind], "pre-dump") ||
+	    (!strcmp(argv[optind], "cpuinfo") && !strcmp(argv[optind + 1], "dump")))
+		return O_DUMP;
+
+	if (!strcmp(argv[optind], "restore") ||
+	    (!strcmp(argv[optind], "cpuinfo") && !strcmp(argv[optind + 1], "restore")))
+		return O_RSTR;
+
+	return -1;
+}
+
 int main(int argc, char *argv[], char *envp[])
 {
 	int ret = -1;
@@ -148,12 +162,29 @@ int main(int argc, char *argv[], char *envp[])
 		}
 	}
 
+	if (opts.stream && image_dir_mode(argv, optind) == -1) {
+		pr_err("--stream cannot be used with the %s command\n", argv[optind]);
+		goto usage;
+	}
+
 	/* We must not open imgs dir, if service is called */
 	if (strcmp(argv[optind], "service")) {
-		ret = open_image_dir(opts.imgs_dir);
+		ret = open_image_dir(opts.imgs_dir, image_dir_mode(argv, optind));
 		if (ret < 0)
 			return 1;
 	}
+
+	/*
+	 * The kernel might send us lethal signals when writing to a pipe
+	 * which reader has disappeared. We deal with write() failures on our
+	 * own, and prefer not to get killed. So we ignore SIGPIPEs.
+	 *
+	 * Pipes are used in various places:
+	 * 1) Receiving application page data
+	 * 2) Transmitting data to the image streamer
+	 * 3) Emitting logs (potentially to a pipe).
+	 */
+	signal(SIGPIPE, SIG_IGN);
 
 	/*
 	 * When a process group becomes an orphan,
@@ -322,6 +353,7 @@ usage:
 "                        this requires running a second instance of criu\n"
 "                        in lazy-pages mode: 'criu lazy-pages -D DIR'\n"
 "                        --lazy-pages and lazy-pages mode require userfaultfd\n"
+"  --stream              dump/restore images using criu-image-streamer\n"
 "\n"
 "* External resources support:\n"
 "  --external RES        dump objects from this list as external resources:\n"

criu/files-reg.c
@@ -154,7 +154,6 @@ static int trim_last_parent(char *path)
 
 static int copy_chunk_from_file(int fd, int img, off_t off, size_t len)
 {
-	char *buf = NULL;
 	int ret;
 
 	while (len > 0) {
@@ -167,7 +166,6 @@ static int copy_chunk_from_file(int fd, int img, off_t off, size_t len)
 		len -= ret;
 	}
 
-	xfree(buf);
 	return 0;
 }
 
@@ -213,7 +211,6 @@ static int copy_file_to_chunks(int fd, struct cr_img *img, size_t file_size)
 
 static int copy_chunk_to_file(int img, int fd, off_t off, size_t len)
 {
-	char *buf = NULL;
 	int ret;
 
 	while (len > 0) {
@@ -221,7 +218,11 @@ static int copy_chunk_to_file(int img, int fd, off_t off, size_t len)
 			pr_perror("Can't seek file");
 			return -1;
 		}
-		ret = sendfile(fd, img, NULL, len);
+
+		if (opts.stream)
+			ret = splice(img, NULL, fd, NULL, len, SPLICE_F_MOVE);
+		else
+			ret = sendfile(fd, img, NULL, len);
 		if (ret < 0) {
 			pr_perror("Can't send data");
 			return -1;
@@ -231,7 +232,6 @@ static int copy_chunk_to_file(int img, int fd, off_t off, size_t len)
 		len -= ret;
 	}
 
-	xfree(buf);
 	return 0;
 }
 

commit 256cb559fa962f52c37cf8b538ae889bfe792645

pagemap: don't ask the streamer for a writable pages image
--auto-dedup sets PR_MOD on every page-read, which opens the pages image
O_RDWR so the restore can punch holes in it as pages are consumed. The
image streamer only ever serves the mode it was started with, so the
writable open trips the BUG_ON(flags != img_streamer_mode) in
img_streamer_open() and criu dies.

Deduplication is already skipped when the pages come from the streamer,
one warning and no punch_hole, so the writable open buys nothing there.
Leave PR_MOD off when the images are streamed.

Fixes: 7d79a58f4daa ("img-streamer: introduction of criu-image-streamer")
Assisted-by: Claude:claude-opus-5
Signed-off-by: Denis V. Lunev <den@openvz.org>

criu/pagemap.c
@@ -2568,7 +2568,7 @@ int open_page_read_at(int dfd, unsigned long img_id, struct page_read *pr, int p
 	 * others are always local.
 	 */
 	pr_flags &= ~PR_REMOTE;
-	if (opts.auto_dedup)
+	if (opts.auto_dedup && !opts.stream)
 		pr_flags |= PR_MOD;
 	if (pr_flags & PR_MOD)
 		flags = O_RDWR;