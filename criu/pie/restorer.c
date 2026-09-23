checkpoint-restore/criu

commit ef0a0481c3b54d8e51fed05113818070513362f0

restorer: Fix periodic timerfd dumped in zero value state
Fast periodic timerfd could be dumped with it_value={0,0}
On restore, sys_timerfd_settime() with it_value={0,0} will
create a timerfd in "switched-off" state, this timerfd will
not start. Using sys_ioctl(..., TFD_IOC_SET_TICKS,...) force
timerfd to fire once, but next read(), in restored application,
will hang on the endless wait: the timer is not started.

To avoid this, the periodic timerfd's with ticks==1 & it_value==0
should set it_value equal to it_interval.

Signed-off-by: juranvrsk <pim1289@yandex.ru>
Signed-off-by: Iurii Metreveli <pim1289@yandex.ru>

criu/pie/restorer.c
@@ -1284,9 +1284,28 @@ static int timerfd_arm(struct task_restore_args *args)
 				 (unsigned long long)t->val.it_value.tv_nsec);
 		}
 
-		ret = sys_timerfd_settime(t->fd, t->settime_flags, &t->val, NULL);
-		if (t->ticks)
+		/*
+		 * Cases like it_value == (0,0), it_interval != {0,0} are
+		 * the edge case, and we should look the ticks value.
+		 * If ticks = 0 - the timerfd is switched off
+		 * If ticks != 0 - the timerfd is periodic and just expired
+		 * After the restore timerfd does not know it switched off or periodic,
+		 * even after setup ticks value to kernel(TFD_IOC_SET_TICKS). 
+		 * Just do it like in posix timers (decode_itimer()).
+		 */
+		if (t->ticks) {
+			if(!(t->val.it_value.tv_sec | t->val.it_value.tv_nsec) && 
+			(t->val.it_interval.tv_sec | t->val.it_interval.tv_nsec)) {
+				pr_info("Zeroed periodic timer are found! Id: %x \n", t->id);
+				t->val.it_value.tv_sec = t->val.it_interval.tv_sec;
+				t->val.it_value.tv_nsec = t->val.it_interval.tv_nsec;
+			}
+			ret = sys_timerfd_settime(t->fd, t->settime_flags, &t->val, NULL);			
 			ret |= sys_ioctl(t->fd, TFD_IOC_SET_TICKS, (unsigned long)&t->ticks);
+		} 
+		else
+			ret = sys_timerfd_settime(t->fd, t->settime_flags, &t->val, NULL);
+
 		if (ret) {
 			pr_err("Can't restore ticks/time for timerfd - %d\n", i);
 			return ret;

commit 76808f629c0d958944d06df2295aefcbe0ac9a39

restorer: remove trailing whitespace
Commit ef0a0481c ("restorer: Fix periodic timerfd dumped in zero value
state") introduced trailing whitespace, which makes "make lint" fail.
Remove it.

Signed-off-by: Rohan Pattanayak <rohanpxquantum@gmail.com>

criu/pie/restorer.c
@@ -1290,19 +1290,19 @@ static int timerfd_arm(struct task_restore_args *args)
 		 * If ticks = 0 - the timerfd is switched off
 		 * If ticks != 0 - the timerfd is periodic and just expired
 		 * After the restore timerfd does not know it switched off or periodic,
-		 * even after setup ticks value to kernel(TFD_IOC_SET_TICKS). 
+		 * even after setup ticks value to kernel(TFD_IOC_SET_TICKS).
 		 * Just do it like in posix timers (decode_itimer()).
 		 */
 		if (t->ticks) {
-			if(!(t->val.it_value.tv_sec | t->val.it_value.tv_nsec) && 
+			if(!(t->val.it_value.tv_sec | t->val.it_value.tv_nsec) &&
 			(t->val.it_interval.tv_sec | t->val.it_interval.tv_nsec)) {
 				pr_info("Zeroed periodic timer are found! Id: %x \n", t->id);
 				t->val.it_value.tv_sec = t->val.it_interval.tv_sec;
 				t->val.it_value.tv_nsec = t->val.it_interval.tv_nsec;
 			}
-			ret = sys_timerfd_settime(t->fd, t->settime_flags, &t->val, NULL);			
+			ret = sys_timerfd_settime(t->fd, t->settime_flags, &t->val, NULL);
 			ret |= sys_ioctl(t->fd, TFD_IOC_SET_TICKS, (unsigned long)&t->ticks);
-		} 
+		}
 		else
 			ret = sys_timerfd_settime(t->fd, t->settime_flags, &t->val, NULL);
 